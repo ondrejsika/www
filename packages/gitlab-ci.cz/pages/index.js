f0d5c82cfa171a30c49027818e39f06bf75a8fbf
Zuzana Jeschkeová <zuzanajeschkeova@gmail.com>
2019-04-19 18:31:41 +0200

    gitlab-ci.cz - import from Jekyll

--- packages/gitlab-ci.cz/pages/index.js
+++ packages/gitlab-ci.cz/pages/index.js
@@ -1,5 +1,6 @@
 import '../css'
 
+import ReactMarkdown from 'react-markdown'
 import Footer from 'course-landing/components/Footer'
 import Header from 'course-landing/components/Header'
 import HeaderLink from 'course-landing/components/HeaderLink'
@@ -27,49 +28,49 @@ const Index = () => (
     <Header
       websiteHeader="Školení Gitlab CI"
       byName="Ondrej Sika"
-      byUrl="https://ondrej-sika.cz/?x_source=skoleni-docker.cz"
-      logoUrl="/static/docker-w.svg"
+      byUrl="https://ondrej-sika.cz/?x_source=skoleni-gitlab-ci.cz"
+      logoUrl="/static/gitlab-ci.svg"
     >
-      <HeaderLink url="https://ondrej-sika.cz/skoleni/docker/poptavka/?x_source=skoleni-docker.cz">Nezávazně poptat školení</HeaderLink>
-      <HeaderLink url="https://ondrej-sika.cz/kontakt/?x_source=skoleni-docker.cz">Kontaktovat</HeaderLink>
+      <HeaderLink url="https://ondrej-sika.cz/skoleni/gitlab-ci/poptavka/?x_source=skoleni-gitlab-ci.cz">Nezávazně poptat školení</HeaderLink>
+      <HeaderLink url="https://ondrej-sika.cz/kontakt/?x_source=skoleni-gitlab-ci.cz">Kontaktovat</HeaderLink>
     </Header>
     <div className="container container-width">
-      <TextFrame header="Co je to Docker">
-        Docker je dnes technologie, která proces vývoje, testování a provozování softwaru velmi zjednodušuje. Provozujte cokoliv a kdekoliv. Už nečekejte na nastavení vašeho serveru nebo na aktualizaci nějaké knihovny. Mějte prostředí jako součást vaší aplikace.
+      <TextFrame header="Popis technologie">
+        Continuous Integration (zkráceně CI nebo CI/CD) je nástroj, který za Vás automatizuje rutijni úkoly jako je pouštění testů, kvality kódu a nebo nasazování. Umožní vám rychleji začleňovat změny do masteru a rychleji nasazovat. Výhodou Gitlab CI je velmi dobrá podpora Dockeru, můžete velmi jednoduše skládat Docker images a nebo pouštět testy v kontejnerech.
       </TextFrame>
       <TextFrame header="Jak školení probíhá">
-        Na školení Úvod do práce s Dockerem si ukážeme vše jak s Dockerem pracovat od vytvoření vlastního kontejnerů po jeho provozování ve Swarmu. Vše si prakticky vyzkoušíme a osaháme.
+        Na školení formou workshopu si vysvětlíme proč je dobré CI používat, ukážeme si možnosti Gitlab CI a vyzkoušíme si napsat CI skript pro ukázkový projekt. Výsledkem bude CI skript pro jednoduchý projekt obsahující joby od buildu aplikace, přez testy až po deployment.
       </TextFrame>
       <TextFrame header="Co Vás naučím">
-        <ul>
-          <li>Teoretický úvod do Dockeru</li>
-          <li>Instalace Dockeru (pro ty kteří nenainstalovali doma)</li>
-          <li>Práce s kontejnery</li>
-          <li>Práce s obrazy</li>
-          <li>Docker Compose - vicekontejnerove aplikace v Dockeru</li>
-          <li>Deployment do Swarmu</li>
-          <li>Ukázka deploymentu do Kubernetes</li>
-        </ul>
+      <ReactMarkdown source={`
+- Obecný úvod do CI
+- Úvoď do Gitlab CI
+- Instalace Gitlab CI Runneru
+- Práce s CI joby
+- Tvorba CI pipelines
+- Automatické nasazování
+- Docker a CI
+        `} escapeHtml={false}/>
       </TextFrame>
       <TextFrame header="Pro koho je školení určeno">
-        Školení je primárně určeno lidem, kteří nemají s Dockerem žádné (nebo malé) zkušenosti. Školení nepočítá s předchozími znalostmi Dockeru. Je vhodné pro vývojáře nebo devops (server administrátory), kteří chtějí Docker využívat v praxi nebo chtějí mít přehled co Docker řeší a kde by jej mohli použít.
+        Školení je primárně určeno vývojářům kteří používají Gitlab a chtějí začít používat Gitlab CI. Zároveň právě Gitlab CI může být důvod přechodu na Gitlab. Školení je také vhodné pro DevOps, kteří si chtějí ulehčit práci s automatizací nasazování.
         <TwoCol
           col1={
             <>
               <h4>Předchozí znalosti</h4>
-              <ul>
-                <li>Základy práce s Linuxem</li>
-                <li>Základy práce v terminálu</li>
-              </ul>
+              <ReactMarkdown source={`
+- Základy práce s Linuxem
+- Základy práce v terminálu
+- Základní znalost Gitu výhodou - [Školení Git](https://ondrej-sika.cz/skoleni/git/)
+              `} escapeHtml={false}/>
             </>
           }
           col2={
             <>
               <h4>Technické požadavky</h4>
-              <ul>
-                <li>Nainstalovaný Docker,  <a href="https://www.ondrej-sika.cz/docker/instalace/">návod na instalaci</a></li>
-                <li>Přístup na internet - ideálně bez korporátní proxy, případně nastavenou proxy v prohlížeči</li>
-              </ul>
+              <ReactMarkdown source={`
+- Přístup na internet - ideálně bez korporátní proxy, případně nastavenou proxy v prohlížeči</li>
+              `} escapeHtml={false}/>
             </>
           }
         />
@@ -78,9 +79,9 @@ const Index = () => (
         header="Lektor: Ondřej Šika"
         imgUrl="/static/ondrejsika.jpg"
         >
-        Jmenuji se Ondřej Sika, vyvíjím software pro Slush Pool a školím. Školím převážně Docker a CI, který s Dockerem velmi úzce souvisí. Další školení naleznete na mém webu ondrej-sika.cz.
+        Jmenuji se Ondřej Sika, vyvíjím software pro Slush Pool a školím. Školím převážně Docker a CI, který s Dockerem velmi úzce souvisí. Další školení naleznete na mém webu <a href="https://ondrej-sika.cz">ondrej-sika.cz</a>.
        <br /><br />
-        Všechny kurzy vedu osobně. S Dockerem mám více než čtyři roky zkušeností v rámci komerčního vývoje softwaru. Docker používám denně jak ve vývoji, tak i na produkci a práci bez něj nedovedu už ani představit.
+        Všechny kurzy vedu osobně. S Gitlab CI mám zkušenosti z mnoha různých projektů a firem. Téměř všechny projekty na kterých pracuji využívají Gitlab CI. Gitlab CI mi velmi ulehčuje a zrychluje práci - nemusím se starat o rutijni akce jako nasazování nebo build aplikace a mohu se věnovat vývoji a činnostem které mi přinášejí zisk.
       </TextFrameWithImage>
 
       <MyClients HeaderClients="Moji nejvýznamější klienti"/>
@@ -109,19 +110,18 @@ const Index = () => (
       <TextFrame header="Mám zájem o školení">
         Pokud máte zájem o školení, neváhejte mě kontaktovat
         <ButtonOutline
-          btnUrl='https://ondrej-sika.cz/skoleni/docker/poptavka/'>
+          btnUrl='https://ondrej-sika.cz/skoleni/gitlab-ci/poptavka/'>
           Nezávazně poptat školení
         </ButtonOutline>
       </TextFrame>
 
       <TextFrame header="Cena">
-        Pokud máte zájem o školení u vás ve firmě, <a href="https://ondrej-sika.cz/skoleni/docker/poptavka/" className="btn btn-large mybutton-outline">nezávazně jej poptejte</a> nebo mi napište email na <a href="/cdn-cgi/l/email-protection#8de2e3e9ffe8e7cde2e3e9ffe8e7fee4e6eca3eee2e0">
-        <span className="__cf_email__" data-cfemail="2e41404a5c4b446e41404a5c4b445d47454f004d4143">ondrej@ondrejsika.com</span>
+        Pokud máte zájem o školení u vás ve firmě, <a href="https://ondrej-sika.cz/skoleni/gitlab-ci/poptavka/" className="btn btn-large mybutton-outline">nezávazně jej poptejte</a> nebo mi napište email na <a href="mailto:ondrej@ondrejsika.com">
         </a>
         <br/>
-        Otevřený termín (self funded): 9 999 CZK bez DPH<br />
-        Otevřený termín (company funded): 14 999 CZK bez DPH<br />
-        Firemní školení (ve firmě): 36 000 CZK bez DPH
+        Otevřený termín (self funded): 4 999 CZK bez DPH<br />
+        Otevřený termín (company funded): 6 999 CZK bez DPH<br />
+        Firemní školení (ve firmě): 18 000 CZK bez DPH
       </TextFrame>
       <Contact />
     </div>
