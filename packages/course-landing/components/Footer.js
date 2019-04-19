99edfa0cce8ea093a991962609f05eec77ee8f34
Zuzana Jeschkeová <zuzanajeschkeova@gmail.com>
2019-04-19 11:33:04 +0200

    skoleni-docker.cz - add gradient to header and footer

--- packages/course-landing/components/Footer.js
+++ packages/course-landing/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => (
   <div>
     <div className="container-fluid bg">
-      <div className="container bg">
+      <div className="container">
         <p className="text-center p-no-bottom pt-3 pb-3 text-white">2019 <a href="https://ondrej-sika.cz" className="text-white">Ondrej Sika</a></p>
       </div>
     </div>
