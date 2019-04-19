5514c023b607be8a798adc69c2f2457170c1f1e5
Ondrej Sika <ondrej@ondrejsika.com>
2019-04-19 21:56:11 +0200

    course-landing/Navigation - fix kubernetes link

--- packages/course-landing/components/Navigation.js
+++ packages/course-landing/components/Navigation.js
@@ -5,7 +5,7 @@ const Navigation = (props) => (
         <ul className='mt-2 mb-2'>
         <p>
           <li><a href='https://skoleni-docker.cz/' >Docker</a></li>
-          <li><a href='https://skoleni-kubetnetes.cz'>Kubernetes</a></li>
+          <li><a href='https://skoleni-kubernetes.cz'>Kubernetes</a></li>
           <li><a href='https://ondrej-sika.cz/skoleni/ansible'>Ansible</a></li>
           <li><a href='https://skoleni-git.cz/'>Git</a></li>
           <li><a href='https://gitlab-ci.cz/'>Gitlab CI</a></li>
