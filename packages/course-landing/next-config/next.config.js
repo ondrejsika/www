3df6543f7296c09c2bba375f4c822116244e7e14
Ondrej Sika <ondrej@ondrejsika.com>
2019-05-20 18:38:33 +0200

    course landing sites - move next.js config to one place and import it to each project. DRY <3

---
+++ packages/course-landing/next-config/next.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' }
+    }
+  },
+  webpack: function (config) {
+    config.module.rules.push(
+      {
+        test: /\.ya?ml$/,
+        use: 'js-yaml-loader',
+      },
+    )
+    return config
+  },
+  transpileModules: [
+    '@app',
+  ]
+}
+
+const withCSS = require('@zeit/next-css')
+module.exports = withCSS(module.exports)
+const withSass = require('@zeit/next-sass')
+module.exports = withSass(module.exports)
+const withTM = require('next-transpile-modules')
+module.exports = withTM(module.exports)
+const withImages = require('next-images')
+module.exports = withImages(module.exports)
--- packages/skoleni-git.cz/next.config.js
+++ packages/skoleni-git.cz/next.config.js
@@ -1,19 +1,2 @@
-module.exports = {
-  exportPathMap: function() {
-    return {
-      '/': { page: '/' }
-    }
-  },
-  transpileModules: [
-    '@app',
-  ]
-}
-
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS(module.exports)
-const withSass = require('@zeit/next-sass')
-module.exports = withSass(module.exports)
-const withTM = require('next-transpile-modules')
-module.exports = withTM(module.exports)
-const withImages = require('next-images')
-module.exports = withImages(module.exports)
+// Next.js config is same for all course landing pages, so we can import it ;)
+module.exports = require('../course-landing/next-config/next.config.js')
