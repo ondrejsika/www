6cdbb5c75f876355fe21eb8ab6f1217a906460b0
Ondrej Sika <ondrej@ondrejsika.com>
2019-05-28 07:53:36 +0200

    common - Add Gauges & GoogleAnaltics tracking code components (with example in archetype-web)

--- packages/archetype-web/config.js
+++ packages/archetype-web/config.js
@@ -2,6 +2,8 @@ const site = {
     former2_url: (process.env.NODE_ENV == 'development') ? 'https://former2-ond-local.xsika.cz' : 'https://former2-idontlikedolphins.xsika.cz',
     origin: (process.env.NODE_ENV == 'development') ? 'http://127.0.0.1:3001' : process.env.SITE_ORIGIN || 'https://archetype-web.xsika.cz',
     name: 'archetype',
+    gauges_site_id: 'GAUGES-XXX-DEMO',
+    google_analytics_site_id: 'GOOGLE-ANALYTICS-XXX-DEMO',
 }
 
 export default site
--- packages/archetype-web/pages/index.js
+++ packages/archetype-web/pages/index.js
@@ -8,17 +8,19 @@ import Translate from '@app/common/components/Translate'
 import BootstrapJS from '@app/common/components/BootstrapJS'
 import BootstrapComponentWithJS from '@app/archetype-theme/components/BootstrapComponentWithJS';
 import Former2 from '@app/common/components/Former2'
+import Gauges from '@app/common/components/Gauges'
 
 import jsonData from '../data/data.json'
 import yamlData from '../data/data.yml'
 
 import site from '@app/archetype-web/config'
+import GoogleAnalytics from '@app/common/components/GoogleAnalytics';
 
 const Index = () => (
   <div>
     <code style={{fontSize: '0.8em', color: 'gray'}}>
       {
-    
+
         Object.entries(site).map(function(el, i){
           return <span key={i}>{el[0] + ': ' + el[1]}, </span>
         })
@@ -52,6 +54,8 @@ const Index = () => (
     <DemoTable data={yamlData} />
     <Footer />
     <BootstrapJS />
+    <Gauges gauges_site_id={site.gauges_site_id} />
+    <GoogleAnalytics google_analytics_site_id={site.google_analytics_site_id} />
   </div>
 )
 
