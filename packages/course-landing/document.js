d28347080a1224abeeb6533a82ef8171051cf336
Ondrej Sika <ondrej@ondrejsika.com>
2019-05-30 16:16:54 +0200

    course-landing document - add charset & viewport

--- packages/course-landing/document.js
+++ packages/course-landing/document.js
@@ -27,6 +27,8 @@ class CourseLandingDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta charSet="utf-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         </Head>
         <body>
           <DevelopmentBar />
