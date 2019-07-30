4fd0a5aae256685b172fd6582cba79b676590beb
Ondrej Sika <ondrej@ondrejsika.com>
2019-07-30 09:05:26 +0200

    Use package `next-yaml`

--- packages/ondrej-sika.cz/next.config.js
+++ packages/ondrej-sika.cz/next.config.js
@@ -42,10 +42,6 @@ module.exports = {
     return buildExportPathMap(["/blog-post"]);
   },
   webpack: function(config) {
-    config.module.rules.push({
-      test: /\.ya?ml$/,
-      use: "js-yaml-loader"
-    });
     config.plugins.push(
       new CopyPlugin([
         {
@@ -69,9 +65,15 @@ module.exports = {
 
 const withCSS = require("@zeit/next-css");
 module.exports = withCSS(module.exports);
+
 const withSass = require("@zeit/next-sass");
 module.exports = withSass(module.exports);
+
 const withTM = require("next-transpile-modules");
 module.exports = withTM(module.exports);
+
 const withImages = require("next-images");
 module.exports = withImages(module.exports);
+
+const withYAML = require('next-yaml')
+module.exports = withYAML(module.exports)
--- packages/ondrejsikalabs.com/next.config.js
+++ packages/ondrejsikalabs.com/next.config.js
@@ -8,10 +8,6 @@ module.exports = {
     };
   },
   webpack: function(config) {
-    config.module.rules.push({
-      test: /\.ya?ml$/,
-      use: "js-yaml-loader"
-    });
     config.plugins.push(
       new CopyPlugin([
         {
@@ -35,9 +31,15 @@ module.exports = {
 
 const withCSS = require("@zeit/next-css");
 module.exports = withCSS(module.exports);
+
 const withSass = require("@zeit/next-sass");
 module.exports = withSass(module.exports);
+
 const withTM = require("next-transpile-modules");
 module.exports = withTM(module.exports);
+
 const withImages = require("next-images");
 module.exports = withImages(module.exports);
+
+const withYAML = require("next-images");
+module.exports = withYAML(module.exports);
