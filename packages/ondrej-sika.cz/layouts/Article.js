b70dad079ae34a2c7dec20d12e65c43d620153a0
Ondrej Sika <ondrej@ondrejsika.com>
2019-08-10 16:02:23 +0200

    ondrej-sika.cz - layouts - Create simple Article layout for Markdown pages

---
+++ packages/ondrej-sika.cz/layouts/Article.js
@@ -0,0 +1,20 @@
+import MainBar from "@app/ondrej-sika.cz/components/MainBar";
+import Markdown from "@app/common/components/Markdown";
+
+import Head from "next/head";
+
+export default props => (
+  <div>
+    <Head>
+      <title>{props.title} - Ondřej Šika</title>
+    </Head>
+    <MainBar MainBarHeader={props.title}></MainBar>
+
+    <div className="container pt-4 pb-2">
+      {props.children}
+      {(() => {
+        if (props.markdown) return <Markdown source={props.markdown} />;
+      })()}
+    </div>
+  </div>
+);
