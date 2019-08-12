d9633c1c4fd78544790e1cfb1e3c9d8cf8745e1d
Ondrej Sika <ondrej@ondrejsika.com>
2019-08-12 18:09:18 +0200

    ondrej-sika.cz/skoleni/react - fix missing props

--- packages/ondrej-sika.cz/pages/skoleni/react.js
+++ packages/ondrej-sika.cz/pages/skoleni/react.js
@@ -1,6 +1,6 @@
 import Course from "@app/ondrej-sika.cz/layouts/Course";
 
-export default () => (
+export default props => (
   <Course
     lang={props.site.lang}
     course_title="Školení React"
