fee7355454aeeddb5386560880ceeee3803fd107
Zuzana Jeschke <zuz@zuzjes.com>
2019-08-20 17:48:01 +0200

    Add new localization nl, ch, at to all courses in  navigation

--- packages/course-landing/components/Navigation.js
+++ packages/course-landing/components/Navigation.js
@@ -5,31 +5,46 @@ const links = {
     cz: "https://ondrej-sika.cz/skoleni/git?x_source=headline",
     de: "https://ondrej-sika.de/training/git?x_source=headline",
     uk: "https://ondrej-sika.uk/training/git?x_source=headline",
-    se: "https://ondrej-sika.se/utbildning/git?x_source=headline"
+    se: "https://ondrej-sika.se/utbildning/git?x_source=headline",
+    at: "https://ondrej-sika.com/training/git?x_source=headline",
+    ch: "https://ondrej-sika.com/training/git?x_source=headline",
+    nl: "https://ondrej-sika.com/training/git?x_source=headline"
   },
   gitlabci: {
     cz: "https://ondrej-sika.cz/skoleni/gitlab-ci?x_source=headline",
     de: "https://ondrej-sika.de/training/gitlab-ci?x_source=headline",
     uk: "https://ondrej-sika.uk/training/gitlab-ci?x_source=headline",
-    se: "https://ondrej-sika.se/utbildning/gitlab?x_source=headline"
+    se: "https://ondrej-sika.se/utbildning/gitlab?x_source=headline",
+    at: "https://ondrej-sika.com/training/gitlab-ci?x_source=headline",
+    ch: "https://ondrej-sika.com/training/gitlab-ci?x_source=headline",
+    nl: "https://ondrej-sika.com/training/gitlab-ci?x_source=headline"
   },
   docker: {
     cz: "https://ondrej-sika.cz/skoleni/docker?x_source=headline",
     de: "https://ondrej-sika.de/training/docker?x_source=headline",
     uk: "https://ondrej-sika.uk/training/docker?x_source=headline",
-    se: "https://ondrej-sika.se/utbildning/docker?x_source=headline"
+    se: "https://ondrej-sika.se/utbildning/docker?x_source=headline",
+    at: "https://ondrej-sika.com/training/docker?x_source=headline",
+    ch: "https://ondrej-sika.com/training/docker?x_source=headline",
+    nl: "https://ondrej-sika.com/training/docker?x_source=headline"
   },
   kubernetes: {
     cz: "https://ondrej-sika.cz/skoleni/kubernetes?x_source=headline",
     de: "https://ondrej-sika.de/training/kubernetes?x_source=headline",
     uk: "https://ondrej-sika.uk/training/kubernetes?x_source=headline",
-    se: "https://ondrej-sika.se/utbildning/kubernetes?x_source=headline"
+    se: "https://ondrej-sika.se/utbildning/kubernetes?x_source=headline",
+    at: "https://ondrej-sika.com/training/kubernetes?x_source=headline",
+    ch: "https://ondrej-sika.com/training/kubernetes?x_source=headline",
+    nl: "https://ondrej-sika.com/training/kubernetes?x_source=headline"
   },
   ansible: {
     cz: "https://ondrej-sika.cz/skoleni/ansible?x_source=headline",
     de: "https://ondrej-sika.de/training/ansible?x_source=headline",
     uk: "https://ondrej-sika.uk/training/ansible?x_source=headline",
-    se: "https://ondrej-sika.se/utbildning/ansible?x_source=headline"
+    se: "https://ondrej-sika.se/utbildning/ansible?x_source=headline",
+    at: "https://ondrej-sika.com/training/ansible?x_source=headline",
+    ch: "https://ondrej-sika.com/training/ansible?x_source=headline",
+    nl: "https://ondrej-sika.com/training/ansible?x_source=headline"
   }
 };
 
