3b35bd9bd04b6db6e8526fecfda54965dd683dcd
Ondrej Sika <ondrej@ondrejsika.com>
2019-09-03 16:43:23 +0200

    ondrej-sika.cz - Add new in progress articles

--- packages/ondrej-sika.cz/pages/clanky/index.js
+++ packages/ondrej-sika.cz/pages/clanky/index.js
@@ -7,7 +7,9 @@ export default () => (
 - [Nastroje, ktere pouzivam a zjednodusuji mi praci a zivot](/clanky/toolchain)
 - [Proc by kazdy mel mit svoji domenu](/clanky/proc-by-kazdy-mel-mit-svoji-domenu/)
 - [Proč mít email na vlastní doméně](/clanky/proc-mit-email-na-vlastni-domene)
+- [Primarni, Soukromy a firmenni email](/clanky/primarni-soukromy-a-firemni-email/)
 - [Vyber osobni wiki](/clanky/vyber-osobni-wiki)
+- [Digitalni Detox](/clanky/digitalni-detox/)
 `}
   />
 );
