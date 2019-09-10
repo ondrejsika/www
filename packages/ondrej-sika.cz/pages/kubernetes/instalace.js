9fc5679d3ba5f6d5f3d4518d06d267b7850ab1bf
Ondrej Sika <ondrej@ondrejsika.com>
2019-09-10 11:15:54 +0200

    ondrej-sika.cz/kubernetes/instalace - rewrite instructions from course repo

--- packages/ondrej-sika.cz/pages/kubernetes/instalace.js
+++ packages/ondrej-sika.cz/pages/kubernetes/instalace.js
@@ -4,82 +4,146 @@ export default () => (
   <Article
     title="Instalace Kubernetes pro lokalni vyvoj"
     markdown={`
-## kubectl
+Aby jste mohli pouzivat kubernetes lokalne, musite mit nainstalovane tyto nastroje:
 
-<https://kubernetes.io/docs/tasks/tools/install-kubectl/>
+- __kubectl__ - Kubernetes client [oficialni navod na instalaci](https://kubernetes.io/docs/tasks/tools/install-kubectl/)
+- __helm__ - Kubernetes package manager (neni nutny) [oficialni navod na instalaci](https://github.com/helm/helm/blob/master/docs/install.md)
+- __minikube__ - Nastoj na vytvareni lokalnich Kubernernes clusteru ve virtualnich strojich [oficialni navod na instalaci](https://kubernetes.io/docs/tasks/tools/install-minikube/#install-minikube)
 
-#### Linux
+## Mac
 
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
+### Kubectl on Mac
 
-#### macOS
+\`\`\`
+brew install kubernetes-cli
+\`\`\`
 
-    brew install kubernetes-cli
+Pokud nebude kubectl prikaz pracovat spravne, je mozne ze mate starou binarku z instalace Dockeru, timto chybu opravite
 
+\`\`\`
+rm /usr/local/bin/kubectl
+brew link --overwrite kubernetes-cli
+\`\`\`
 
-#### Windows
+### Helm on Mac
 
-Using Chocolatey
+\`\`\`
+brew install kubernetes-helm
+\`\`\`
 
-    choco install kubernetes-cli
+### Minikube on Mac
 
-## Minikube
+\`\`\`
+brew cask install minikube
+\`\`\`
 
-<https://kubernetes.io/docs/tasks/tools/install-minikube/#install-minikube>
+Pokud nemate VirtualBox, muzete jej nainstalovat prez Brew
 
-Minikube requires VirtualBox - <https://www.virtualbox.org/wiki/Downloads>
+\`\`\`
+brew cask install virtualbox
+\`\`\`
 
-#### Linux
+## Linux
 
-    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+### Kubectl on Linux
 
+\`\`\`
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
+\`\`\`
 
-#### macOS
+### Helm on Linux
 
-    brew cask install minikube
-
-If you dont have VirtualBox, you can install it using Brew.
-
-    brew cask install virtualbox
-
-#### Windows
-
-Using Chocolatey
-
-    choco install minikube
-
-
-#### Bash Completion
-
-    source <(kubectl completion bash)
-    source <(minikube completion bash)
-
-Or save to \.bashrc\`
-
-    echo "source <(kubectl completion bash)" >> ~/.bashrc
-    echo "source <(minikube completion bash)" >> ~/.bashrc
-
-## Helm
 
 Docs <https://github.com/helm/helm/blob/master/docs/install.md>
 
-Or oneliner for Linux:
+Pomoci package manageru snap:
 
-    curl https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash
+\`\`\`
+sudo snap install helm --classic
+\`\`\`
 
-on Mac:
+Nebo:
 
-    brew install kubernetes-helm
+\`\`\`
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash
+\`\`\`
+
+### Minikube on Linux (Linux on host)
+
+Potrebujete [Virtual Box](https://www.virtualbox.org/wiki/Downloads)
+
+\`\`\`
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+\`\`\`
+
+### Minikube on Linux (Linux in virtual machine)
+
+Pokud instalujete Kubernetes do virtualniho serveru, nepotrebujete (nemuzete) pouzit VirtualBox, muzete specifikovat VM driver none pri vytvareni Kubernetes clusteru.
+
+\`\`\`
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+\`\`\`
+
+Spustte takto:
+
+\`\`\`
+minikube start --vm-driver=none
+\`\`\`
+
+#### Microk8s.io on Linux (Linux in virtual machine)
+
+Pokud pouzivate Ubuntu (nebo jiny Linux s package managerem __snap__), muzete pouzit [microk8s.io](https://microk8s.io)
+
+\`\`\`
+sudo snap install microk8s --classic
+\`\`\`
+
+Vice informaci na <https://microk8s.io/docs/>
+
+### k3s (minimalistic kubernetes for Linux)
+
+Pokud nemuzete spustit __minikube__ (s VirtualBoxem nebo VM driverem none) nebo __microk8s__, muzete zkusit __k3s__.
+
+Vice informaci na <https://k3s.io/>
 
 
-and on Windows:
+## Windows
 
-    choco install kubernetes-helm
+### Kubectl for Windows
 
+\`\`\`
+choco install kubernetes-cli
+\`\`\`
 
-#### Bash Completion
+### Helm for Windows
 
-    source <(helm completion bash)
+\`\`\`
+choco install kubernetes-helm
+\`\`\`
+
+### Minikube for Windows
+
+\`\`\`
+choco install minikube
+\`\`\`
+
+## Bash Completion
+
+\`\`\`
+source <(kubectl completion bash)
+source <(minikube completion bash)
+source <(helm completion bash)
+\`\`\`
+
+Nebo ulozit do  \`.bashrc\`
+
+\`\`\`
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+echo "source <(minikube completion bash)" >> ~/.bashrc
+echo "source <(helm completion bash)" >> ~/.bashrc
+\`\`\`
+
+Take funguje pro __zsh__, napriklad: \`source <(kubectl completion zsh)\`
 
 `}
   />
