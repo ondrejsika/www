38c0d485330290e20a3a4fcb8b728be0e8793ea4
Ondrej Sika <ondrej@ondrejsika.com>
2019-09-11 10:37:48 +0200

    ondrej-sika.cz/ - add page prepare for {docker,kubernetes} training

---
+++ packages/ondrej-sika.cz/pages/skoleni/docker/priprava-na-skoleni.js
@@ -0,0 +1,10 @@
+import Article from "@app/ondrej-sika.cz/layouts/Article";
+
+export default () => (
+  <Article
+    title="Priprava na Skoleni Dockeru"
+    markdown={`
+Na skoleni Dockeru je potreba mit nainstalovany Docker, navod na instalaci najdete v clanku [Instalace Dockeru](/docker/instalace)
+`}
+  />
+);
