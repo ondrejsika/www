102389df7f8f70d42b9a116c4db278cf0457475a
Ondrej Sika <ondrej@ondrejsika.com>
2019-09-19 18:10:19 +0200

    ondrej-sika.cz - rewite inhouse inguiry pages

--- packages/ondrej-sika.cz/pages/skoleni/ansible/poptavka.js
+++ packages/ondrej-sika.cz/pages/skoleni/ansible/poptavka.js
@@ -1,36 +1,3 @@
-import Header from "@app/ondrej-sika.cz/components/Header";
-import TextArea from "@app/ondrej-sika.cz/components/TextArea";
-import Form from "@app/ondrej-sika.cz/components/Form";
+import InquiryInhouse from "../../../layouts/InquiryInhouse";
 
-import Head from "next/head";
-
-const Index = () => (
-  <div>
-    <Head>
-      <title>Poptávka Školení Ansible - Ondřej Šika</title>
-    </Head>
-    <Header
-      websiteHeader="Poptávka Školení Ansible"
-      inquiryBtn="Nezávazně poptat školení"
-      logo="/static/ansible.png"
-    ></Header>
-    <div className="container course-page">
-      <TextArea TextParagraph="Pole označené * jsou povinné.">
-        <Form
-          course="Ansible"
-          FormName="Jméno *"
-          FromNamePlaceholder="Anton Hrabiš"
-          FormCompany="Společnost"
-          FormCompanyPlaceholder="Drůbežárny"
-          FormEmail="Email *"
-          FormEmailPlaceholder="anton@hrabis.cz"
-          FormPhone="Telefon *"
-          FormPhonePlaceholder="+420111222333"
-          FormSent="Odeslat"
-        />
-      </TextArea>
-    </div>
-  </div>
-);
-
-export default Index;
+export default () => <InquiryInhouse course_name="Skoleni Ansible" />;
