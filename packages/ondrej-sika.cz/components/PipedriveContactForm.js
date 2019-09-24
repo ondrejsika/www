776e4e33c3881b116d6b0616865a80a3bc6da4a6
Ondrej Sika <ondrej@ondrejsika.com>
2019-09-24 17:42:06 +0200

    PipedriveContactForm - temporary replace for by link to form's page

--- packages/ondrej-sika.cz/components/PipedriveContactForm.js
+++ packages/ondrej-sika.cz/components/PipedriveContactForm.js
@@ -3,19 +3,14 @@ import React from "react";
 export default class PipedriveContactForm extends React.Component {
   render() {
     return (
-      <div className="text-center">
-        <div
-          class="pipedriveWebForms"
-          data-pd-webforms="https://pipedrivewebforms.com/form/f504126e0fb134359eccc29fa2a22bf16899125"
-        ></div>
+      <div className="text-center mb-5" id="form">
+        <a
+          href="https://pipedrivewebforms.com/form/f504126e0fb134359eccc29fa2a22bf16899125"
+          className="btn btn-large btn-primary"
+        >
+          Mám zájem o firmemní školení
+        </a>
       </div>
     );
-  }
-  componentDidMount() {
-    const script = document.createElement("script");
-    script.src =
-      "https://cdn.pipedriveassets.com/web-form-assets/webforms.min.js";
-    script.async = true;
-    document.body.appendChild(script);
   }
 }
