f69e2c344fc9732f6620c2c21f25109416e570d4
Ondrej Sika <ondrej@ondrejsika.com>
2019-09-28 23:45:31 +0200

    PipedriveContactForm - use just iframe, I think it works

--- packages/ondrej-sika.cz/components/PipedriveContactForm.js
+++ packages/ondrej-sika.cz/components/PipedriveContactForm.js
@@ -4,12 +4,13 @@ export default class PipedriveContactForm extends React.Component {
   render() {
     return (
       <div className="text-center mb-5" id="form">
-        <a
-          href="https://pipedrivewebforms.com/form/f504126e0fb134359eccc29fa2a22bf16899125"
-          className="btn btn-large btn-primary"
-        >
-          Mám zájem o firmemní školení
-        </a>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+          <iframe src="https://pipedrivewebforms.com/form/f504126e0fb134359eccc29fa2a22bf16899125?embeded=1&amp" scrolling="no" style="border: none; overflow: hidden; width: 100%; max-width: 768px; min-width: 320px; height: 1319px; position: relative;"></iframe>
+        `
+          }}
+        />
       </div>
     );
   }
