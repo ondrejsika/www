ba77120c96f2cf26481faa6ba99d65211547fdec
Ondrej Sika <ondrej@ondrejsika.com>
2019-10-04 16:57:10 +0200

    ondrej-sika.cz/jobs - add junior position & little update

--- packages/ondrej-sika.cz/pages/jobs.js
+++ packages/ondrej-sika.cz/pages/jobs.js
@@ -32,6 +32,9 @@ const Install = () => (
         source={`
 FAQ: Je tato nabidka aktualni? Ano.
 
+- [__Konzultant a Skolitel DevOps__](#hledam-partaka-na-skoleni-a-konzultace-devops) HOT!
+- [Technicky asistent & Junior DevOps Engineer](#technicky-asistent---junior-devops-engineer)
+
 ## Hledam partaka na skoleni a konzultace DevOps
 
 Shanim nekoho kdo mi pomuze se skolenimi a workshopy na DevOps temata. Skolim a konzultuji tyto nastroje a praci s nimi.
@@ -53,23 +56,53 @@ Moji klienti jsou startupy i korporace. Napriklad O2, AirBank, Satoshi Labs, E.O
 ### Nabizim
 
 - Plat az 13 000 CZK za jeden skolici den
-- Moznost rustu
+- Moznost rustu, budeme partneri ne zamestnanec vs zamestnavatel
 - Prace na zajimavych projektech
-- Maly (punk) team
+- Maly (punkovy) team
 
 ### Pozadavky
 
 - Prakticke zkusenosti s nastroji, ktere budes skolit
-- Motivace a chut se ucit nove veci ohledne IT a DevOps.
+- Motivace a chut se ucit nove veci ohledne IT a DevOps
 - Cestina, Anglictina
 - Samostatnost
 
 ### Vyhodou
 
 - Praha – obcas se budem muset potkat osobne
+- Zkusenosti s prednasenim / lektorovanim
+
+
+Pokud te nabidka zaujala, napis mi email na <ondrej@ondrejsika.com>. Napada te nekdo kdo by mel zajem? Doporuc mu prosim tuto nabidku, diky.
+
+
+## Technicky asistent & Junior DevOps Engineer
+
+Zajimas se o DevOps, ale nemas jeste moc zkusenosti? Nevadi, pojd nejake ziskat.
+
+Shanim nekoho kdo mi pomuze s:
+
+- Implementaci DevOps pro klienty
+- Pripravou skoleni
+- Sprava promo webu ([ondrej-sika.cz](https://ondrej-sika.cz), [skoleni-docker.cz](https://skoleni-docker.cz), ...)
+
+### Nabizim
+
+- Moznost ucit se od nejlepsich
+- Prace na zajimavych projektech
+
+### Pozaduji
+
+- Chut se ucit nove veci
+- Zajem o DevOps
+
+### Vyhodou
+
+- Praha – obcas se budem muset potkat osobne
 - Student IT (CVUT)
 
-Pokud te nabidka zaujala, napis mi email na <ondrej@ondrejsika.com>
+Pokud te nabidka zaujala, napis mi email na <ondrej@ondrejsika.com>. Napada te nekdo kdo by mel zajem? Doporuc mu prosim tuto nabidku, diky.
+
 `}
       />
     </div>
