59a4511416b122cccc600ccd0babfc7cf8d9b34b
Ondrej Sika <ondrej@ondrejsika.com>
2019-10-10 19:34:11 +0200

    ondrej-sika.cz - add contact to Trainera s.r.o.

--- packages/ondrej-sika.cz/pages/kontakt-trainera.js
+++ packages/ondrej-sika.cz/pages/kontakt-trainera.js
@@ -5,7 +5,7 @@ import PipedriveContactForm from "@app/ondrej-sika.cz/components/PipedriveContac
 const Index = props => (
   <div>
     <Head>
-      <title>Kontakty - Ondrej Sika</title>
+      <title>Kontakty - Trainera s.r.o.</title>
     </Head>
     <div className="bar-header">
       <div className="container">
@@ -15,30 +15,26 @@ const Index = props => (
       </div>
     </div>
     <div className="container">
-      <h3 className="pt-4 pb-2">Ondřej Šika</h3>
+      <h3 className="pt-4 pb-2">Trainera s.r.o.</h3>
       <div className="row">
         <div className="col-lg-4 col-md-6 col-sm-12 col-12">
           <p className="">
-            <a href="tel:+420773452376">+420 773 452 376</a> (
-            <a href="https://t.me/ondrejsika">Telegram</a>)
-            <br />
-            <a href="mailto:ondrej@sika.io?subject=Poptavka skoleni">
-              ondrej@sika.io
-            </a>{" "}
-            (<a href="/pgp">PGP</a>)
+            <a href="mailto:deals@trainera.io?subject=Poptavka skoleni">
+              deals@trainera.io
+            </a>
           </p>
           <p className="pb-4">
-            <a href="https://linkedin.com/in/ondrejsika">
-              linkedin.com/in/ondrejsika
+            <a href="https://www.linkedin.com/company/traineraio">
+              linkedin.com/company/traineraio
             </a>
           </p>
         </div>
         <div className="col-lg-4 col-md-6 col-sm-12 col-12">
           <h4>Fakturační údaje</h4>
           <p>
-            Klatovská třída 1532/71
+            Bělehradská 858/23
             <br />
-            301 00 Plzeň
+            120 00 Praha 2
             <br />
             Česká Republika
           </p>
@@ -46,15 +42,15 @@ const Index = props => (
             <tbody>
               <tr>
                 <td>IČ:</td>
-                <td>88114163</td>
+                <td>08591491</td>
               </tr>
               <tr>
                 <td>DIČ:</td>
-                <td>CZ9302252102</td>
+                <td>CZ08591491 (neplatce DPH)</td>
               </tr>
               <tr>
                 <td>Účet:&nbsp;&nbsp;&nbsp;</td>
-                <td>2301432033/2010 (Fio)</td>
+                <td>???/2010 (Fio)</td>
               </tr>
             </tbody>
           </table>
@@ -65,15 +61,15 @@ const Index = props => (
             <tbody>
               <tr>
                 <td>ČR (CZK):</td>
-                <td>2301432033 / 2010</td>
+                <td>??? / 2010</td>
               </tr>
               <tr>
                 <td>SK (EUR):</td>
-                <td>2301432033 / 8330</td>
+                <td>??? / 8330</td>
               </tr>
               <tr>
                 <td>IBAN:</td>
-                <td>CZ0620100000002301432033</td>
+                <td>???</td>
               </tr>
               <tr>
                 <td>BIC / SWIFT:</td>
@@ -99,8 +95,8 @@ const Index = props => (
       </div>
       <p>
         Kontakt na{" "}
-        <Link href="/kontakt-trainera">
-          <a>Trainera s.r.o.</a>
+        <Link href="/kontakt">
+          <a>Ondřej Šika</a>
         </Link>
       </p>
       <PipedriveContactForm lang={props.lang} />
