c328ed503412df7ab663cf9f10e3dfdd304e9f73
Ondrej Sika <ondrej@ondrejsika.com>
2019-10-11 13:33:42 +0200

    trainera.io - add contact & some info to homepage

--- packages/trainera.io/pages/index.js
+++ packages/trainera.io/pages/index.js
@@ -5,12 +5,38 @@ const Index = () => (
     <Head>
       <title>Trainera.io</title>
     </Head>
-    <h2>O nas</h2>
-    <p>In progress ...</p>
-    <h2>Co nabizime</h2>
-    <p>In progress ...</p>
-    <h2>Kontakt</h2>
-    <p>In progress ...</p>
+    <div className="container mt-4">
+      <h2>O nás</h2>
+      <p>
+        Nabízíme školení a konzultace v DevOps. Primárně Git, Gitlab CI, Docker,
+        Kubernetes, Terraform, Ansible, ELK/EFK, ...
+      </p>
+      <h2>Kontakt</h2>
+      <p>
+        Poptávky služeb školení a konzultace posílejte na{" "}
+        <a href="mailto:deals@trainera.io" style={{ fontWeight: "bold" }}>
+          deals@trainera.io
+        </a>
+      </p>
+      <p>
+        Kontakt na vedení firmy je{" "}
+        <a href="mailto:trainera@trainera.io">trainera@trainera.io</a>
+      </p>
+      <h3>Fakturační údaje</h3>
+      <p>
+        Bělehradská 858/23
+        <br />
+        120 00 Praha 2<br />
+        Česká Republika
+      </p>
+      <p>
+        IČ: 08591491
+        <br />
+        DIČ: CZ08591491 (neplaátce DPH)
+      </p>
+      <h3>Platební údaje</h3>
+      <p>Bankovní účet: 2801705982 / 2010 (Fio)</p>
+    </div>
   </div>
 );
 
