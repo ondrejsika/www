53d9d208d6f09eff9f436e15e0dbc25d693df2c6
Ondrej Sika <ondrej@ondrejsika.com>
2019-10-16 06:50:01 +0200

    ondrej-sika.cz/skoleni/terraform - Add write course info, remove course in progres

--- packages/ondrej-sika.cz/pages/skoleni/terraform.js
+++ packages/ondrej-sika.cz/pages/skoleni/terraform.js
@@ -8,11 +8,47 @@ export default props => (
     price_self_funded="4 999 CZK"
     price_company_funded="7 999 CZK"
     price_in_house="25 000 CZK"
-  >
-    <div className="alert alert-danger mt-5" role="alert">
-      <h2>Pripravuji ...</h2>
-      Na kurzu momentalne pracuji, pokud mate zajem, napiste mi email nebo
-      vyplnte poptavku. Ozvu se Vam.
-    </div>
-  </Course>
+    description={`
+### Co to je Terraform a k čemu se používá?
+
+Terraform je nástroj, který se používá ke správě infrastruktury v cloudu, jak veřejném (AWS, Azure, GCP), tak privátním (OpenStack, VMware vSphere).
+
+Terraform Vám umožní __spravovat infrastruktutru jako kód__, to znamená verzovanou v Gitu, přidávání resources pomocí merge (pull) requestů a mnoho dalších výhod. Infrastrukturu můžete dále nasadit do testovacího prostředé přesně tak, jako je v produkci bez zbytečného úsilí. O všech komponentách máte přehled a můžete predikovat ceny. Konec klikání infrastruktury, zkuste Terraform.
+
+Terraform umožnuje spravat více cloudových providerů současne, zároveň umožnuje pracovat s hibridními cloudy (část v privátním cloudu, část ve veřejném).
+
+### Jak školení probíhá
+
+Školení je formou workshopu, všechno si podrobně vyzkoušíme a vysvětlíme. Ukážeme si jak Terraform pracuje, jak jej používat v teamu a jak jím spravovat infrastukturu. Uděláme si příklad infrastruktury jednoduché aplikace, kde nastavíme vše - od DNS, přez Kubernetes cluster po S3 file storage.
+
+### Co Vás naučím
+
+- Základní koncept Terraformu
+- Výhody Terraformu proti Ansible, Puppetu, ...
+- Konfigurace Terraformu pro práci v teamu
+- Ovládání Terraform CLI
+- Úvod do Terraform Configuration Language (HCL)
+- Terraform Providers - napojení na cloudy
+- Práce s resources v Terraformu
+- Terraform moduly
+- Best Practice
+
+### Pro koho je školení určeno
+
+Školení je určeno pro DevOps a adminy, kteří chtějí lépe a efektivněji spravovat infrastrukturu. Zároveň také pro vývojáře, kteří se starají o nejakou infrastrukturu v cloudech.
+    `}
+    col1={`
+#### Předchozí znalosti
+
+- Základní znalost veřejných cloudů (AWS, Digital Ocean, Azure, ...)
+- Základy práce v terminálu
+`}
+    col2={`
+#### Technické požadavky
+
+- [Nainstalovaný Terraform](/terraform/instalace)
+- Přístup na internet (ideálně bez korporatni proxy)
+- Vlastní účet v Digital Oceanu výhodou - [Registrace + 50 USD kredit](https://sika.link/do-ref)
+`}
+  />
 );
