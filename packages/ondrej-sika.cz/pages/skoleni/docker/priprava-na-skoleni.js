26fc407e0af6f9651c1321c415f2a01aac84b9ee
Ondrej Sika <ondrej@ondrejsika.com>
2019-10-23 17:30:38 +0200

    ondrej-sika.cz/skoleni/docker/priprava-na-skoleni - update article (add requrements, add diacritic)

--- packages/ondrej-sika.cz/pages/skoleni/docker/priprava-na-skoleni.js
+++ packages/ondrej-sika.cz/pages/skoleni/docker/priprava-na-skoleni.js
@@ -2,9 +2,17 @@ import Article from "@app/ondrej-sika.cz/layouts/Article";
 
 export default () => (
   <Article
-    title="Priprava na Skoleni Dockeru"
+    title="Příprava na Školení Dockeru"
     markdown={`
-Na skoleni Dockeru je potreba mit nainstalovany Docker, navod na instalaci najdete v clanku [Instalace Dockeru](/docker/instalace)
+Díky za Váš zájem o školení Dockeru.
+
+Na školení Dockeru potřebujete
+
+- Vlastní notebook
+- Nainstalovaný Docker - [návod na instalaci](/docker/instalace)
+- Přístup na internet ideálně __bez korporátní proxy__, případne nastavenou proxy v Docker Desktop (na Mac stačí mít nastavenou systémovou proxy)
+
+Pokud budete mít jakýkoliv problém nebo dotaz, neváhejte napsat na <ondrej@sika.io>
 `}
   />
 );
