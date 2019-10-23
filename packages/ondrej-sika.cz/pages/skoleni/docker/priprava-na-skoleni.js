2e4837c1268231aab585796c0bcb243af2f6bf5a
Ondrej Sika <ondrej@ondrejsika.com>
2019-10-23 17:57:55 +0200

    ondrej-sika.cz/skoleni/docker/priprava-na-skoleni - add info about docker pull at home

--- packages/ondrej-sika.cz/pages/skoleni/docker/priprava-na-skoleni.js
+++ packages/ondrej-sika.cz/pages/skoleni/docker/priprava-na-skoleni.js
@@ -12,6 +12,8 @@ Na školení Dockeru potřebujete
 - Nainstalovaný Docker - [návod na instalaci](/docker/instalace)
 - Přístup na internet ideálně __bez korporátní proxy__, případne nastavenou proxy v Docker Desktop (na Mac stačí mít nastavenou systémovou proxy)
 
+Doporučuji ješte doma (nebo na dobrém internetu) stáhnout Docker images, které budeme používat. Je to cca 5 GB a občas to bývá při stahování na školení na wifi hodně. Na tomto odkazu je seznam příkazů jak je stáhnout: <https://gist.github.com/ondrejsika/e9748d49120cb1a656d39558ad379f45>
+
 Pokud budete mít jakýkoliv problém nebo dotaz, neváhejte napsat na <ondrej@sika.io>
 `}
   />
