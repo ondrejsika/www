45cd6b503ba17d2f52432a26bc0d3762cfbb6fef
Ondrej Sika <ondrej@ondrejsika.com>
2019-10-29 11:09:10 +0100

    Docker - add redirects.txt support

---
+++ ci/docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+touch /usr/share/nginx/html/redirects.txt
+
+echo "" > /etc/nginx/redirects.conf
+echo "map_hash_bucket_size 128;" >> /etc/nginx/redirects.conf
+echo "map \$request_uri \$redirect_uri {" >> /etc/nginx/redirects.conf
+
+while read line; do
+  [ -z "$line" ] && continue
+  echo "$line;" >> /etc/nginx/redirects.conf
+done </usr/share/nginx/html/redirects.txt
+
+echo "}" >> /etc/nginx/redirects.conf
+
+exec "$@"
