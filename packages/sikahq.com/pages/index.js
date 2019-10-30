d9b74b41944d28bb0356d31806b0e651ae6d0994
Ondrej Sika <ondrej@ondrejsika.com>
2019-10-30 16:15:29 +0100

    Add site sikahq.com

---
+++ packages/sikahq.com/pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const Index = () => (
+  <div>
+    <Head>
+      <title>SikaHQ.com</title>
+    </Head>
+    <div className="container mt-4">
+      <h1>
+        Ondrej Sika's team for{" "}
+        <a href="https://which.sika.io">freelance training, consulting</a> and
+        other projects.
+      </h1>
+    </div>
+  </div>
+);
+
+export default Index;
