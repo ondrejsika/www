893ceb004a56e3c3b22565fc1b93e6e389df0b8a
Ondrej Sika <ondrej@ondrejsika.com>
2019-11-01 17:21:46 +0100

    ondrej-sika.cz/skoleni/git - enable actual sessions

--- packages/ondrej-sika.cz/pages/skoleni/git.js
+++ packages/ondrej-sika.cz/pages/skoleni/git.js
@@ -4,7 +4,7 @@ export default props => (
   <Course
     lang={props.site.lang}
     course_id="git"
-    // show_sessions={true}
+    show_sessions={true}
     course_title="Školení Gitu"
     logo="/static/git.svg"
     price_self_funded="5 999 CZK"
