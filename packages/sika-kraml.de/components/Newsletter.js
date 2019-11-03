73b039e2e94f89e94084e196605f3990b4599cd3
Ondrej Sika <ondrej@ondrejsika.com>
2019-11-03 16:25:30 +0100

    sika-kraml.de - fix newsletter (remove form, add link to Google Form)

--- packages/sika-kraml.de/components/Newsletter.js
+++ packages/sika-kraml.de/components/Newsletter.js
@@ -1,40 +1,19 @@
-import Former2 from "@app/common/components/Former2";
-import site from "@app/sika-kraml.de/config";
-var color = {
-  background: "#4EA9A6"
-};
-
 const IntroBar = props => (
-  <div style={color}>
+  <div
+    style={{
+      background: "#4EA9A6"
+    }}
+  >
     <div className="container pt-4 pb-3 text-white">
       <div className="row">
         <div className="col-8">
           <h3 className="text-white">Bestellen Sie unser Newsletter!</h3>
           <p className="text-white">Seien Sie immer im Bild</p>
         </div>
-        <div className="col-4">
-          <div className="input-group pt-3">
-            <Former2
-              site={site}
-              name="Newsletter Subscription"
-              redirect_path="/odeslano"
-            >
-              <div className="input-group-append">
-                <input
-                  type="email"
-                  name="email"
-                  className="form-control"
-                  id="validationDefault03"
-                  placeholder="E-Mail"
-                />
-                <input
-                  type="submit"
-                  className="btn btn-outline-header"
-                  value={props.NewsletterBtn}
-                />
-              </div>
-            </Former2>
-          </div>
+        <div className="col-4 pt-3">
+          <a className="btn btn-lg btn-outline-header" target="_blank" href="https://sika.link/newsletter-sika-kraml">
+            Subscribe
+          </a>
         </div>
       </div>
     </div>
