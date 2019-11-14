3989165bdf9f79b97f20d2a5b5dd0b6692412da6
Ondrej Sika <ondrej@ondrejsika.com>
2019-11-14 09:02:47 +0100

    ondrej-sika.cz/clanky - add intro text

--- packages/ondrej-sika.cz/pages/clanky/index.js
+++ packages/ondrej-sika.cz/pages/clanky/index.js
@@ -4,6 +4,9 @@ export default () => (
   <Article
     title="Články"
     markdown={`
+
+  Zde jsou mě články a eseje na různá témata od osobního rozvoje, přez minimalismus po správu osobní IT ifrastruktury. Jsou určené pro široké publikum, ne jako zbytek mého webu, který je primárně určen pro lidí, který řeší vývoj a provozování software.
+
 - [Nastroje, ktere pouzivam a zjednodusuji mi praci a zivot](/clanky/toolchain)
 - [Proc by kazdy mel mit svoji domenu](/clanky/proc-by-kazdy-mel-mit-svoji-domenu/)
 - [Proč mít email na vlastní doméně](/clanky/proc-mit-email-na-vlastni-domene)
