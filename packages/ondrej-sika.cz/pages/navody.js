9e64f6daa2903617799c8590c0b6a217192a77c8
Ondrej Sika <ondrej@ondrejsika.com>
2019-11-14 08:56:49 +0100

    ondrej-sika.cz/navody - add Helm 3 blog post

--- packages/ondrej-sika.cz/pages/navody.js
+++ packages/ondrej-sika.cz/pages/navody.js
@@ -16,6 +16,7 @@ Pokud Vas zajima Docker, podivejte se na me [Skoleni Dockeru](/skoleni/docker)
 
 - [Instalace](/kubernetes/instalace)
 - [Priprava na skoleni Kubernetes](/skoleni/kubernetes/priprava-na-skoleni) - vice informaci o skoleni [zde](/skoleni/kubernetes)
+- [Novinky v Helm 3](/blog/helm3) (blog post)
 
 Pokud Vas zajima Kubernetes, podivejte se na me [Skoleni Kubernetes](/skoleni/kubernetes)
 
