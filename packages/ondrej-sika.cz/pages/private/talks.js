33527a29de9c2f0decd3c4dcf9744b2af75a55bb
Ondrej Sika <ondrej@ondrejsika.com>
2019-11-18 16:31:07 +0100

    ondrej-sika.cz/private/talks - add page with talka I can do

---
+++ packages/ondrej-sika.cz/pages/private/talks.js
@@ -0,0 +1,26 @@
+import Article from "@app/ondrej-sika.cz/layouts/Article";
+import talks from "@app/data/ondrejsika/talks.yml";
+
+export default props => (
+  <Article title="Talks, I Can Do on Your Event">
+    {talks.map(talk => (
+      <div>
+        <h2>{talk.title}</h2>
+        <h4>Bio</h4>
+        <p>{talk.bio}</p>
+        <h4>Perex</h4>
+        <p>{talk.perex}</p>
+        <p>
+          <a
+            href={`mailto:ondrej@sika.io?subject=[${props.site.name}] Ask for a Talk: ${talk.title}`}
+          >
+            I'm interested in {talk.title}
+          </a>
+        </p>
+        <hr
+          style={{ color: "black", borderTop: "1px solid", margin: "15px 0" }}
+        />
+      </div>
+    ))}
+  </Article>
+);
