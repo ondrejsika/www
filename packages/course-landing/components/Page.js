a6c5f7a5bc34ae5735b2030a2fb57201b394ba44
Ondrej Sika <ondrej@ondrejsika.com>
2019-11-22 16:01:14 +0100

    course-landing/components/Page - Create data driven Landing page layout

---
+++ packages/course-landing/components/Page.js
@@ -0,0 +1,217 @@
+import Header from "@app/course-landing/components/Header";
+import ButtonOutline from "@app/course-landing/components/ButtonOutline";
+import Bar from "@app/course-landing/components/Bar";
+import HeaderLink from "@app/course-landing/components/HeaderLink";
+import TextFrame from "@app/course-landing/components/TextFrame";
+import MyClients from "@app/course-landing/components/MyClients";
+import ThreeCol from "@app/course-landing/components/ThreeCol";
+import TwoCol from "@app/course-landing/components/TwoCol";
+import Navigation from "@app/course-landing/components/Navigation";
+import Recommendation from "@app/course-landing/components/Recommendation";
+import FooterOndrejSika from "@app/course-landing/components/FooterOndrejSika";
+import Head from "next/head";
+import Markdown from "@app/common/components/Markdown";
+import UpcomingSessions from "@app/course-landing/components/UpcomingSessions";
+import TwitterRecommendations from "@app/course-landing/components/TwitterRecommendations";
+import Translate from "@app/common/components/Translate";
+
+import courses_file from "@app/data/training/courses.yml";
+import StaticDB from "@app/common/staticdb";
+
+const Page = props => {
+  let site = props.site;
+  let lang = props.site.lang;
+  let location = props.site.location;
+
+  let db = new StaticDB();
+  db.add("courses", courses_file);
+  db.setCursor("courses");
+  db.filter("id", site.x_course);
+  let course = db.getOne();
+
+  let course_name = course.name[lang];
+  let price_open = course.price.open[location];
+  let price_in_house = course.price.in_house[location];
+  let twitter_recommendations = course.twitter_recommendations[lang];
+  let description = course.description[lang];
+  let recommendations = course.recommendations[lang];
+  return (
+    <div>
+      <Head>
+        <title>
+          {course_name} - {site.x_site_claim}
+        </title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <Navigation wide={true} location={site.location} />
+      <Header
+        wide={true}
+        websiteHeader={course_name}
+        byName="Ondrej Sika"
+        byUrl={`https://ondrej-sika.cz/?x_source=${site.name}`}
+        logoUrl={site.x_logo}
+      >
+        <HeaderLink url={site.x_inquiry_url}>
+          <Translate lang={site.lang} cs="Nezávazně poptat školení" />
+        </HeaderLink>
+        <HeaderLink url={site.x_contact_link}>
+          <Translate lang={site.lang} cs="Kontaktovat" />
+        </HeaderLink>
+      </Header>
+      <div className="container">
+        <h2>
+          <Translate lang={site.lang} cs="Otevřené ternímy" />
+        </h2>
+        <UpcomingSessions
+          site_name={site.name}
+          lang={site.lang}
+          course={site.x_course}
+          location={site.location}
+        />
+        <div className="row">
+          <div className="col-7">
+            <Markdown source={description} />
+          </div>
+          <div className="col-5">
+            <TextFrame>
+              <div className="text-center">
+                <img
+                  src="/static/ondrejsika.jpg"
+                  className="img-fluid rounded-circle mb-3"
+                  width="140"
+                />
+                <h4>Ondřej Šika</h4>
+              </div>
+              <p>
+                <Translate
+                  lang={site.lang}
+                  cs="Jsem freelance DevOps konzultant a lektor. Zlepšuji práci
+                vývojářských týmu zaváděním efektivních procesů ve vývoji.
+                Naučím Vás používat prověřené nástroje a technologie, které
+                povedou k rychlejšímu vývoji a bezpečnějšímu provozu vašeho
+                software."
+                />
+              </p>
+            </TextFrame>
+            <TextFrame>
+              <div className="">
+                <h3>
+                  <Translate lang={lang} cs="Mám zájem o školení" />
+                </h3>
+                <p>
+                  <Translate
+                    lang={lang}
+                    cs="Pokud máte zájem o školení, vyberte si variantu a pošlete
+                  nezávaznou poptávku."
+                  />
+                </p>
+                <div className="row text-center mt-4">
+                  <div className="col-6">
+                    <h4>
+                      <Translate lang={lang} cs="Otevřený termín" />
+                    </h4>
+                    <div className="mb-2" style={{ fontSize: "1.3em" }}>
+                      {price_open}
+                    </div>
+                    <ButtonOutline btnUrl={site.x_inquiry_url}>
+                      <Translate lang={lang} cs="Nezávazne poptat" />
+                    </ButtonOutline>
+                  </div>
+                  <div className="col-6">
+                    <h5>
+                      <Translate lang={lang} cs="Firemní školení" />
+                    </h5>
+                    <div className="mb-2" style={{ fontSize: "1.3em" }}>
+                      {price_in_house}
+                    </div>
+                    <ButtonOutline btnUrl={site.x_inquiry_url}>
+                      <Translate lang={lang} cs="Nezávazne poptat" />
+                    </ButtonOutline>
+                  </div>
+                </div>
+                <div className="mt-4">
+                  <h3>
+                    <Translate lang={lang} cs="Dotazy" />
+                  </h3>
+                  <Translate
+                    lang={lang}
+                    cs="Pokud máte jakýkoliv dotaz, naváhejte mi napsat na"
+                  />{" "}
+                  <a
+                    href={`mailto:ondrej@sika.io?subject=[${site.name}] Question about ${course_name}`}
+                  >
+                    ondrej@sika.io
+                  </a>
+                </div>
+              </div>
+            </TextFrame>
+            {/* <TextFrame>
+              <h3>Kontakt</h3>
+              <p>
+                <h4>Ondrej Sika</h4>
+                <a href="tel:+420773452376">+420 773 452 376</a>
+                <br /> <a href="mailto:ondrej@sika.io">ondrej@sika.io</a>
+              </p>
+              <p>
+                <a href="https://ondrej-sika.cz/kontakt">Fakturacni udaje</a>
+              </p>
+            </TextFrame> */}
+
+            {/* <Contact /> */}
+          </div>
+        </div>
+      </div>
+      <Bar>
+        <div className="text-center">
+          <h2 style={{ color: "white", fontSize: "2.4em" }}>
+            <Translate lang={lang} cs="Statistiky kurzu" />
+          </h2>
+        </div>
+        <div className="row">
+          <div className="col-4">
+            <h2 className="text-center text-white mb-0 text-huge">
+              {props.NumberOfPeople}150+
+            </h2>
+            <p className="text-white text-center statistic-bottom">
+              <Translate lang={lang} cs="účastníků proškoleno" />
+            </p>
+          </div>
+          <div className="col-4">
+            <h2 className="text-white text-center mb-0 text-huge ">
+              {props.NumberOfCompanies}15+
+            </h2>
+            <p className="text-white text-center statistic-bottom">
+              <Translate lang={lang} cs="firem proškoleno" />
+            </p>
+          </div>
+          <div className="col-4">
+            <h2 className="text-center text-white mb-0 text-huge">20+</h2>
+            <p className="text-white text-center statistic-bottom">
+              <Translate lang={lang} cs="běhů školení" />
+            </p>
+          </div>
+        </div>
+      </Bar>
+      <div className="container">
+        <MyClients
+          HeaderClients={
+            <Translate lang={lang} cs="Moji nejvýznamější klienti" />
+          }
+        />
+        <TwitterRecommendations ids={twitter_recommendations} />
+        <ThreeCol
+          header={<Translate lang={lang} cs="Reference" />}
+          col1={<Recommendation id={recommendations[0]} lang={site.lang} />}
+          col2={<Recommendation id={recommendations[1]} lang={site.lang} />}
+          col3={<Recommendation id={recommendations[2]} lang={site.lang} />}
+        />
+      </div>
+      <FooterOndrejSika />
+    </div>
+  );
+};
+
+export default Page;
