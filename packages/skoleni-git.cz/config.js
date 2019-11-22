3e0a77c1145c0668d395783f6725adacc8956f92
Ondrej Sika <ondrej@ondrejsika.com>
2019-11-22 17:23:40 +0100

    Migrate skoleni-git.cz to course-landing/Page

--- packages/skoleni-git.cz/config.js
+++ packages/skoleni-git.cz/config.js
@@ -1,8 +1,17 @@
+import logo from "@app/data/pictures/courses/git-w.svg";
+
 const site = {
   lang: "cs",
   location: "cz",
   gauges_site_id: "5a05820e4b9eb7101600f9ff",
-  google_analytics_site_id: "UA-39461955-5"
+  google_analytics_site_id: "UA-39461955-5",
+
+  x_course: "git",
+  x_site_claim: "Nejlepší skolení Gitu v ČR",
+  x_logo: logo,
+  x_contact_link: "https://ondrej-sika.cz/kontakt/?x_source=skoleni-git.cz",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/skoleni/git/poptavka/?x_source=skoleni-git.cz"
 };
 
 export default site;
