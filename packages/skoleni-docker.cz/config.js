b0fe62f7e541fe3b0e63b1a85c317dc535174386
Ondrej Sika <ondrej@ondrejsika.com>
2019-11-22 16:02:21 +0100

    skoleni-docker.cz - Migrate to data drive Landing page

--- packages/skoleni-docker.cz/config.js
+++ packages/skoleni-docker.cz/config.js
@@ -1,8 +1,18 @@
+import logo from "@app/data/pictures/courses/docker-w.svg";
+
 const site = {
+  name: "skoleni-docker.cz",
   lang: "cs",
   location: "cz",
   gauges_site_id: "5a0582e9bad3a73e7400e710",
-  google_analytics_site_id: "UA-39461955-7"
+  google_analytics_site_id: "UA-39461955-7",
+
+  x_course: "docker",
+  x_site_claim: "Nejlepší skolení Dockeru v ČR",
+  x_logo: logo,
+  x_contact_link: "https://ondrej-sika.cz/kontakt/?x_source=skoleni-docker.cz",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/skoleni/docker/poptavka/?x_source=skoleni-docker.cz"
 };
 
 export default site;
