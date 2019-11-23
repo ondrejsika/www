340dfb614db67a57576f43d237170e16b0a0b734
Ondrej Sika <ondrej@ondrejsika.com>
2019-11-23 09:43:32 +0100

    Migrate skoleni-kubernetes.cz to course-landing/Page

--- packages/skoleni-kubernetes.cz/config.js
+++ packages/skoleni-kubernetes.cz/config.js
@@ -1,8 +1,18 @@
+import logo from "@app/data/pictures/courses/kubernetes.png";
+
 const site = {
   lang: "cs",
   location: "cz",
   gauges_site_id: "5cefba676dcfe05a1102e239",
-  google_analytics_site_id: "UA-39461955-21"
+  google_analytics_site_id: "UA-39461955-21",
+
+  x_course: "kubernetes",
+  x_site_claim: "Nejlepší skolení Kubernetes v ČR",
+  x_logo: logo,
+  x_contact_link:
+    "https://ondrej-sika.cz/kontakt/?x_source=skoleni-kubernetes.cz",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/skoleni/kubernetes/poptavka/?x_source=skoleni-kubernetes.cz"
 };
 
 export default site;
