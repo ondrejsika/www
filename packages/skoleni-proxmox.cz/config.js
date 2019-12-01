de241db88d58deb6d80000ec1a96d92fb41448ae
Ondrej Sika <ondrej@ondrejsika.com>
2019-12-01 18:54:09 +0100

    add site skoleni-proxmox.cz

---
+++ packages/skoleni-proxmox.cz/config.js
@@ -0,0 +1,18 @@
+import logo from "@app/data/pictures/courses/proxmox-white.svg";
+
+const site = {
+  name: "skoleni-proxmox.cz",
+  lang: "cs",
+  location: "cz",
+  gauges_site_id: "???",
+  google_analytics_site_id: "???",
+
+  x_course: "proxmox",
+  x_site_claim: "Školení Proxmoxu v ČR",
+  x_logo: logo,
+  x_contact_link: "https://ondrej-sika.cz/kontakt/?x_source=skoleni-proxmox.cz",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/skoleni/proxmox?x_source=skoleni-proxmox.cz#form"
+};
+
+export default site;
