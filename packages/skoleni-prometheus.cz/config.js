3999240f8c0c09765aec2df642251c5aa6400dfb
Ondrej Sika <ondrej@ondrejsika.com>
2019-12-02 10:00:52 +0100

    Add site skoleni-prometheus.cz

---
+++ packages/skoleni-prometheus.cz/config.js
@@ -0,0 +1,19 @@
+import logo from "@app/data/pictures/courses/prometheus-white.png";
+
+const site = {
+  name: "skoleni-prometheus.cz",
+  lang: "cs",
+  location: "cz",
+  gauges_site_id: "???",
+  google_analytics_site_id: "???",
+
+  x_course: "prometheus",
+  x_site_claim: "Školení Promethea v ČR",
+  x_logo: logo,
+  x_contact_link:
+    "https://ondrej-sika.cz/kontakt/?x_source=skoleni-prometheus.cz",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/skoleni/prometheus?x_source=skoleni-prometheus.cz#form"
+};
+
+export default site;
---
+++ packages/skoleni-prometheus.cz/pages/index.js
@@ -0,0 +1,5 @@
+import Page from "@app/course-landing/components/Page";
+
+const Index = props => <Page site={props.site} />;
+
+export default Index;
