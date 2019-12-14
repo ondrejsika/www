1f1e97922c44222e6ea298e6760bddd194ec13ce
Ondrej Sika <ondrej@ondrejsika.com>
2019-12-14 22:16:26 +0100

    ansible-skoleni.cz - rewrite under new data driven template

--- packages/ansible-skoleni.cz/config.js
+++ packages/ansible-skoleni.cz/config.js
@@ -1,8 +1,18 @@
+import logo from "@app/data/pictures/courses/ansible.png";
+
 const site = {
+  name: "ansible-skoleni.cz",
   lang: "cs",
   location: "cz",
   gauges_site_id: "5cecd2020c3be10989c5bffc",
-  google_analytics_site_id: "UA-39461955-26"
+  google_analytics_site_id: "UA-39461955-26",
+
+  x_course: "ansible",
+  x_site_claim: "Školení Ansible v ČR",
+  x_logo: logo,
+  x_contact_link: "https://ondrej-sika.cz/kontakt/?x_source=ansible-skoleni.cz",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/skoleni/ansible/poptavka/?x_source=ansible-skoleni.cz"
 };
 
 export default site;
