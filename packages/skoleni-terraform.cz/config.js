d4bc4ccc41aa34e85b20a383d361868ba7cfb5ae
Ondrej Sika <ondrej@ondrejsika.com>
2019-12-14 22:49:27 +0100

    skoleni-terraform.cz - rewrite to data drive

--- packages/skoleni-terraform.cz/config.js
+++ packages/skoleni-terraform.cz/config.js
@@ -1,8 +1,18 @@
+import logo from "@app/data/pictures/courses/terraform-white.png";
+
 const site = {
   lang: "cs",
   location: "cz",
   gauges_site_id: "5da6a304c129fd782225c7da",
-  google_analytics_site_id: "UA-39461955-45"
+  google_analytics_site_id: "UA-39461955-45",
+
+  x_course: "terraform",
+  x_site_claim: "Nejlepší skolení Terraformu v ČR",
+  x_logo: logo,
+  x_contact_link:
+    "https://ondrej-sika.cz/kontakt/?x_source=skoleni-terraform.cz",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/skoleni/terraform?x_source=skoleni-terraform.cz#form"
 };
 
 export default site;
