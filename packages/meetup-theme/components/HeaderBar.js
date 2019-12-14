f20afd88174963fea0570a94cf21b2f6b8200f9d
Ondrej Sika <ondrej@ondrejsika.com>
2019-12-14 23:33:46 +0100

    meetup-theme - add Trainera to HeaderBar

--- packages/meetup-theme/components/HeaderBar.js
+++ packages/meetup-theme/components/HeaderBar.js
@@ -1,7 +1,8 @@
 export default props => (
   <div className="container headerbar">
     <h3>
-      Project of <a href="https://sika.io">Ondrej Sika</a>
+      Project of <a href="https://sika.io">Ondrej Sika</a> and{" "}
+      <a href="https://trainera.io">Trainera</a>
     </h3>
     <style jsx>{`
       .headerbar h3 {
