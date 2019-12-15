251a3e9aa6585a67b6e88cdfc01bbf696c6e7d6a
Zuzana Jeschke <zuz@zuzjes.com>
2019-12-15 12:09:04 +0100

    skolenie-docker.sk - rewrite to data driven

--- packages/skolenie-docker.sk/config.js
+++ packages/skolenie-docker.sk/config.js
@@ -1,8 +1,18 @@
+import logo from "@app/data/pictures/courses/docker-w.svg";
+
 const site = {
+  name: "skolenie-docker.sk",
   lang: "sk",
   location: "sk",
   gauges_site_id: "5cefbc8a6dcfe0579c02e25f",
-  google_analytics_site_id: "UA-39461955-40"
+  google_analytics_site_id: "UA-39461955-40",
+
+  x_course: "docker",
+  x_site_claim: "Najlepšie školenie Docker v SR",
+  x_logo: logo,
+  x_contact_link: "https://ondrej-sika.cz/kontakt/?x_source=skoleni-docker.cz",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/skoleni/docker/poptavka/?x_source=skoleni-docker.cz"
 };
 
 export default site;
