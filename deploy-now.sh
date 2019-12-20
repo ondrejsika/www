e0d6ffd213f08fd14895f3fead282d7ec582c3a0
Ondrej Sika <ondrej@ondrejsika.com>
2019-12-20 22:15:46 +0100

    deploy-now.sh - fix cleanup & print sod curl

--- deploy-now.sh
+++ deploy-now.sh
@@ -6,10 +6,15 @@ SITE=$1
 
 IMAGE=$CI_REGISTRY_IMAGE/now/$SITE
 
+rm -rf packages/$SITE/out packages/$SITE/.next
 yarn static-$SITE
 rm -rf ci/docker/out
 cp -r packages/$SITE/out ci/docker/out
 docker build -t $IMAGE ci/docker
-docker push $IMAGE
-curl -f "$SOD_URL/api/v1/deploy/docker/?image=$IMAGE&domain=$SITE&token=$SOD_TOKEN&registry=$CI_REGISTRY&registry_user=$CI_REGISTRY_USER&registry_password=$CI_REGISTRY_PASSWORD"
 rm -rf ci/docker/out
+docker push $IMAGE
+sod_curl_url="$SOD_URL/api/v1/deploy/docker/?image=$IMAGE&domain=$SITE&token=$SOD_TOKEN&registry=$CI_REGISTRY&registry_user=$CI_REGISTRY_USER&registry_password=$CI_REGISTRY_PASSWORD"
+sod_curl_cmd="curl -f $sod_curl_url"
+echo $sod_curl_cmd
+$sod_curl_cmd
+echo
