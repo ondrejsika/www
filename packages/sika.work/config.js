3255acd384401eec585d9efa2417be45fb1a3bf8
Ondrej Sika <ondrej@ondrejsika.com>
2019-12-23 21:20:26 +0100

    Add sika.work

---
+++ packages/sika.work/config.js
@@ -0,0 +1,7 @@
+const site = {
+  name: "sika.work",
+  gauges_site_id: "GAUGES-XXX-DEMO",
+  google_analytics_site_id: "GOOGLE-ANALYTICS-XXX-DEMO"
+};
+
+export default site;
---
+++ packages/sika.work/pages/index.js
@@ -0,0 +1,21 @@
+import "../css";
+import Head from "next/head";
+
+const Index = () => (
+  <>
+    <Head>
+      <title>sika.work</title>
+    </Head>
+    <div className="mt-4">
+      <h1>Ondrej Sika</h1>
+      <p>
+        <strong>Software &amp; DevOps Engineer</strong>
+      </p>
+      <p>
+        <a href="mailto:ondrej@sika.work">ondrej@sika.work</a>
+      </p>
+    </div>
+  </>
+);
+
+export default Index;
