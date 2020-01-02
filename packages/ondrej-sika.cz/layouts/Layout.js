acf5e47069fa24ba01a27244cb27db365549dbff
Ondrej Sika <ondrej@ondrejsika.com>
2020-01-02 13:29:35 +0100

    ondrej-sika.cz - add favicon

--- packages/ondrej-sika.cz/layouts/Layout.js
+++ packages/ondrej-sika.cz/layouts/Layout.js
@@ -15,6 +15,7 @@ export default props => (
     <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/png" href="/icon.png" />
       <link
         href="https://fonts.googleapis.com/css?family=Fira+Sans|Merriweather"
         rel="stylesheet"
