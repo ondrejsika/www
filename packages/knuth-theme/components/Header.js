7ce00cb5823c896815ed3bfac07e9c0d33b7f416
Ondrej Sika <ondrej@ondrejsika.com>
2020-01-04 21:54:47 +0100

    packages/knuth-theme - add simple theme for ondrejsika.io blog (using CMU TypeWriter)

---
+++ packages/knuth-theme/components/Header.js
@@ -0,0 +1,52 @@
+import Link from "next/link";
+
+export default props => {
+  let site_name = props.site_name;
+  let menu_links = props.menu_links;
+  return (
+    <header className="header">
+      <div className="site-name">
+        <span style={{ fontSize: "1.3em" }}>{site_name}</span>
+      </div>
+      <nav className="site-nav">
+        <ul>
+          {menu_links.map(link => (
+            <li
+              className="ml-4"
+              style={{ display: "inline-block", fontSize: "1.3em" }}
+            >
+              <Link href={link[1]}>
+                <a>{link[0]}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <style jsx>{`
+        ul {
+          display: block;
+          list-style-type: disc;
+          margin-block-start: 0px;
+          margin-block-end: 0px;
+          margin-inline-start: 0px;
+          margin-inline-end: 0px;
+          padding-inline-start: 0px;
+        }
+        .header {
+          display: flex;
+        }
+        .site-nav {
+          margin-left: auto;
+        }
+        li:first-of-type {
+          margin-left: 0px !important;
+        }
+        @media (max-width: 600px) {
+          .header {
+            display: block;
+          }
+        }
+      `}</style>
+    </header>
+  );
+};
---
+++ packages/knuth-theme/layouts/Layout.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Head from "next/head";
+
+// Imported CSS
+import "bootstrap-css-only/css/bootstrap.min.css";
+
+// Import Fonts
+import Font from "@app/computer-modern-typewriter-light";
+
+import Header from "../components/Header";
+
+export default props => {
+  let site_name = props.site_name;
+  let menu_links = props.menu_links;
+  let site_footer = props.site_footer;
+  return (
+    <div>
+      <Font />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <style jsx global>{`
+        * {
+          font-family: "Computer Modern Typewriter Light";
+        }
+      `}</style>
+      <div className="container">
+        <Header menu_links={menu_links} site_name={site_name} />
+
+        {props.children}
+        <p style={{ marginTop: "30px" }}>{site_footer}</p>
+      </div>
+    </div>
+  );
+};
