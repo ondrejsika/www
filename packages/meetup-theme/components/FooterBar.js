fddc69c9908d51851f7bbfc64c3baea637ca36d8
Ondrej Sika <ondrej@ondrejsika.com>
2020-01-09 16:37:56 +0100

    meetup-theme - Add component FooterBar

---
+++ packages/meetup-theme/components/FooterBar.js
@@ -0,0 +1,18 @@
+export default props => (
+  <div className="container footerbar text-center">
+    <p>
+      2019 - 2020 &copy; <strong>Salzburg DevOps Meetups</strong> by{" "}
+      <a
+        href={`https://sika.io/?utm_source=landingpage&utm_medium=meetups&utm_campaign=${props.site_name}&utm_content=footer`}
+      >
+        Ondrej Sika
+      </a>{" "}
+      and{" "}
+      <a
+        href={`https://trainera.io/?utm_source=landingpage&utm_medium=meetups&utm_campaign=${props.site_name}&utm_content=footer`}
+      >
+        Trainera
+      </a>
+    </p>
+  </div>
+);
