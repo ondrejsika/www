216cee1902a2b5c8fa4a2b7273a6c2f6494eef29
Ondrej Sika <ondrej@ondrejsika.com>
2020-01-10 16:53:15 +0100

    ondrej-sika.cz/layouts/Article - add newsletter & ad to article layout

--- packages/ondrej-sika.cz/layouts/Article.js
+++ packages/ondrej-sika.cz/layouts/Article.js
@@ -1,23 +1,39 @@
 import MainBar from "@app/ondrej-sika.cz/components/MainBar";
 import Markdown from "@app/common/components/Markdown";
+import AdBar from "@app/ondrej-sika.cz/components/AdBar";
+import NewsletterBar from "@app/ondrej-sika.cz/components/NewsletterBar";
 
 import Head from "next/head";
 
-export default props => (
-  <div>
-    <Head>
-      <title>{props.title} - Ondřej Šika</title>
-    </Head>
-    <MainBar MainBarHeader={props.header || props.title}></MainBar>
+export default props => {
+  return (
+    <div>
+      <Head>
+        <title>{props.title} - Ondřej Šika</title>
+      </Head>
+      <MainBar MainBarHeader={props.header || props.title}></MainBar>
 
-    <div className="container pt-4 pb-2 article-body">
-      {props.children}
-      {(() => {
-        if (props.markdown) return <Markdown source={props.markdown} />;
-      })()}
-      {(() => {
-        if (props.after) return props.after;
-      })()}
+      <div className="container pt-4 pb-2 article-body">
+        {props.children}
+        {(() => {
+          if (props.markdown) return <Markdown source={props.markdown} />;
+        })()}
+        {(() => {
+          if (props.after) return props.after;
+        })()}
+
+        {!props.ad && <NewsletterBar />}
+        {props.ad && (
+          <div class="row">
+            <div class="col-6">
+              <AdBar ad={props.ad} />
+            </div>
+            <div class="col-6">
+              <NewsletterBar />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
