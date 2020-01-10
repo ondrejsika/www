41600c5ca32a504bc30fd97e539045644cc6abff
Ondrej Sika <ondrej@ondrejsika.com>
2020-01-10 18:06:52 +0100

    ondrej-sika.cz/private/settings - add settings page for website

---
+++ packages/ondrej-sika.cz/pages/private/settings.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Head from "next/head";
+
+import { useCookie } from "@app/common/components/AustraliaOnFireBar/useCookie";
+
+let Bar = props => (
+  <div style={{ backgroundColor: props.color, padding: "15px", width: 100}}>
+    {props.children}
+  </div>
+);
+
+export default () => {
+  const [hideBar, setHideBar] = useCookie("AustraliaFiresBarhideBar");
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Website Settings - Ondřej Šika</title>
+      </Head>
+      <h1>Website Settings</h1>
+      <h2>Australia Fires Bar</h2>
+      {hideBar ? (
+        <Bar color="lightgray">off</Bar>
+      ) : (
+        <Bar color="lightgreen">on</Bar>
+      )}
+      <a href="#" onClick={() => setHideBar(true)}>
+        hide
+      </a>{` `}
+      <a href="#" onClick={() => setHideBar("")}>
+        show
+      </a>
+    </div>
+  );
+};
