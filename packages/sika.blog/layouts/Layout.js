8aad4dff143f5e5f91003141d81d3c01b0115c05
Ondrej Sika <ondrej@ondrejsika.com>
2020-01-15 18:45:43 +0100

    add sika.blog

---
+++ packages/sika.blog/layouts/Layout.js
@@ -0,0 +1,13 @@
+import Layout from "@app/bootstrap-theme/layouts/Layout";
+import Gauges from "@app/common/components/Gauges";
+import GoogleAnalytics from "@app/common/components/GoogleAnalytics";
+
+export default props => (
+  <Layout>
+    <div className="container">{props.children}</div>
+    <Gauges gauges_site_id={props.site.gauges_site_id} />
+    <GoogleAnalytics
+      google_analytics_site_id={props.site.google_analytics_site_id}
+    />
+  </Layout>
+);
---
+++ packages/sika.blog/pages/index.js
@@ -0,0 +1,23 @@
+import "../css";
+import Head from "next/head";
+
+const Index = () => (
+  <>
+    <Head>
+      <title>sika.blog</title>
+    </Head>
+    <div className="mt-4">
+      <h1>sika.blog</h1>
+      <p>
+        <a href="https://ondrej.sika.blog">ondrej.sika.blog</a>
+        <br />
+        <a href="https://knihy.sika.blog">knihy.sika.blog</a>
+        <br />
+        <a href="https://random.sika.blog">random.sika.blog</a>
+        <br />
+      </p>
+    </div>
+  </>
+);
+
+export default Index;
