7399d3d9bb05e18cdf1cf0b8b2c8f773a6111a96
Ondrej Sika <ondrej@ondrejsika.com>
2020-01-20 14:59:07 +0100

    Add static.sika.io (with ondrejsika3.jpg)

---
+++ packages/static.sika.io/pages/index.js
@@ -0,0 +1,24 @@
+import "../css";
+import Head from "next/head";
+import Markdown from "@app/common/components/Markdown";
+
+const Index = () => (
+  <>
+    <Head>
+      <title>static.sika.io</title>
+    </Head>
+    <div className="mt-4">
+      <h1>static.sika.io</h1>
+      <p>Static content for other wesbites or externals (conferences, ...).</p>
+      <h2>Files</h2>
+      <Markdown source={`
+- [ondrejsika3.jpg](/files/photos/ondrejsika3.jpg)
+- [ondrejsika3_200px.jpg](/files/photos/ondrejsika3_200px.jpg)
+- [ondrejsika3_32px.jpg](/files/photos/ondrejsika3_32px.jpg)
+- [ondrejsika3_32.png](/files/photos/ondrejsika3_32px.png)
+`} />
+    </div>
+  </>
+);
+
+export default Index;
