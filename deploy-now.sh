e8c2ff3dfc5c1451a3daea3043422c1f327573e6
Ondrej Sika <ondrej@ondrejsika.com>
2020-01-20 15:06:20 +0100

    deploy-now.sh - add pull parameter to docker build

--- deploy-now.sh
+++ deploy-now.sh
@@ -10,7 +10,7 @@ rm -rf packages/$SITE/out packages/$SITE/.next
 yarn static-$SITE
 rm -rf ci/docker/out
 cp -r packages/$SITE/out ci/docker/out
-docker build -t $IMAGE ci/docker
+docker build -t $IMAGE ci/docker --pull
 rm -rf ci/docker/out
 docker push $IMAGE
 sod_curl_url="$SOD_URL/api/v1/deploy/docker/?image=$IMAGE&domain=$SITE&token=$SOD_TOKEN&registry=$CI_REGISTRY&registry_user=$CI_REGISTRY_USER&registry_password=$CI_REGISTRY_PASSWORD"
