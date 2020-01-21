a303fd18d5e71ab38bd9a67f452883b84f3b7171
Ondrej Sika <ondrej@ondrejsika.com>
2020-01-21 06:10:32 +0100

    add deploy-now-k8s.sh script

---
+++ deploy-now-k8s.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+. deploy-now.env.sh
+
+SITE=$1
+NAME=$(echo $SITE | sed "s/\./-/g")
+
+IMAGE=$CI_REGISTRY_IMAGE-$SITE-deploy-now
+
+rm -rf packages/$SITE/out packages/$SITE/.next
+yarn static-$SITE
+rm -rf ci/docker/out
+cp -r packages/$SITE/out ci/docker/out
+docker build -t $IMAGE ci/docker --pull
+rm -rf ci/docker/out
+docker push $IMAGE
+ORIGINAL_KUBERNETES_CONTEXT=$(kubectl config current-context)
+kubectl config use-context $KUBERNETES_CONTEXT
+helm upgrade --install $NAME ondrejsika/one-image --set host=$SITE --set image=$IMAGE
+kubectl config use-context $ORIGINAL_KUBERNETES_CONTEXT
