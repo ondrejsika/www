cb6ed63183f3e0489bb1d41f4acc1a5aee60793a
Ondrej Sika <ondrej@ondrejsika.com>
2020-01-31 14:47:53 +0100

    talks,workshops.yml - Add proposals for Install Fest 2020

--- packages/ondrej-sika.cz/pages/private/talks.js
+++ packages/ondrej-sika.cz/pages/private/talks.js
@@ -1,8 +1,10 @@
 import Article from "@app/ondrej-sika.cz/layouts/Article";
 import talks from "@app/data/ondrejsika/talks.yml";
+import workshops from "@app/data/ondrejsika/workshops.yml";
 
 export default props => (
-  <Article title="Talks, I Can Do on Your Event">
+  <Article title="Talks &amp; Workshops , I Can Do on Your Event">
+    <h1>Talks</h1>
     {talks.map(talk => (
       <div>
         <h2>{talk.title}</h2>
@@ -15,6 +17,26 @@ export default props => (
             href={`mailto:ondrej@sika.io?subject=[${props.site.name}] Ask for a Talk: ${talk.title}`}
           >
             I'm interested in {talk.title}
+          </a>
+        </p>
+        <hr
+          style={{ color: "black", borderTop: "1px solid", margin: "15px 0" }}
+        />
+      </div>
+    ))}
+    <h1>Workshops</h1>
+    {workshops.map(workshop => (
+      <div>
+        <h2>{workshop.title}</h2>
+        <h4>Bio</h4>
+        <p>{workshop.bio}</p>
+        <h4>Abstract</h4>
+        <p>{workshop.abstract}</p>
+        <p>
+          <a
+            href={`mailto:ondrej@sika.io?subject=[${props.site.name}] Ask for a Workshop: ${workshop.title}`}
+          >
+            I'm interested in {workshop.title}
           </a>
         </p>
         <hr
