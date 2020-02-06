14ede71afd986a10f74529346663f7057934384a
Ondrej Sika <ondrej@ondrejsika.com>
2020-02-06 15:21:20 +0100

    deploy-now-k8s.sh - tag images & add chenge cause

--- deploy-now-k8s.sh
+++ deploy-now-k8s.sh
@@ -5,7 +5,7 @@
 SITE=$1
 NAME=$(echo $SITE | sed "s/\./-/g")
 
-IMAGE=$CI_REGISTRY_IMAGE-$SITE-deploy-now
+IMAGE=$CI_REGISTRY_IMAGE-$SITE-deploy-now:$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
 
 rm -rf packages/$SITE/out packages/$SITE/.next
 yarn static-$SITE
@@ -16,5 +16,5 @@ rm -rf ci/docker/out
 docker push $IMAGE
 ORIGINAL_KUBERNETES_CONTEXT=$(kubectl config current-context)
 kubectl config use-context $KUBERNETES_CONTEXT
-helm upgrade --install $NAME ondrejsika/one-image --set host=$SITE --set image=$IMAGE
+helm upgrade --install $NAME ondrejsika/one-image --set host=$SITE --set image=$IMAGE --set changeCause=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
 kubectl config use-context $ORIGINAL_KUBERNETES_CONTEXT
