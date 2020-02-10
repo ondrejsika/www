b66ce42c3d49b210d63118b106a8c5a99565b5fa
Martin Jurco <martin@jurco.io>
2020-02-10 16:41:07 +0100

    fixed typo & diacritics on Invite page

--- packages/ondrej-sika.cz/pages/invite.js
+++ packages/ondrej-sika.cz/pages/invite.js
@@ -4,25 +4,25 @@ import Article from "@app/ondrej-sika.cz/layouts/Article";
 export default function InvitePage() {
   return (
     <Article
-      title="Připojte se na Slack, Linkedin a Facebook"
+      title="Připojte se na Slack, LinkedIn a Facebook"
       hideNewsletter={true}
       markdown={`
-Pokud se budete chti pripojit do komunity ucastniku mych skoleni a konzultaci, muzete se pridat do Slacku nebo do skupin na Facebooku a Linkedinu. Muzete se tam ptat me i ostatnich ucastniku na nazor nebo zkusenosti. Proste resit vase problemy s podobnymi lidmi. Zarove Vas budu informovat o novinkach na me strane.
+Pokud se budete chtít připojit do komunity účastníků mých školení a konzultací, můžete se přidat do Slacku nebo do skupin na Facebooku a Linkedinu. Můžete se tam ptát mě i ostatních účastníků na názor nebo zkušenosti. Prostě řešit Vaše problémy s podobnými lidmi. Zároveň Vás budu informovat o novinkách na mé straně.
 
 ## Slack
 
-Na Slack se muzete pripojit pomoci odkazu zde: <https://sika.link/slack-sikapublic>
+Na Slack se můžete připojit pomocí odkazu zde: <https://sika.link/slack-sikapublic>
 
-Pokud odkaz nebude fungovat (bohuzel ho musim kazdych 20 dni aktualizovat), poslete mi email s predmetem: [Invite me to Slack](mailto:ondrej@sika.io?subject=Invite%20me%20to%20Slack,%20Sika%20Public)
+Pokud odkaz nebude fungovat (bohužel ho musím každých 20 dní aktualizovat), pošlete mi email s předmětem: [Invite me to Slack](mailto:ondrej@sika.io?subject=Invite%20me%20to%20Slack,%20Sika%20Public)
 
 ## LinkedIn
 
-- Moje Stranka: [Ondrej Sika](https://www.linkedin.com/company/ondrejsika)
-- Skupina pro ucastniky: [Ondrej Sika Training Attendees](https://www.linkedin.com/groups/8902190/)
+- Moje stránka: [Ondrej Sika](https://www.linkedin.com/company/ondrejsika)
+- Skupina pro účastníky: [Ondrej Sika Training Attendees](https://www.linkedin.com/groups/8902190/)
 
 ## Facebook
 
-- Moje Stranka: [Ondrej Sika](https://www.facebook.com/ondrejsikafreelancer/)
+- Moje stránka: [Ondrej Sika](https://www.facebook.com/ondrejsikafreelancer/)
 - Skupina: [Ondrej Sika Skoleni - Ucastnici](https://www.facebook.com/groups/3484478441626937/)
 
 ## Newsletter
