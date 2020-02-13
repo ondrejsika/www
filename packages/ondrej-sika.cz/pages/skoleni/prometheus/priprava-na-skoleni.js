b7b1e7a2611784ae07c7b013261629b2cde8a998
Ondrej Sika <ondrej@ondrejsika.com>
2020-02-13 11:08:00 +0100

    ondrej-sika.cz/ - add page /skoleni/prometheus/priprava-na-skoleni

---
+++ packages/ondrej-sika.cz/pages/skoleni/prometheus/priprava-na-skoleni.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Article from "@app/ondrej-sika.cz/layouts/Article";
+
+const Page = () => (
+  <Article
+    title="Příprava na Školení Promethea"
+    markdown={`
+Díky za Váš zájem o školení Promethea. Školení pokrývá práci s Promethem, Alert Managerem a Grafanou.
+
+Na školení Promethea potřebujete:
+
+- Vlastní notebook
+- Docker nebo nainstalovaný Prométheus, Alert Manager, Push Gateway a Grafanau
+- Případně můžete pracovat v Cloudu přes SSH, ale dopodručuji lokální instalaci (Docker nebo nativní aplikace)
+
+Binárky (Linux, Mac, Windows) pro Prometheus, Alert Manager a Push Gateway naleznete zde: <https://prometheus.io/download/>
+
+Grafanu nainstalujete podle návodu zde: <https://grafana.com/docs/grafana/latest/installation/>
+
+Pokud budete mít jakýkoliv problém nebo dotaz, neváhejte napsat na <ondrej@sika.io>
+`}
+  />
+);
+
+export default Page;
