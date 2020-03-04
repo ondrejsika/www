e51495209f70bb7a80466a9860a12718147d2925
Ondrej Sika <ondrej@ondrejsika.com>
2020-03-04 20:26:25 +0100

    ondrej-sika.cz/components/MainBar - fix margins (remove duplicite container)

--- packages/ondrej-sika.cz/components/MainBar.js
+++ packages/ondrej-sika.cz/components/MainBar.js
@@ -13,7 +13,7 @@ const Container = styled(BootstrapContainer)`
 const MainBar = props => (
   <Background>
     <Container>
-      <div className="container pt-4 pb-4">
+      <div className="pt-4 pb-4">
         <h1 className="text-white pt-3 pb-3">{props.MainBarHeader}</h1>
         <p className="text-white" style={{ fontSize: "18px" }}>
           {props.MainBarText}
