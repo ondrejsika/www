6b812dc7b84dff7497bb1b26aa2c7dac01a9b9cd
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-04 12:28:38 +0100

    ondrej-sika.cz - edit MainBarHomepage component and delete color.ts

--- packages/ondrej-sika.cz/components/MainBarHomepage.js
+++ packages/ondrej-sika.cz/components/MainBarHomepage.js
@@ -1,13 +1,18 @@
 import React from "react";
+import color from "../config/colors";
+import styled from "styled-components";
 
+const TopBar = styled.div`
+  background-color: ${color.BLUE};
+`;
 const MainBarHomepage = props => (
   <div>
-    <div className="bar-grey">
+    <TopBar>
       <div className="container pt-4 pb-4">
         <h1 className="text-white pt-3 pb-3">{props.MainBarHeader}</h1>
         <h3 className="text-white">{props.MainBarText}</h3>
       </div>
-    </div>
+    </TopBar>
   </div>
 );
 
