dfec2be3792d6922fe1b6391f8886c31a8ea0614
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-05 13:26:31 +0100

    ondrej-sika.cz - Add styled components to _app page in Head component

--- packages/ondrej-sika.cz/pages/_app.js
+++ packages/ondrej-sika.cz/pages/_app.js
@@ -1,8 +1,8 @@
-import React from "next/react";
+import React from "react";
 import App from "next/app";
 import site from "@app/ondrej-sika.cz/config";
-
 import Layout from "@app/ondrej-sika.cz/layouts/Layout";
+import Head from "next/head";
 
 // Imported CSS
 import "@app/ondrej-sika.cz/css";
@@ -16,9 +16,17 @@ class MyApp extends App {
     const { Component, pageProps } = this.props;
     pageProps.site = this.site;
     return (
-      <Layout {...pageProps}>
-        <Component lang={site.lang} {...pageProps} />
-      </Layout>
+      <>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Serif&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Layout {...pageProps}>
+          <Component lang={site.lang} {...pageProps} />
+        </Layout>
+      </>
     );
   }
 }
