b48be932db4a2e85782e06ba65827e815a649ab9
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-05 15:52:48 +0100

    ondrej-sika.cz - fix course header and other header

--- packages/ondrej-sika.cz/components/MainBar.js
+++ packages/ondrej-sika.cz/components/MainBar.js
@@ -14,7 +14,7 @@ const MainBar = props => (
   <Background>
     <Container>
       <div className="pt-4 pb-4">
-        <h1 className="text-white pt-3 pb-3">{props.MainBarHeader}</h1>
+        <h1 className="text-white">{props.MainBarHeader}</h1>
         <p className="text-white" style={{ fontSize: "18px" }}>
           {props.MainBarText}
         </p>
