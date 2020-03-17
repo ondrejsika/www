b2d7ab0c032d1578ff80ce46adc8ab8df6138b42
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-17 16:52:31 +0100

    fix prettier format

--- packages/ondrej-sika.cz/pages/_document.js
+++ packages/ondrej-sika.cz/pages/_document.js
@@ -15,8 +15,12 @@ export default class MyDocument extends Document {
   render() {
     return (
       <html lang="en">
-        <Head>{this.props.styleTags}
-          <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans&display=swap" rel="stylesheet" />
+        <Head>
+          {this.props.styleTags}
+          <link
+            href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
