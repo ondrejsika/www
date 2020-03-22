73b03c613aa4791fee6660b1eb7883e2b143ccbc
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-22 09:01:30 +0100

    ondrejsika-theme - make styled component from paragraph

--- packages/ondrejsika-theme/components/TextArea.js
+++ packages/ondrejsika-theme/components/TextArea.js
@@ -1,11 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const P = styled.p`
+  font-size: 18px;
+`;
 
 const TextArea = props => (
   <div>
     <h2 className="pt-4 pb-2">{props.TextHeader}</h2>
     {(() => {
       if (props.TextParagraph) {
-        return <p>{props.TextParagraph}</p>;
+        return <P>{props.TextParagraph}</P>;
       }
     })()}
     {props.children}
