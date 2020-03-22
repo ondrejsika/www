d9be2c35e1eb79855fd6442d800e6e711a0c3245
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-22 17:51:11 +0100

    skoleni.io - add skoleni.io-text and Navbar component

--- packages/skoleni.io/pages/index.js
+++ packages/skoleni.io/pages/index.js
@@ -1,33 +1,30 @@
 import React from "react";
 import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import styled from "styled-components";
+import Container from "react-bootstrap/Container";
+
+const P = styled.p`
+  padding: 0.5em 0;
+  font-size: 1.3em;
+`;
 
 const Index = () => (
   <>
+    <Navbar />
     <Header header="SKOLENI.IO" />
-    <p>
-      V dobách pandemií roste poptávka po zdravotnickém materiálu; třeba v
-      případě koronaviru po rouškách a respirátorech. Zvýšená poptávka na volném
-      trhu způsobí růst ceny; to se může zdát na první pohled jako nevýhoda, leč
-      nic nemůže být dále od pravdy. Vyšší cena má dva zásadní efekty: Zaprvé
-      motivuje producenty vyrábět více zboží a přesouvat výrobní kapacity od
-      méně potřebných artiklů k potřebnějším – čím naléhavější potřeba, tím
-      vyšší cena, a tedy i vyšší motivace vydělávat, prodávat, produkovat,
-      dodávat; tento stav potrvá tak dlouho, dokud nebude poptávka uspokojena,
-      což způsobí i pokles cen. Zadruhé motivuje konzumenty s nedostatkovým
-      zbožím šetřit a nalézat substituty; krom toho vyšší cena způsobí
-      rovnoměrnější rozložení statků v populaci (nebudou okamžitě vykoupeny za
-      nízkou cenu těmi, kdo přijdou první). Mnozí namítnou, že při zvýšení cen
-      může nastat situace, ve které si někteří nebudou moci dovolit zakoupit to,
-      co potřebují; zapomínají však přitom, že výchozí situace spočívala v tom,
-      že není dost zdravotnického materiálu pro všechny – takže prostě reálně
-      není možné, aby měli všichni to, co potřebují! Onen nedostatek je ostatně
-      také důvod růstu cen. Jinými slovy se nacházíme v situaci, kdy tak či onak
-      není dost zdrojů k uspokojení všech potřeb, takže ať uděláme cokoliv, na
-      někoho se nedostane (což je samozřejmě zlé; ale nevyřešíme to tím, že
-      budeme před realitou strkat hlavu do písku). Rozdíl je v tom, že zvýšená
-      cena způsobí rychlejší řešení situace a zmírní škody, je-li to možné
-      (vizte předchozí odstavec).
-    </p>
+    <Container>
+      <P>
+        Skoleni.io je platforma kde freelanceri mohou prezentovat sve kurzy a
+        skoleni, prevazne IT. Platforma nabizi alternativu ke skolicim firmam,
+        kde nevite kdo presne bude dane tema skolit a nevite jakou kvalitu
+        ocekavat.
+        <br /> Je to misto kde naleznete kurzy co dani profesionalove nabizi s
+        referencemi od proskolenych lidi a firem. Nejsme skolici firma nebo
+        agentura, kurzy si kazdy lektor resi po svem, od registrace (my pouze
+        posleme email s poptavkou) az po platbu.
+      </P>
+    </Container>
   </>
 );
 export default Index;
