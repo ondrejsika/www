521c9137d8288f200d0b36a3200b5f0b172cb027
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-22 09:07:15 +0100

    ondrejsika-theme/OurTeam - create component

---
+++ packages/ondrejsika-theme/components/OurTeam.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import colors from "../config/colors";
+import { Row, Col } from "react-bootstrap";
+
+const Img = styled.img`
+  border-radius: 50%;
+  width: 100%;
+  display: block;
+`;
+const H4 = styled.h4`
+  font-size: 1em;
+  text-transform: capitalize;
+  text-align: center;
+  padding: 0.7em 0;
+`;
+const ImgWrapper = styled.div`
+  ${H4}:hover & {
+    background-color: ${colors.LIGHT_GREY};
+  }
+`;
+
+const OurTeam = props => (
+  <>
+    <Row>
+      {props.lecturers.map(lector => (
+        <Col xs={2}>
+          <ImgWrapper>
+            <Img src={lector[0]} fluid />
+            <H4>{lector[1]}</H4>
+          </ImgWrapper>
+        </Col>
+      ))}
+    </Row>
+  </>
+);
+export default OurTeam;
