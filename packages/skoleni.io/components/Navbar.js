f871c5f497ab4294d8bbf3898e46eec09da1427c
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-22 17:47:42 +0100

    skoleni.io - move NavLinks to right

--- packages/skoleni.io/components/Navbar.js
+++ packages/skoleni.io/components/Navbar.js
@@ -1,12 +1,47 @@
 import React from "react";
+import styled from "styled-components";
+import Link from "next/link";
+const Nav = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+`;
+const NavItem = styled.li`
+  float: right;
+`;
+const NavLink = styled.a`
+  display: block;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+`;
 
-export const Navbar = () => {
+const Navbar = () => (
   <>
-    <ul>
-      <li>Home</li>
-      <li>Skoleni</li>
-      <li>Kontakty</li>
-      <li>Lektori</li>
-    </ul>
-  </>;
-};
+    <Nav>
+      <NavItem>
+        <Link href="#">
+          <NavLink>Home</NavLink>
+        </Link>
+      </NavItem>
+      <NavItem>
+        <Link href="#">
+          <NavLink>Skoleni</NavLink>
+        </Link>
+      </NavItem>
+      <NavItem>
+        <Link href="#">
+          <NavLink>Lektori</NavLink>
+        </Link>
+      </NavItem>
+      <NavItem>
+        <Link href="#">
+          <NavLink>Kontakty</NavLink>
+        </Link>
+      </NavItem>
+    </Nav>
+  </>
+);
+
+export default Navbar;
