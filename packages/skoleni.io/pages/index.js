a70fc8d7934cac347bf4fb14a615475b92258a6c
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-22 19:29:47 +0100

    skoleni.io - use CourseBar and add backgroud to Paragraph

--- packages/skoleni.io/pages/index.js
+++ packages/skoleni.io/pages/index.js
@@ -3,28 +3,36 @@ import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import styled from "styled-components";
 import Container from "react-bootstrap/Container";
+import CourseBar from "../components/CourseBar";
 
 const P = styled.p`
   padding: 0.5em 0;
   font-size: 1.3em;
 `;
 
+const Background = styled.div`
+  background: linear-gradient(#efefef, white);
+`;
+
 const Index = () => (
   <>
     <Navbar />
     <Header header="SKOLENI.IO" />
-    <Container>
-      <P>
-        Skoleni.io je platforma kde freelanceri mohou prezentovat sve kurzy a
-        skoleni, prevazne IT. Platforma nabizi alternativu ke skolicim firmam,
-        kde nevite kdo presne bude dane tema skolit a nevite jakou kvalitu
-        ocekavat.
-        <br /> Je to misto kde naleznete kurzy co dani profesionalove nabizi s
-        referencemi od proskolenych lidi a firem. Nejsme skolici firma nebo
-        agentura, kurzy si kazdy lektor resi po svem, od registrace (my pouze
-        posleme email s poptavkou) az po platbu.
-      </P>
-    </Container>
+    <Background>
+      <Container>
+        <P>
+          Skoleni.io je platforma kde freelanceri mohou prezentovat sve kurzy a
+          skoleni, prevazne IT. Platforma nabizi alternativu ke skolicim firmam,
+          kde nevite kdo presne bude dane tema skolit a nevite jakou kvalitu
+          ocekavat.
+          <br /> Je to misto kde naleznete kurzy co dani profesionalove nabizi s
+          referencemi od proskolenych lidi a firem. Nejsme skolici firma nebo
+          agentura, kurzy si kazdy lektor resi po svem, od registrace (my pouze
+          posleme email s poptavkou) az po platbu.
+        </P>
+      </Container>
+    </Background>
+    <CourseBar />
   </>
 );
 export default Index;
