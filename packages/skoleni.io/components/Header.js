3aab8f9008266b56b78e4177702ec84b68c947a7
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-22 17:46:17 +0100

    skoleni.io - add styles and container from bootstrap

--- packages/skoleni.io/components/Header.js
+++ packages/skoleni.io/components/Header.js
@@ -1,7 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import Container from "react-bootstrap/Container";
 
-const H1 = styled.h1``;
+const H1 = styled.h1`
+  color: white;
+  font-weight: bold;
+  padding: 1em 0;
+  font-family: "IBM Plex Mono", monospace;
+  font-size: 3.5em;
+`;
+const Background = styled.div`
+  background-color: #202020;
+`;
+const Header = props => (
+  <>
+    <Background>
+      <Container>
+        <H1>{props.header}</H1>
+      </Container>
+    </Background>
+  </>
+);
 
-const Header = props => <H1>{props.header}</H1>;
 export default Header;
