62412e52660c9d8b5d1acb616f8d46626b85bc6f
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-23 18:23:41 +0100

    skoleni.io - add lecturer link and page to Header component

--- packages/skoleni.io/components/Header.js
+++ packages/skoleni.io/components/Header.js
@@ -5,18 +5,36 @@ import Container from "react-bootstrap/Container";
 const H1 = styled.h1`
   color: white;
   font-weight: bold;
-  padding: 1em 0;
   font-family: "IBM Plex Mono", monospace;
   font-size: 3.5em;
 `;
+
+const Lecturer = styled.h5`
+  color: white;
+`;
+
 const Background = styled.div`
   background-color: #202020;
+  margin: 1em 0;
 `;
+
+const Wrapper = styled.div`
+  padding: 1em 0;
+`;
+
 const Header = props => (
   <>
     <Background>
       <Container>
-        <H1>{props.header}</H1>
+        <Wrapper>
+          <H1>{props.header}</H1>
+          {props.lecturer && (
+            <Lecturer>
+              lektor:{" "}
+              <a href={`/lektor/${props.lecturerId}`}>{props.lecturer}</a>
+            </Lecturer>
+          )}
+        </Wrapper>
       </Container>
     </Background>
   </>
---
+++ packages/skoleni.io/pages/lektor/ondrejsika.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Ondrejsika = () => (
+  <>
+    <h1>HI MY NAME IS ONDREJ SIKA!!! I LOVE ZUZ </h1>
+  </>
+);
+
+export default Ondrejsika;
