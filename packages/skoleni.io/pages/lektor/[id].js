c215bbed350e714b8b3e6dbae5bfe07a46b90ce4
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-25 20:31:09 +0100

    skoleni.io - fix static render for no in id lecturers.yml

--- packages/skoleni.io/pages/lektor/[id].js
+++ packages/skoleni.io/pages/lektor/[id].js
@@ -1,7 +1,9 @@
 import React from "react";
 import LecturerDD from "@app/skoleni.io/layouts/LecturerDD";
 
-const Page = props => <LecturerDD lecturer_id={props.lecturer_id} />;
+const Page = props => (
+  <>{props.lecturer_id && <LecturerDD lecturer_id={props.lecturer_id} />}</>
+);
 
 Page.getInitialProps = async function(context) {
   return { lecturer_id: context.query.id };
