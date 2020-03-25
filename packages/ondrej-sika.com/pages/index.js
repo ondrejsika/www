e10297eb13eaf78f8403cedbd0f1a6d251f08589
Ondrej Sika <ondrej@ondrejsika.com>
2020-03-25 12:05:08 +0100

    ondrej-sika.com/homepage - update site claim (now it's one line)

--- packages/ondrej-sika.com/pages/index.js
+++ packages/ondrej-sika.com/pages/index.js
@@ -14,7 +14,7 @@ const Index = props => (
       </title>
     </Head>
     <MainBar
-      header="IT Training, Consulting and Workshops in Europe"
+      header="IT &amp; DevOps Training and Consulting in Europe"
       text="I'm boosting effectivity & productivity of software development teams by using tools which lead to faster development and reliable operation of software products."
     ></MainBar>
     <StatisticBar
