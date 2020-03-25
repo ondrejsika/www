5083db34ef6c23a1167b836177d66ae3a6de6ce3
Ondrej Sika <ondrej@ondrejsika.com>
2020-03-25 12:54:46 +0100

    salzburgdevops.com - add link to Meetup.com

--- packages/salzburgdevops.com/config.js
+++ packages/salzburgdevops.com/config.js
@@ -2,7 +2,10 @@ const site = {
   name: "salzburgdevops.com",
   lang: "en",
   gauges_site_id: "5e17419ade0e572fb4c2f3a4",
-  google_analytics_site_id: "XXX"
+  google_analytics_site_id: "XXX",
+  meetup: {
+    meetupcom_url: "https://www.meetup.com/Salzburg-DevOps-Meetups/"
+  }
 };
 
 export default site;
--- packages/salzburgdevops.com/pages/index.js
+++ packages/salzburgdevops.com/pages/index.js
@@ -1,9 +1,11 @@
+import React from "react";
 import MeetupPage from "@app/meetup-theme/layouts/MeetupPage";
 
-const Index = () => (
+const Index = props => (
   <MeetupPage
     site_name="salzburgdevops.com"
     meetup_name="Salzburg DevOps Meetups"
+    meetupcom_url={props.site.meetup.meetupcom_url}
     about_us_header="About Us"
     about_us_content={
       "We are DevOps community in Salzburg. We share knowledge and experiences from using tools like CI, Docker, Kubernetes, ELK / EFK, and much more."
@@ -11,7 +13,9 @@ const Index = () => (
     join_us_header="Join Us"
     join_us_content={
       <p>
-        Let us know if you are interested. Drop us mail to{" "}
+        Let us know if you are interested. Join us on{" "}
+        <a href={props.site.meetup.meetupcom_url}>Meetup.com</a> or drop us mail
+        to{" "}
         <a href="mailto:meetup@salzburgdevops.com">meetup@salzburgdevops.com</a>
         .
       </p>
