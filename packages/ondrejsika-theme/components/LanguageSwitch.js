7c7213d93d63043441c9cc8e3cb257ee673824f1
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-25 15:58:20 +0100

    ondrejsika-theme - add ENG, DE, CZE flags to LanguageSwitch component

--- packages/ondrejsika-theme/components/LanguageSwitch.js
+++ packages/ondrejsika-theme/components/LanguageSwitch.js
@@ -1,5 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+import Container from "react-bootstrap/Container";
 
-const LanguageSwitch = () => <></>;
+const Wrapper = styled.div`
+  background: #121480;
+  text-align: right;
+  padding-top: 0.5em;
+`;
+
+const White = styled.a`
+  color: white !important;
+`;
+
+const LanguageSwitch = () => (
+  <Wrapper>
+    <Container>
+      <White>🇬🇧/ 🇩🇪 / 🇨🇿</White>
+    </Container>
+  </Wrapper>
+);
 
 export default LanguageSwitch;
