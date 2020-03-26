94e4f4add710a55a0e70c5cf0a4cf079e59d5a3f
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-26 08:35:30 +0100

    ondrejsika-theme  - add color asi prop to LanguageSwitch and use it in Trainera

--- packages/ondrejsika-theme/components/LanguageSwitch.js
+++ packages/ondrejsika-theme/components/LanguageSwitch.js
@@ -1,23 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 import Container from "react-bootstrap/Container";
-
-const Wrapper = styled.div`
-  background: #121480;
-  text-align: right;
-  padding-top: 0.5em;
-`;
+import default_colors from "@app/ondrejsika-theme/config/colors";
 
 const White = styled.a`
   color: white !important;
 `;
 
-const LanguageSwitch = () => (
-  <Wrapper>
-    <Container>
-      <White>🇬🇧/ 🇩🇪 / 🇨🇿</White>
-    </Container>
-  </Wrapper>
-);
+const LanguageSwitch = props => {
+  const Wrapper = styled.div`
+    background: ${(props.site &&
+      props.site.colors &&
+      props.site.colors.PRIMARY) ||
+      default_colors.BLUE};
+    text-align: right;
+    padding-top: 0.5em;
+  `;
+
+  return (
+    <Wrapper>
+      <Container>
+        <White>🇬🇧/ 🇩🇪 / 🇨🇿</White>
+      </Container>
+    </Wrapper>
+  );
+};
 
 export default LanguageSwitch;
