425a1ea5cc7863a395775c7a998c8cf617d00ea6
Zuzana Jeschke <zuz@zuzjes.com>
2020-03-26 09:55:45 +0100

    ondrejsika-theme - add color as prop from site

--- packages/ondrejsika-theme/components/MainBarHomepage.js
+++ packages/ondrejsika-theme/components/MainBarHomepage.js
@@ -1,33 +1,37 @@
 import React from "react";
-import color from "@app/ondrejsika-theme/config/colors";
+import default_colors from "@app/ondrejsika-theme/config/colors";
 import styled from "styled-components";
 import { Container as BootstrapContainer } from "react-bootstrap";
 
-const Background = styled.div`
-  background-color: ${color.BLUE};
-`;
-
 const Text = styled.h3`
-  color: ${color.WHITE};
+  color: ${default_colors.WHITE};
   padding: 0;
   font-family: "IBM Plex Sans", sans-serif;
 `;
 const Header = styled.h1`
   font-weight: bolder;
-  color: ${color.WHITE};
+  color: ${default_colors.WHITE};
   padding: 0.5em 0;
 `;
 const Container = styled(BootstrapContainer)`
   padding: 1.5em 0.8em;
 `;
 
-const MainBarHomepage = props => (
-  <Background>
-    <Container>
-      <Header>{props.header}</Header>
-      <Text>{props.text}</Text>
-    </Container>
-  </Background>
-);
+const MainBarHomepage = props => {
+  const Background = styled.div`
+    background-color: ${(props.site &&
+      props.site.colors &&
+      props.site.colors.PRIMARY) ||
+      default_colors.BLUE};
+  `;
+  return (
+    <Background>
+      <Container>
+        <Header>{props.header}</Header>
+        <Text>{props.text}</Text>
+      </Container>
+    </Background>
+  );
+};
 
 export default MainBarHomepage;
