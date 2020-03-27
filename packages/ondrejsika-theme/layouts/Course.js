fd62700a16282bb6842da173eb19fed1e94775a8
Ondrej Sika <ondrej@ondrejsika.com>
2020-03-27 14:56:09 +0100

    ondrejsika-theme/layouts/Course - add en bio

--- packages/ondrejsika-theme/layouts/Course.js
+++ packages/ondrejsika-theme/layouts/Course.js
@@ -97,6 +97,7 @@ const Course = props => (
                 Naučím Vás používat prověřené nástroje a technologie, které
                 povedou k rychlejšímu vývoji a bezpečnějšímu provozu vašeho
                 software."
+                en="I'm a software engineer since 2009. These days, I'm focused in DevOps. I've stated with training in 2013, since then I've made more than 170 training sessions for more than 750 people. My cliets are banks, telco & large enterpises as well as startups and small companies."
                 sk="Volám sa Ondřej Šika, vyvíjam softvér pre Slush Pool a školím. Školím prevažne Docker a CI, ktorý spolu s Dockerom veľmi úzko súvisí. Ďalšie školenia nájdete na mojom webe [ondrej-sika.cz](https://ondrej-sika.cz/). Všetky kurzy vediem osobne. S Dockerom mám viac ako štyri roky skúseností v rámci komerčného vývoja softvéru. Docker používam denne vo vývoji, ale aj na produkcii a prácu bez neho si už neviem ani predstaviť."
               />
             </p>
