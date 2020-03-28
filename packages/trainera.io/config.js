f3d3aeaac495a61e9f036b4fdc3d11407ee4ca2e
Ondrej Sika <ondrej@ondrejsika.com>
2020-03-28 10:06:06 +0100

    trainera.io/config - fix site name

--- packages/trainera.io/config.js
+++ packages/trainera.io/config.js
@@ -7,7 +7,7 @@ const site = {
     process.env.NODE_ENV == "development"
       ? "http://127.0.0.1:3010"
       : process.env.SITE_ORIGIN || "https://ondrej-sika.cz",
-  name: "ondrej-sika.cz",
+  name: "trainera.io",
   lang: "cs",
   gauges_site_id: "5a892d8dba4ae31b28000034",
   google_analytics_site_id: "UA-39461955-14",
