8624fffbe3c26ece02f16f9f4566a3b3c605b18d
Ondrej Sika <ondrej@ondrejsika.com>
2020-03-29 15:34:46 +0200

    ondrejsika-theme - Create SessionDD, move there all logic from ondrej-sika.cz/verejne-terminy/[id] & add some example sections

--- packages/ondrej-sika.cz/pages/verejne-terminy/[id].js
+++ packages/ondrej-sika.cz/pages/verejne-terminy/[id].js
@@ -1,34 +1,7 @@
 import React from "react";
+import SessionDD from "@app/ondrejsika-theme/layouts/SessionDD";
 
-import Article from "@app/ondrejsika-theme/layouts/Article";
-
-import sessions from "@app/data/training/sessions.yml";
-
-let session_id_map = {};
-sessions.map((element, i) => {
-  session_id_map[element.id] = i;
-});
-
-let Page = props => {
-  let session = sessions[session_id_map[props.session_id]];
-  if (!session) {
-    return <></>;
-  }
-  return (
-    <>
-      <Article
-        title={`${session.name} ${session.city} - Ondrej Sika`}
-        header={`${session.name} ${session.city}`}
-        subheader={
-          session.date_from == session.date_to
-            ? `Termín školení ${session.date_from}`
-            : `Termín školení od ${session.date_from} do ${session.date_to}`
-        }
-        hideNewsletter={true}
-      ></Article>
-    </>
-  );
-};
+let Page = props => <SessionDD session_id={props.session_id} />;
 
 Page.getInitialProps = async function(context) {
   return { session_id: context.query.id };
