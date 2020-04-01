0d87f662857572336c5a8f6b9fd52ee4511cb9a9
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-01 17:32:01 +0200

    skoleni.io - export H1 and H3 asi component and use it

---
+++ packages/skoleni.io/components/H1.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const H1 = styled.span`
+  font-size: 6em;
+  padding: 0.05em 0;
+  font-weight: bold;
+  text-transform: uppercase;
+`;
+
+export default H1;
