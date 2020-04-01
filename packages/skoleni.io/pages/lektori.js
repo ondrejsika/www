620a1f74ddce071512378902d54941ba582922eb
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-01 18:53:38 +0200

    skoleni.io - edit Our Team comp and use it in lektori page

--- packages/skoleni.io/pages/lektori.js
+++ packages/skoleni.io/pages/lektori.js
@@ -1,33 +1,20 @@
 import React from "react";
-import styled from "styled-components";
 import OurTeam from "@app/skoleni.io/components/OurTeam";
-import ondrejsika from "@app/data/pictures/ondrejsika.jpg";
-import janpospisil from "@app/data/pictures/lecturers/janpospisil.jpg";
 import { Container } from "react-bootstrap";
-import Header from "@app/skoleni.io/components/Header";
+import H3 from "@app/skoleni.io/components/H3";
 
-const Margin = styled.div`
-  margin: 22em;
-`;
-
-const lektori = () => (
+const Lektori = () => (
   <>
-    <Header header="Nasi lektori" underline="- vybirame z tech nejlepsich" />
-
     <Container>
-      <OurTeam
-        lecturers={[
-          [ondrejsika, "Ondřej Šika", "ondrejsika", "DevOps"],
-          [janpospisil, "Honza Pospíšil", "janpospisil", "xxxxxxx"],
-          [ondrejsika, "Ondřej Šika", "ondrejsika", "xxxxxxx"],
-          [janpospisil, "Honza Pospíšil", "janpospisil", "xxxxxxx"],
-          [ondrejsika, "Ondřej Šika", "ondrejsika"],
-          [ondrejsika, "Ondřej Šika", "ondrejsika"],
-          [janpospisil, "Honza Pospíšil", "janpospisil"]
-        ]}
-      />
-      <Margin />
+      <H3>Lektoři</H3>
     </Container>
+    <OurTeam
+      lecturers={[
+        ["Ondřej Šika", "ondrejsika"],
+        ["Michael Kaplan", "michaelkaplan"],
+        ["Vojtěch Mareš", "vojtechmares"]
+      ]}
+    />
   </>
 );
-export default lektori;
+export default Lektori;
