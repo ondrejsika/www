e7e65796b3d761b2e6c9567e2c4465484d98138f
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-02 12:49:49 +0200

    skoleni.io - change header to H1 in Contact page

--- packages/skoleni.io/pages/kontakty.js
+++ packages/skoleni.io/pages/kontakty.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Container } from "react-bootstrap";
-import Header from "@app/skoleni.io/components/Header";
+import H1 from "@app/skoleni.io/components/H1";
 
 const Margin = styled.div`
   margin-bottom: 25em;
@@ -13,18 +13,17 @@ const P = styled.p`
 
 const Kontakty = () => (
   <>
-    <Header header="Kontakty" />
-
     <Container>
+      <H1>Kontakty</H1>
       <P>
-        <b>Chcete inzerovat sva skoleni na nasem webu?</b> Napiste email a
-        urcite se domluvime. Cena za inzerci je 20% z ceny kurzu za kazdeho kdo
-        se na kurz registruje prez tento web. Pokud k vam klient, ktereho jste
-        ziskali prez skoleni.io, prijde i na dalsi skoleni, budu rad kdyz
-        zaplatite 5% z ceny kurzu. Verim ve svobodu a ferovost, chci at si kazdy
-        sve kurzy promuje jak uzna za vhodne, skoleni.io nemusi byt jedine misto
-        kde bude nabizet a prodavat kurzy. Je to ale dobre misto kde kurzy
-        agregovat a nechat firmy prochazet nezavysle profesionaly.
+        Chcete inzerovat sva skoleni na nasem webu? Napiste email a urcite se
+        domluvime. Cena za inzerci je 20% z ceny kurzu za kazdeho kdo se na kurz
+        registruje prez tento web. Pokud k vam klient, ktereho jste ziskali prez
+        skoleni.io, prijde i na dalsi skoleni, budu rad kdyz zaplatite 5% z ceny
+        kurzu. Verim ve svobodu a ferovost, chci at si kazdy sve kurzy promuje
+        jak uzna za vhodne, skoleni.io nemusi byt jedine misto kde bude nabizet
+        a prodavat kurzy. Je to ale dobre misto kde kurzy agregovat a nechat
+        firmy prochazet nezavysle profesionaly.
       </P>
       <P>
         Mate zajem o kurz? Napiste nam email na:
