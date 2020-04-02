b42ba84b4fa6481ef4356e65fc9e42ba45154a53
Ondrej Sika <ondrej@ondrejsika.com>
2020-04-02 10:50:56 +0200

    ondrej-sika.cz/clanky/zajimave-osobni-stranky - add Petr Manek's site

--- packages/ondrej-sika.cz/pages/clanky/zajimave-osobni-stranky.js
+++ packages/ondrej-sika.cz/pages/clanky/zajimave-osobni-stranky.js
@@ -24,6 +24,7 @@ Můj seznam zajímavých osobních webů obsahem i formou. Průběžne aktualizu
 - <https://endler.dev/about/>
 - <https://sugarshin.net/>
 - <https://nat.github.io/hello/> ([source](https://github.com/nat/hello))
+- <https://petrmanek.cz/>
 
     `}
     />
