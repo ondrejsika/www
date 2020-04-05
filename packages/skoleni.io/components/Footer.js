f5dee5ede4d215fb27420583bbf3f613046d3acc
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-05 17:46:30 +0200

    skoleni.io - edit code struture of Footer

--- packages/skoleni.io/components/Footer.js
+++ packages/skoleni.io/components/Footer.js
@@ -8,10 +8,6 @@ import Link from "next/link";
 const A = styled.a`
   color: white !important;
 `;
-const Wrapper = styled.div`
-  border-top: 1px solid black;
-  margin-top: 2em;
-`;
 
 const Content = styled.div`
   padding: 3em 0;
@@ -20,43 +16,40 @@ const Content = styled.div`
 
 const Bg = styled.div`
   background-color: black;
+  padding-top: 1.5em;
 `;
 
 const Footer = props => (
-  <>
-    <Bg>
-      <Container>
-        <Wrapper>
-          <Row>
-            <Col md={6}>
-              <Content>
-                <H3>skoleni.io</H3>
-                <Link href="skoleni@skoleni.io">
-                  <A href="skoleni@skoleni.io">skoleni@skoleni.io</A>
-                </Link>
-                <br />
-                <Link href="spoluprace@skoleni.io">
-                  <A href="spoluprace@skoleni.io">spoluprace@skoleni.io</A>
-                </Link>
-                <br />
-                <Link href="dotazy@skoleni.io">
-                  <A href="dotazy@skoleni.io">dotazy@skoleni.io</A>
-                </Link>
-              </Content>
-            </Col>
-            <Col md={6}>
-              <Newsletter
-                site={props.site}
-                header="Zajímají Vás novinky?"
-                text="Odebírejte můj newsletter a budete v obraze!"
-                btnText="Přihlásit se k odběru článků a novinek"
-              />
-            </Col>
-          </Row>
-        </Wrapper>
-      </Container>
-    </Bg>
-  </>
+  <Bg>
+    <Container>
+      <Row>
+        <Col md={6}>
+          <Content>
+            <H3>skoleni.io</H3>
+            <Link href="skoleni@skoleni.io">
+              <A href="skoleni@skoleni.io">skoleni@skoleni.io</A>
+            </Link>
+            <br />
+            <Link href="spoluprace@skoleni.io">
+              <A href="spoluprace@skoleni.io">spoluprace@skoleni.io</A>
+            </Link>
+            <br />
+            <Link href="dotazy@skoleni.io">
+              <A href="dotazy@skoleni.io">dotazy@skoleni.io</A>
+            </Link>
+          </Content>
+        </Col>
+        <Col md={6}>
+          <Newsletter
+            site={props.site}
+            header="Zajímají Vás novinky?"
+            text="Odebírejte můj newsletter a budete v obraze!"
+            btnText="Přihlásit se k odběru článků a novinek"
+          />
+        </Col>
+      </Row>
+    </Container>
+  </Bg>
 );
 
 export default Footer;
