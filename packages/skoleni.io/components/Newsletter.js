d2d62e745c8d4de157c9d3daaceb572f6767c29f
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-05 14:01:36 +0200

    skoleni.io - edit button in footer for small resolution

--- packages/skoleni.io/components/Newsletter.js
+++ packages/skoleni.io/components/Newsletter.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import Button from "@app/ondrejsika-theme/components/Button";
 import H3 from "@app/skoleni.io/components/H3";
 
 const Wrapper = styled.div`
   padding: 3em 0;
+  @media (max-width: 400px) {
+    padding: 1em 0;
+  }
 `;
 
 const H3_white = styled(H3)`
@@ -15,17 +17,30 @@ const P = styled.p`
   color: white;
 `;
 
+const FooterButton = styled.a`
+  color: white;
+  border: 2px solid white;
+  padding: 0.475rem 0.85rem;
+  margin: 0.5em 0 1em 0;
+  display: inline-block;
+  font-weight: bold;
+  &:hover {
+    color: #1f1f1f;
+    background-color: white;
+    cursor: pointer;
+  }
+  @media (max-width: 400px) {
+    padding: 0.375rem 0.45rem !important;
+  }
+`;
+
 const Newsletter = props => (
   <Wrapper>
     <H3_white>{props.header}</H3_white>
     <P>{props.text}</P>
-    <Button
-      site={props.site}
-      type="outline"
-      href="https://sika.link/newsletter"
-    >
+    <FooterButton href="https://sika.link/newsletter">
       {props.btnText}
-    </Button>
+    </FooterButton>
   </Wrapper>
 );
 
