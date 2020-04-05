2f8cd876a76623057fee6fc0bda654e7a47deb2b
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-05 11:40:29 +0200

    skoleni.io - fix H1 width on all resolution

--- packages/skoleni.io/components/H1.js
+++ packages/skoleni.io/components/H1.js
@@ -5,6 +5,13 @@ const H1 = styled.span`
   padding: 0.05em 0;
   font-weight: bold;
   text-transform: uppercase;
+  @media (max-width: 992px) {
+    font-size: 4em !important;
+  }
+  @media (max-width: 600px) {
+    font-size: 3em !important;
+    display: block;
+  }
 `;
 
 export default H1;
