d6191d60b9df362a1224894d2b974c9a4fde599b
Ondrej Sika <ondrej@ondrejsika.com>
2020-04-09 14:55:00 +0200

    [GTM] common/google_tag_manager - Add my simple GTM (Google Tag Manager) library

---
+++ packages/common/google_tag_manager/GoogleTagManager.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const GoogleTagManager = props => (
+  <>
+    <script
+      async
+      src={`https://www.googletagmanager.com/gtm.js?id=${props.gtm_id}`}
+    />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${props.gtm_id}', {
+              page_path: window.location.pathname,
+            });
+          `
+      }}
+    />
+  </>
+);
+
+export default GoogleTagManager;
---
+++ packages/common/google_tag_manager/lib.js
@@ -0,0 +1,22 @@
+import Router from "next/router";
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (gtm_id, url) => {
+  window.gtag("config", gtm_id, {
+    page_path: url
+  });
+  console.log(`GTM (${gtm_id}): Pageview: ${url}`);
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value
+  });
+};
+
+export const init = gtm_id => {
+  Router.events.on("routeChangeComplete", url => pageview(gtm_id, url));
+};
