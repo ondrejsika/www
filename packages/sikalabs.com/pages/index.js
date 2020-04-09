1e782744e6fcfde395321712860b58770d2ad18a
Ondrej Sika <ondrej@ondrejsika.com>
2020-04-09 20:18:20 +0200

    sikalabs.com - update site claim

--- packages/sikalabs.com/pages/index.js
+++ packages/sikalabs.com/pages/index.js
@@ -7,10 +7,11 @@ const Index = () => (
       <title>SikaLabs</title>
     </Head>
     <h1>SikaLabs</h1>
-    <h2>Training</h2>
-    <h2>Consulting</h2>
-    <h2>DevOps</h2>
-    <h2>Software Development</h2>
+    <h2>
+      Training; Consulting;
+      <br />
+      DevOps; Development;
+    </h2>
   </div>
 );
 
