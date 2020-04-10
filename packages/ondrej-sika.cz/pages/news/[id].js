07a53e39f29e1750f01c143f5dd8ab6988912bf1
Ondrej Sika <ondrej@ondrejsika.com>
2020-04-10 14:39:06 +0200

    ondrej-sika.cz - add twitter recommendations to news

--- packages/ondrej-sika.cz/pages/news/[id].js
+++ packages/ondrej-sika.cz/pages/news/[id].js
@@ -6,10 +6,38 @@ let news_id_map = {};
 news_list.map((element, i) => {
   news_id_map[element.id] = i;
 });
+import twitter_recommendation_file from "@app/data/training/recommendations/twitter_recommendation.yml";
+import { TwitterTweetEmbed } from "react-twitter-embed";
 
 let Page = props => {
   let news = news_list[news_id_map[props.post_id]];
-  return <Article hideNewsletter title={news.title} markdown={news.content} />;
+  return (
+    <Article
+      hideNewsletter
+      title={news.title}
+      markdown={news.content}
+      after={
+        news.twitter_recommendations && (
+          <>
+            <h4 className="mt-5">Doporučení z Twitteru</h4>
+            <div className="card-columns">
+              {twitter_recommendation_file.map((rec, i) => {
+                if (!news.twitter_recommendations.includes(rec.id)) return;
+                return (
+                  <div key={i} className="card" style={{ border: "none" }}>
+                    <TwitterTweetEmbed
+                      tweetId={rec.tweet_id}
+                      options={{ conversation: "none" }}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        )
+      }
+    />
+  );
 };
 
 Page.getInitialProps = async function(context) {
