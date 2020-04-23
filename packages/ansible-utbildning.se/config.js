87a5813e85b0eca92e7d58c9b556d063edf177b9
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-23 12:11:53 +0200

    ansible-utbildning.se - edit to new 2col layout

--- packages/ansible-utbildning.se/config.js
+++ packages/ansible-utbildning.se/config.js
@@ -1,9 +1,19 @@
+import logo from "@app/data/pictures/courses/ansible.png";
+
 const site = {
   name: "ansible-utbildning.se",
-  lang: "se",
+  lang: "en",
   location: "se",
   gauges_site_id: "5cefafa67fc5fd09cd3b506c",
-  google_analytics_site_id: "UA-39461955-28"
+  google_analytics_site_id: "UA-39461955-28",
+
+  x_course: "ansible",
+  x_site_claim: "Ansible training in Sweden",
+  x_logo: logo,
+  x_contact_link:
+    "https://ondrej-sika.com/contact/?x_source=ansible-utbildning.se",
+  x_inquiry_url:
+    "https://ondrej-sika.com/training/ansible/inquiry/?x_source=ansible-utbildning.se"
 };
 
 export default site;
