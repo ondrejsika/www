04ae88aba30c7052c4806d2306ca232031f0b4c4
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-23 17:23:47 +0200

    docker-training.nl - fix typo in config

--- packages/docker-training.nl/config.js
+++ packages/docker-training.nl/config.js
@@ -1,7 +1,7 @@
 import logo from "@app/data/pictures/courses/docker-w.svg";
 
 const site = {
-  name: "docker-training.de",
+  name: "docker-training.nl",
   lang: "en",
   location: "nl",
   gauges_site_id: "5a78d7ae0b3591119b00003e",
@@ -11,9 +11,9 @@ const site = {
   x_site_claim: "Docker training in Netherlands",
   x_logo: logo,
   x_contact_link:
-    "https://ondrej-sika.com/contact/?x_source=docker-training.de",
+    "https://ondrej-sika.com/contact/?x_source=docker-training.nl",
   x_inquiry_url:
-    "https://ondrej-sika.com/training/ansible/inquiry/?x_source=docker-training.de"
+    "https://ondrej-sika.com/training/ansible/inquiry/?x_source=docker-training.nl"
 };
 
 export default site;
