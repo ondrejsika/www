644465ad0c4ac360bf6c6a38ce688729b9eca0f4
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-23 12:43:44 +0200

    docker-training.ch - edit to 2col layout

--- packages/docker-training.ch/config.js
+++ packages/docker-training.ch/config.js
@@ -1,8 +1,19 @@
+import logo from "@app/data/pictures/courses/docker-w.svg";
+
 const site = {
+  name: "docker-training.ch",
   lang: "en",
   location: "ch",
   gauges_site_id: "5af2b07820013a4dbe11ed0e",
-  google_analytics_site_id: "UA-39461955-17"
+  google_analytics_site_id: "UA-39461955-17",
+
+  x_course: "ansible",
+  x_site_claim: "Docker training in Switzerland",
+  x_logo: logo,
+  x_contact_link:
+    "https://ondrej-sika.com/contact/?x_source=docker-training.ch",
+  x_inquiry_url:
+    "https://ondrej-sika.com/training/ansible/inquiry/?x_source=docker-training.ch"
 };
 
 export default site;
