ec624fbf00556b52d5c32d8c1c8ea69abc0eb73e
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-23 16:16:26 +0200

    docker-training.at/ch - fic topo in config

--- packages/docker-training.at/config.js
+++ packages/docker-training.at/config.js
@@ -7,7 +7,7 @@ const site = {
   gauges_site_id: "5af2af5454a1b966f31f109d",
   google_analytics_site_id: "UA-39461955-16",
 
-  x_course: "ansible",
+  x_course: "docker",
   x_site_claim: "Docker training in Austria",
   x_logo: logo,
   x_contact_link:
--- packages/docker-training.ch/config.js
+++ packages/docker-training.ch/config.js
@@ -7,7 +7,7 @@ const site = {
   gauges_site_id: "5af2b07820013a4dbe11ed0e",
   google_analytics_site_id: "UA-39461955-17",
 
-  x_course: "ansible",
+  x_course: "docker",
   x_site_claim: "Docker training in Switzerland",
   x_logo: logo,
   x_contact_link:
