aab8a7cdaa8373305489afd9e94ea67b18fbe627
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-23 17:21:11 +0200

    docker-training.nl

--- packages/docker-training.nl/config.js
+++ packages/docker-training.nl/config.js
@@ -1,8 +1,19 @@
+import logo from "@app/data/pictures/courses/docker-w.svg";
+
 const site = {
+  name: "docker-training.de",
   lang: "en",
   location: "nl",
   gauges_site_id: "5a78d7ae0b3591119b00003e",
-  google_analytics_site_id: "UA-39461955-11"
+  google_analytics_site_id: "UA-39461955-11",
+
+  x_course: "docker",
+  x_site_claim: "Docker training in Netherlands",
+  x_logo: logo,
+  x_contact_link:
+    "https://ondrej-sika.com/contact/?x_source=docker-training.de",
+  x_inquiry_url:
+    "https://ondrej-sika.com/training/ansible/inquiry/?x_source=docker-training.de"
 };
 
 export default site;
