9989e5a672cdd49bf4ca0933e80f3a16f3e722f5
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-23 12:32:59 +0200

    docker-training.at - edit to 2col ayout

--- packages/docker-training.at/config.js
+++ packages/docker-training.at/config.js
@@ -1,8 +1,19 @@
+import logo from "@app/data/pictures/courses/docker-w.svg";
+
 const site = {
+  name: "docker-training.at",
   lang: "en",
   location: "at",
   gauges_site_id: "5af2af5454a1b966f31f109d",
-  google_analytics_site_id: "UA-39461955-16"
+  google_analytics_site_id: "UA-39461955-16",
+
+  x_course: "ansible",
+  x_site_claim: "Docker training in Austria",
+  x_logo: logo,
+  x_contact_link:
+    "https://ondrej-sika.com/contact/?x_source=docker-training.at",
+  x_inquiry_url:
+    "https://ondrej-sika.com/training/ansible/inquiry/?x_source=docker-training.at"
 };
 
 export default site;
