a1e1dcfd62a4203a30794e1a0613789c0bb985bd
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-24 11:04:03 +0200

    gitlab-ci.cz - edit to 2col layout

--- packages/gitlab-ci.cz/config.js
+++ packages/gitlab-ci.cz/config.js
@@ -1,8 +1,18 @@
+import logo from "@app/data/pictures/courses/gitlab-ci.svg";
+
 const site = {
+  name: "gitlab-ci.cz",
   lang: "cs",
   location: "cz",
   gauges_site_id: "5c2f2cec8d21955f797331fa",
-  google_analytics_site_id: "UA-39461955-20"
+  google_analytics_site_id: "UA-39461955-20",
+
+  x_course: "gitlab-ci",
+  x_site_claim: "Skoleni Gilab CI",
+  x_logo: logo,
+  x_contact_link: "https://ondrej-sika.cz/kontakt/?x_source=gitlab-ci.cz",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/skoleni/gitlab-ci/inquiry/?x_source=gitlab-ci.cz"
 };
 
 export default site;
