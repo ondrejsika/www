78d88e14e8e04def66b97fe728f153d159e616d5
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-24 11:35:16 +0200

    gitlab-training.uk - edit to 2col layout

--- packages/gitlab-training.uk/config.js
+++ packages/gitlab-training.uk/config.js
@@ -1,8 +1,19 @@
+import logo from "@app/data/pictures/courses/gitlab-ci.svg";
+
 const site = {
+  name: "gitlab-training.uk",
   lang: "en",
   location: "uk",
   gauges_site_id: "5cefb7917fc5fd2cd63b5080",
-  google_analytics_site_id: "UA-39461955-34"
+  google_analytics_site_id: "UA-39461955-34",
+
+  x_course: "gitlab-ci",
+  x_site_claim: "Gilab CI training in UK",
+  x_logo: logo,
+  x_contact_link:
+    "https://ondrej-sika.com/contact/?x_source=gitlab-training.uk",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/training/gitlab-ci/inquiry/?x_source=gitlab-training.uk"
 };
 
 export default site;
