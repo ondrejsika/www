00efc9f59cf1b944ca417aaed5d1b5e6ef6226b5
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-24 11:45:18 +0200

    gitlab-utbildning.se - edit to 2col layout

--- packages/gitlab-utbildning.se/config.js
+++ packages/gitlab-utbildning.se/config.js
@@ -1,9 +1,19 @@
+import logo from "@app/data/pictures/courses/gitlab-ci.svg";
+
 const site = {
   name: "gitlab-utbildning.se",
-  lang: "se",
+  lang: "en",
   location: "se",
   gauges_site_id: "5cefb7f76dcfe034ca02e27a",
-  google_analytics_site_id: "UA-39461955-35"
+  google_analytics_site_id: "UA-39461955-35",
+
+  x_course: "gitlab-ci",
+  x_site_claim: "Gilab CI training in Sweden",
+  x_logo: logo,
+  x_contact_link:
+    "https://ondrej-sika.com/contact/?x_source=gitlab-utbildning.se",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/training/gitlab-ci/inquiry/?x_source=gitlab-utbildning.se"
 };
 
 export default site;
