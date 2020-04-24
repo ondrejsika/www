7561095afb9f25d8accc8748be8eacd962958973
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-24 12:33:09 +0200

    kubernetes-utbildning.se - edit to 2col layout

--- packages/kubernetes-utbildning.se/config.js
+++ packages/kubernetes-utbildning.se/config.js
@@ -1,9 +1,19 @@
+import logo from "@app/data/pictures/courses/kubernetes.png";
+
 const site = {
-  name: "kubernetes-utbildnung.se",
-  lang: "se",
+  name: "kubernetes-utbildning.se",
+  lang: "en",
   location: "se",
   gauges_site_id: "5cefb93c6dcfe049aa02e25a",
-  google_analytics_site_id: "UA-39461955-38"
+  google_analytics_site_id: "UA-39461955-38",
+
+  x_course: "kubernetes",
+  x_site_claim: "Kubernetes training in Sweden",
+  x_logo: logo,
+  x_contact_link:
+    "https://ondrej-sika.com/contact/?x_source=kubernetes-utbildning.se",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/training/kubernetes/inquiry/?x_source=kubernetes-utbildning.se"
 };
 
 export default site;
