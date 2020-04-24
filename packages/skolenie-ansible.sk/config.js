a988e3688a40a973084fca31e3be0d46b6cc1c7e
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-24 13:42:18 +0200

    skolenie-ansible.sk - edit to 2col and data driven

--- packages/skolenie-ansible.sk/config.js
+++ packages/skolenie-ansible.sk/config.js
@@ -1,8 +1,19 @@
+import logo from "@app/data/pictures/courses/ansible.png";
+
 const site = {
+  name: "skolenie-ansible.sk",
   lang: "sk",
   location: "cz",
   gauges_site_id: "5cefbbe86dcfe04e0202e27e",
-  google_analytics_site_id: "UA-39461955-39"
+  google_analytics_site_id: "UA-39461955-39",
+
+  x_course: "ansible",
+  x_site_claim: "Ansible skolenie",
+  x_logo: logo,
+  x_contact_link:
+    "https://ondrej-sika.cz/kontakt/?x_source=skolenie-ansible.sk",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/training/ansible/poptavka/?x_source=skolenie-ansible.sk"
 };
 
 export default site;
