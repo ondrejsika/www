3b3e924e67897752e2a5abbe23b69f02fad20385
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-25 17:32:33 +0200

    skolenie.kubernetes.sk - edit to data driven and 2col layout

--- packages/skolenie.kubernetes.sk/config.js
+++ packages/skolenie.kubernetes.sk/config.js
@@ -1,8 +1,19 @@
+import logo from "@app/data/pictures/courses/kubernetes.svg";
+
 const site = {
+  name: "skolenie.kubernetes.sk",
   lang: "sk",
   location: "sk",
   gauges_site_id: "5cefbe086dcfe06a8102e23b",
-  google_analytics_site_id: "UA-39461955-43"
+  google_analytics_site_id: "UA-39461955-43",
+
+  x_course: "kubernetes",
+  x_site_claim: "Najlepšie školenie Kubernetes v SR",
+  x_logo: logo,
+  x_contact_link:
+    "https://ondrej-sika.cz/kontakt/?x_source=skolenie.kubernetes.sk",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/skoleni/kubernetes/poptavka/?x_source=skolenie.kubernetes.sk"
 };
 
 export default site;
