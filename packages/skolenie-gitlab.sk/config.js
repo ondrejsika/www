c1417857111558fe4efa8606bc70afbfaf4a8549
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-25 16:55:24 +0200

    skolenie-gitlab.sk - edit to data driven and 2col layout

--- packages/skolenie-gitlab.sk/config.js
+++ packages/skolenie-gitlab.sk/config.js
@@ -1,8 +1,18 @@
+import logo from "@app/data/pictures/courses/gitlab-ci.svg";
+
 const site = {
+  name: "skolenie-gitlab.sk",
   lang: "sk",
   location: "sk",
   gauges_site_id: "5cefbd6b7fc5fd4ca63b5069",
-  google_analytics_site_id: "UA-39461955-42"
+  google_analytics_site_id: "UA-39461955-42",
+
+  x_course: "gitlab-ci",
+  x_site_claim: "Najlepšie školenie Gitlabu CI v SR",
+  x_logo: logo,
+  x_contact_link: "https://ondrej-sika.cz/kontakt/?x_source=skolenie-gitlab.sk",
+  x_inquiry_url:
+    "https://ondrej-sika.cz/skoleni/gitlab-ci/poptavka/?x_source=skolenie-gitlab.sk"
 };
 
 export default site;
