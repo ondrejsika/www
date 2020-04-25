ce1ed53c72c8f5813a4107740679e452ea0130c6
Ondrej Sika <ondrej@ondrejsika.com>
2020-04-25 14:13:36 +0200

    ondrej-sika.cz/skoleni/rancher/priprava-na-skoleni - fix link

--- packages/ondrej-sika.cz/pages/skoleni/rancher/priprava-na-skoleni.js
+++ packages/ondrej-sika.cz/pages/skoleni/rancher/priprava-na-skoleni.js
@@ -8,7 +8,7 @@ const Page = () => (
     markdown={`
 Na školení Rancheru je potřeba:
 - Nainstalovaný Terraform - [návod na instalaci](/terraform/instalace)
-- Nainstalovaný Rancher CLI & RKE - [návod na instalaci](/rancher/instalace)
+- Nainstalovaný Rancher CLI & RKE - [návod na instalaci](/rancher/instalace-rancher-cli-a-rke)
 - Účet na DigitalOcean (Pokud nemáte můžete použít můj referral: [Registrace + 100 USD kredit](https://sika.link/do-ref))
 - Přístup na internet - ideálně bez korporátní proxy, případně nastavenou proxy v prohlížeči
 `}
