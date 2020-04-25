76d235fa57ddbe6b30f6396437cbfe190aefa1f9
Zuzana Jeschke <zuz@zuzjes.com>
2020-04-25 16:32:18 +0200

    fix typos

--- packages/skolenie-ansible.sk/config.js
+++ packages/skolenie-ansible.sk/config.js
@@ -7,12 +7,12 @@ const site = {
   google_analytics_site_id: "UA-39461955-39",
 
   x_course: "ansible",
-  x_site_claim: "Ansible skolenie",
+  x_site_claim: "Ansible skolenie v SR",
   x_logo: logo,
   x_contact_link:
     "https://ondrej-sika.cz/kontakt/?x_source=skolenie-ansible.sk",
   x_inquiry_url:
-    "https://ondrej-sika.cz/training/ansible/poptavka/?x_source=skolenie-ansible.sk"
+    "https://ondrej-sika.cz/skoleni/ansible/poptavka/?x_source=skolenie-ansible.sk"
 };
 
 export default site;
--- packages/skolenie-docker.sk/pages/index.js
+++ packages/skolenie-docker.sk/pages/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Page from "@app/course-landing/components/Page";
 
 const Index = props => <Page site={props.site} />;
