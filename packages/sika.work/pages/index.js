2f34202ded3972e3204e04d266cbc426955192b6
Ondrej Sika <ondrej@ondrejsika.com>
2020-04-29 18:00:29 +0200

    sika.work - update claim (add Lead)

--- packages/sika.work/pages/index.js
+++ packages/sika.work/pages/index.js
@@ -11,7 +11,7 @@ const Index = () => (
     <div className="mt-2">
       <h1>Ondrej Sika</h1>
       <p>
-        <strong>Your CTO / Software &amp; DevOps Engineer</strong>
+        <strong>Your CTO / Lead Software &amp; DevOps Engineer</strong>
       </p>
       <p>
         <strong>Hire Me!</strong> (New York, London, Berlin, Zurich, Dubai,
