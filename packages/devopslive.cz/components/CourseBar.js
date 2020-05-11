b2bfae31ea4718d02046a09a7220e581455243e4
Zuzana Jeschke <zuz@zuzjes.com>
2020-05-11 14:23:29 +0200

    devopslive.cz - add sorting by date in lecture

--- packages/devopslive.cz/components/CourseBar.js
+++ packages/devopslive.cz/components/CourseBar.js
@@ -22,36 +22,50 @@ const About = styled.p`
   color: white;
   margin-bottom: 0.3rem;
 `;
-const Date = styled.span`
+const LectureDate = styled.span`
   font-size: 1em;
   color: white;
   margin-bottom: 1.3em !important;
 `;
 
-const CourseBar = props => (
-  <>
-    {devops_live_data.map((devops_live, i) => {
-      return (
-        <div key={i}>
-          <Courses>
-            <Container>
-              <div>
-                <H3>
-                  <Link href={devops_live.id}>
-                    <Lecture href={devops_live.id}>{props.title}</Lecture>
-                  </Link>
-                </H3>
-                <About>{devops_live.text}</About>
-                <Date>
-                  <p>- {devops_live.date}</p>
-                </Date>
-              </div>
-            </Container>
-          </Courses>
-        </div>
-      );
-    })}
-  </>
-);
+const CourseBar = () => {
+  devops_live_data.sort((a, b) => {
+    let dateA = new Date(a.date_id);
+    let dateB = new Date(b.date_id);
+    if (dateA - dateB > 0) return 1;
+    else if (dateA - dateB < 0) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+  return (
+    <>
+      {devops_live_data.map((devops_live, i) => {
+        return (
+          <div key={i}>
+            <Courses>
+              <Container>
+                <div>
+                  <H3>
+                    <Link href={devops_live.id}>
+                      <Lecture href={devops_live.id}>
+                        {devops_live.title}
+                      </Lecture>
+                    </Link>
+                  </H3>
+                  <About>{devops_live.text}</About>
+                  <LectureDate>
+                    <p>- {devops_live.date}</p>
+                  </LectureDate>
+                </div>
+              </Container>
+            </Courses>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
 export default CourseBar;
