456eb41a143773325f174a8ebea009ec920abf3a
Zuzana Jeschke <zuz@zuzjes.com>
2020-05-12 22:06:44 +0200

    ondrej-sika.com - edit Kubernetes to DD, add price to courses.yml

--- packages/ondrej-sika.com/pages/training/kubernetes.js
+++ packages/ondrej-sika.com/pages/training/kubernetes.js
@@ -1,51 +1,15 @@
 import React from "react";
-import Course from "@app/ondrejsika-theme/layouts/Course";
+import CourseDD from "@app/ondrejsika-theme/layouts/CourseDD";
+import logo from "@app/data/pictures/courses/kubernetes.png";
 
 const Page = props => (
-  <Course
-    lang={props.site.lang}
+  <CourseDD
+    site={props.site}
     course_id="kubernetes"
+    lang={props.site.lang}
+    logo={logo}
+    location="de"
     show_sessions={true}
-    course_title="Kubernetes Training"
-    logo="/static/kubernetes.png"
-    price_self_funded="800 EUR"
-    price_in_house="2600 EUR"
-    description={`
-### What is Kubernetes?
-Kubernetes is one of the most used platforms for running your Docker containers and it is supported by key market leaders (Google, Amazon, Microsoft, etc.) via Cloud Native Computing Foundation. Kubernetes is being used by businesses of all scales, from startups to corporations.
-
-### Course Description
-This course is ran in the form of a two-day hands-on workshop where you will be able to practice everything you learn. You will be shown how to run Kubernetes locally (for development) and on a cloud platform, Digital Ocean. You will also find out the basic structure of Kubernetes, including its components. You will learn how to create a Kubernetes Manifest and setup application in Docker. Also, you will be shown how to work with permissions and how to use (and create) packages in Helm.
-
-### Course Outline
-- Theory introduction to Kubernetes
-- How to install locally (minikube and kubectl)
-- Description of Kubernetes components
-- Deployment to Kubernetes
-- Working with permissions in the Kubernetes cluster
-- Theory introduction to Helm packages
-- Installation/Deployment using Helm
-- Creating a custom Helm package
-
-#### Who Should Attend
-This course is primarily aimed at people (developers & DevOps), who want to maintain their applications in Kubernetes.
-    `}
-    col1={`
-#### Skill Requirements
-- Basic Docker knowledge
-- Basic Linux knowledge
-- Basic Terminal knowledge
-`}
-    col2={`
-#### Technical Requirements
-- Have Kubernetes installed & running, instruction here
-- Access to the internet - ideally without corporate proxy
-`}
-    recommendations={["oxy", "oxyshop"]}
-    twitter_recommendations={[
-      "petr-todorov-kubernetes",
-      "pavel-mattivi-kubernetes"
-    ]}
   />
 );
 
