bbd0858cb6d996783a0d256daa89e6391aa7bdf8
Zuzana Jeschke <zuz@zuzjes.com>
2020-05-12 22:10:59 +0200

    ondrej-sika.com - edit Prometheus to DD, add price to courses.yml

--- packages/ondrej-sika.com/pages/training/prometheus.js
+++ packages/ondrej-sika.com/pages/training/prometheus.js
@@ -1,42 +1,15 @@
 import React from "react";
-import Course from "@app/ondrejsika-theme/layouts/Course";
+import CourseDD from "@app/ondrejsika-theme/layouts/CourseDD";
+import logo from "@app/data/pictures/courses/prometheus-white.png";
 
 const Page = props => (
-  <Course
-    lang={props.site.lang}
+  <CourseDD
+    site={props.site}
     course_id="prometheus"
+    lang={props.site.lang}
+    logo={logo}
+    location="de"
     show_sessions={true}
-    course_title="Prometheus Training"
-    logo="/static/prometheus.png"
-    price_self_funded="399 EUR"
-    price_in_house="1 300 EUR"
-    description={`
-### What is Prometheus
-Prometheus is widely-adopted open-source systems monitoring and alerting toolkit and metrics server. It allows you to collect data metrics from your app and most of your infrastructure components, as well as slicing and dicing them in order to generate ad-hoc graphs, tables and alerts. Both Prometheus and Kubernetes joined CNCF - Cloud Native Computing Foundation.
-
-### Course Outline
-- Introduction to Prometheus ecosystem
-- How to install Prometheus and Alertmanager
-- Data collection - Exporters
-- Prometheus and Alertmanager configuration
-- How to work with PROMQL (query language of Prometheus)
-- Grafana integration
-- Prometheus Pushgateway
-- Docker, Kubernetes and custom app integrations
-
-### Who Should Attend
-This training course is primarily aimed at developers and DevOps (Server Administrators) teams, who want to monitor applications easily and comfortably.
-
-#### Skill Requirements
-- Basic Linux knowledge
-- Basic Terminal knowledge
-
-#### Technical Requirements
-- Access to the internet - ideally without corporate proxy
-- Installed Docker or Prometheus, Alertmanager, Pushgateway and Grafana
-
-Installation instructions [here](/instructions/prometheus)
-    `}
   />
 );
 
