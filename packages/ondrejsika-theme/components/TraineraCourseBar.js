48da5a7c47baecb590d68a664b64d73517774d51
Zuzana Jeschke <zuz@zuzjes.com>
2020-05-13 16:55:27 +0200

    ondrejsika-theme  - add primary background to courses bar

--- packages/ondrejsika-theme/components/TraineraCourseBar.js
+++ packages/ondrejsika-theme/components/TraineraCourseBar.js
@@ -1,37 +1,44 @@
 import React from "react";
 import Link from "next/link";
 import styled from "styled-components";
-import docker from "@app/data/pictures/courses/docker.svg";
-import git from "@app/data/pictures/courses/git.svg";
-import gitlabci from "@app/data/pictures/courses/gitlab-ci.svg";
-import k8s from "@app/data/pictures/courses/kubernetes.png";
-import react from "@app/data/pictures/courses/react.svg";
-import terraform from "@app/data/pictures/courses/terraform.png";
-import ansible from "@app/data/pictures/courses/ansible.png";
-import rancher from "@app/data/pictures/courses/rancher.png";
-import prometheus from "@app/data/pictures/courses/prometheus.png";
+import docker from "@app/data/pictures/courses/white/docker.svg";
+import git from "@app/data/pictures/courses/white/git.svg";
+import gitlabci from "@app/data/pictures/courses/white/gitlab_ci.svg";
+import k8s from "@app/data/pictures/courses/white/kubernetes.svg";
+import react from "@app/data/pictures/courses/white/react.svg";
+import terraform from "@app/data/pictures/courses/white/terraform.svg";
+import ansible from "@app/data/pictures/courses/white/ansible.svg";
+import rancher from "@app/data/pictures/courses/white/rancher.svg";
+import prometheus from "@app/data/pictures/courses/white/prometheus.svg";
 import color from "../config/colors";
+import default_colors from "@app/ondrejsika-theme/config/colors";
 
 const H4 = styled.h4`
-  color: black;
+  color: white;
   text-align: center;
   font-size: 0.8em;
   padding-top: 0.4em;
 `;
 
-const Box = styled.div`
-  width: 20%;
-  min-width: 110px;
-  border: 1px solid ${color.LIGHT_GREY};
-  border-radius: 5px;
-  margin: 3px;
-`;
 const Trainera = styled.div``;
 
 const Card = props => {
   let training = { en: "training", cs: "skoleni" }[props.lang || "cs"];
+
+  const Box = styled.div`
+    width: 20%;
+    min-width: 110px;
+    border: 1px solid ${color.LIGHT_GREY};
+    border-radius: 5px;
+    margin: 3px;
+    background-color: ${(props.site &&
+      props.site.colors &&
+      props.site.colors.PRIMARY) ||
+      default_colors.BLUE};
+    /* background-color: #088958; */
+  `;
   return (
-    <Box>
+    <Box site={props.site}>
       <Link href={`/${training}/${props.courseId}`}>
         <a className="course-header">
           <H4>
@@ -47,40 +54,46 @@ const Card = props => {
 const TraineraCourseBar = props => {
   return (
     <Trainera>
-      <h2 className="pt-4 pb-2">{props.LectureImgHeader}</h2>
+      <h2 className="pt-4 pb-2 ">{props.LectureImgHeader}</h2>
       <div className="container-flex banner-courses">
         <div className="d-flex flex-wrap flex-sm-wrap flex-lg-nowrap d-lg-flex flex-row">
           <Card
+            site={props.site}
             lang={props.lang}
             courseName="Docker"
             courseId="docker"
             courseImg={docker}
           />
           <Card
+            site={props.site}
             lang={props.lang}
             courseName="Kubernetes"
             courseId="kubernetes"
             courseImg={k8s}
           />
           <Card
+            site={props.site}
             lang={props.lang}
             courseName="Git"
             courseId="git"
             courseImg={git}
           />
           <Card
+            site={props.site}
             lang={props.lang}
             courseName="Gitlab CI"
             courseId="gitlab-ci"
             courseImg={gitlabci}
           />
           <Card
+            site={props.site}
             lang={props.lang}
             courseName="Ansible"
             courseId="ansible"
             courseImg={ansible}
           />
           <Card
+            site={props.site}
             lang={props.lang}
             courseName="Prometheus"
             courseId="prometheus"
@@ -91,18 +104,21 @@ const TraineraCourseBar = props => {
               return (
                 <>
                   <Card
+                    site={props.site}
                     lang={props.lang}
                     courseName="Terraform"
                     courseId="terraform"
                     courseImg={terraform}
                   />
                   <Card
+                    site={props.site}
                     lang={props.lang}
                     courseName="Rancher"
                     courseId="rancher"
                     courseImg={rancher}
                   />
                   <Card
+                    site={props.site}
                     lang={props.lang}
                     courseName="React"
                     courseId="react"
