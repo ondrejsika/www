adf9ce0a7668842ffee54e037e7afacab0fbb8ff
Ondrej Sika <ondrej@ondrejsika.com>
2020-05-19 06:54:55 +0200

    ondrejsika-theme/Page404 - prepare for multi site & multi lang support

--- packages/ondrejsika-theme/components/Page404.js
+++ packages/ondrejsika-theme/components/Page404.js
@@ -2,6 +2,7 @@ import React from "react";
 import CourseBar from "@app/ondrejsika-theme/components/CourseBar";
 import Article from "@app/ondrejsika-theme/layouts/Article";
 import styled from "styled-components";
+import Translate from "@app/common/components/Translate";
 
 const FontSize = styled.div`
   p,
@@ -10,25 +11,62 @@ const FontSize = styled.div`
   }
 `;
 
-export const Page404 = () => {
+export const Page404 = props => {
   return (
     <>
-      <Article title="404 Nenašli jste co hledáte ..." hideNewsletter={false}>
+      <Article
+        site={props.site}
+        title="404 Not found"
+        header={
+          <Translate
+            lang={props.site.lang}
+            default="en"
+            en="404 Don't found you are looking for ..."
+            cs="404 Nenašli jste co hledáte ..."
+          />
+        }
+        hideNewsletter={props.site.lang != "cs"}
+      >
         <FontSize>
-          <p>
-            Napište mi email na{" "}
-            <a href="mailto:ondrej@sika.io">ondrej@sika.io</a> a dejte mi vědět
-            co potřebujete. Rád Vám pomohu. Nebo se podivejte na kurzy co
-            školím:
-          </p>
+          <Translate
+            lang={props.site.lang}
+            default="en"
+            en={
+              <p>
+                Send me and email{" "}
+                <a href="mailto:ondrej@sika.io">ondrej@sika.io</a> and let me
+                know what you need. I&apos;d like to help you.
+              </p>
+            }
+            cs={
+              <p>
+                Napište mi email na{" "}
+                <a href="mailto:ondrej@sika.io">ondrej@sika.io</a> a dejte mi
+                vědět co potřebujete. Rád Vám pomohu. Nebo se podivejte na kurzy
+                co školím:
+              </p>
+            }
+          />
         </FontSize>
-        <CourseBar LectureImgHeader="" />
+        <CourseBar lang={props.site.lang} LectureImgHeader="" />
         <div className="mt-5" />
         <FontSize>
-          <p>
-            Pokud něco nefunguje, dejte mi vědět na{" "}
-            <a href="mailto:ondrej@sika.io">ondrej@sika.io</a>
-          </p>
+          <Translate
+            lang={props.site.lang}
+            default="en"
+            en={
+              <p>
+                If something doesnt work, please report to me on{" "}
+                <a href="mailto:ondrej@sika.io">ondrej@sika.io</a>
+              </p>
+            }
+            cs={
+              <p>
+                Pokud něco nefunguje, dejte mi vědět na{" "}
+                <a href="mailto:ondrej@sika.io">ondrej@sika.io</a>
+              </p>
+            }
+          />
         </FontSize>
       </Article>
     </>
