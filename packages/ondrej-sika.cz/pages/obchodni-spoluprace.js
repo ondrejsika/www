af84f421c1bdbf22ce252f7105f1f3cece984edc
Ondrej Sika <ondrej@ondrejsika.com>
2020-05-19 09:52:33 +0200

    ondrej-sika.cz/obchodni-spoluprace - fix markdown

--- packages/ondrej-sika.cz/pages/obchodni-spoluprace.js
+++ packages/ondrej-sika.cz/pages/obchodni-spoluprace.js
@@ -4,9 +4,9 @@ import Article from "@app/ondrejsika-theme/layouts/Article";
 export const ObchodniSpoluprace = () => (
   <Article
     title="Obchodní Spolupráce"
+    hideNewsletter={true}
     markdown={`
-    Pokud děláte aktivní obchod a máte zájem prodávat moje školení a konzultace tak napište email na ondrej@ondrejsika.com a můžeme se domvluvit. Rád zodpovím veškeré vaše dotazy. Pokud víte o někom kdo by měl potencionální zájem, předejte mu prosím kontakt na mě. Díky.
-
+Pokud děláte aktivní obchod a máte zájem prodávat moje školení a konzultace tak napište email na <ondrej@ondrejsika.com> a můžeme se domvluvit. Rád zodpovím veškeré vaše dotazy. Pokud víte o někom kdo by měl potencionální zájem, předejte mu prosím kontakt na mě. Díky.
 `}
   />
 );
