535f2b7a7fb00ce953a7b49695202b1ae28a90dc
Ondrej Sika <ondrej@ondrejsika.com>
2020-05-23 13:31:40 +0200

    skoleni.io - update footer (add Ondrej Sika column, fix styles)

--- packages/skoleni.io/components/Footer.js
+++ packages/skoleni.io/components/Footer.js
@@ -3,15 +3,14 @@ import styled from "styled-components";
 import { Row, Col, Container } from "react-bootstrap";
 import Newsletter from "@app/skoleni.io/components/Newsletter";
 import H3 from "@app/skoleni.io/components/H3";
-import Link from "next/link";
 
 const A = styled.a`
   color: white !important;
 `;
 
 const Content = styled.div`
-  padding: 3em 0;
   color: white;
+  padding-bottom: 10px;
 `;
 
 const Bg = styled.div`
@@ -19,33 +18,39 @@ const Bg = styled.div`
   padding-top: 1.5em;
 `;
 
+const H3l = styled(H3)`
+  text-transform: none;
+`;
+
 const Footer = props => (
   <Bg>
-    <Container>
+    <Container className="pb-5">
       <Row>
-        <Col md={6}>
+        <Col md={4}>
           <Content>
             <H3>skoleni.io</H3>
-            <Link href="skoleni@skoleni.io">
-              <A href="skoleni@skoleni.io">skoleni@skoleni.io</A>
-            </Link>
+            <A href="mailto:skoleni@skoleni.io">skoleni@skoleni.io</A>
             <br />
-            <Link href="spoluprace@skoleni.io">
-              <A href="spoluprace@skoleni.io">spoluprace@skoleni.io</A>
-            </Link>
-            <br />
-            <Link href="dotazy@skoleni.io">
-              <A href="dotazy@skoleni.io">dotazy@skoleni.io</A>
-            </Link>
+            <A href="mailto:spoluprace@skoleni.io">spoluprace@skoleni.io</A>
           </Content>
         </Col>
-        <Col md={6}>
-          <Newsletter
-            site={props.site}
-            header="Zajímají Vás novinky?"
-            text="Odebírejte můj newsletter a budete v obraze!"
-            btnText="Přihlásit se k odběru článků a novinek"
-          />
+        <Col md={4}>
+          <Content>
+            <H3l>Ondřej Šika</H3l>
+            <A href="mailto:ondrej@sika.io">ondrej@sika.io</A>
+            <br />
+            <A href="https://ondej-sika.cz">ondej-sika.cz</A>
+          </Content>
+        </Col>
+        <Col md={4}>
+          <Content>
+            <Newsletter
+              site={props.site}
+              header="Novinky"
+              text="Odebírejte náš newsletter a budete v obraze!"
+              btnText="Přihlásit se k odběru novinek"
+            />
+          </Content>
         </Col>
       </Row>
     </Container>
--- packages/skoleni.io/components/Newsletter.js
+++ packages/skoleni.io/components/Newsletter.js
@@ -2,15 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import H3 from "@app/skoleni.io/components/H3";
 
-const Wrapper = styled.div`
-  padding: 3em 0;
-  @media (max-width: 400px) {
-    padding: 1em 0;
-  }
-`;
-
 const H3_white = styled(H3)`
   color: white;
+  text-transform: uppercase;
 `;
 
 const P = styled.p`
@@ -35,13 +29,13 @@ const FooterButton = styled.a`
 `;
 
 const Newsletter = props => (
-  <Wrapper>
+  <>
     <H3_white>{props.header}</H3_white>
     <P>{props.text}</P>
     <FooterButton href="https://sika.link/newsletter">
       {props.btnText}
     </FooterButton>
-  </Wrapper>
+  </>
 );
 
 export default Newsletter;
