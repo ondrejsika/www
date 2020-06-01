4095d7f486d08a3bab730ece1cad01e88ce6c464
Zuzana Jeschke <zuz@zuzjes.com>
2020-06-01 15:33:54 +0200

    trainera.io - fix proxmox image name

--- packages/trainera.de/pages/training/proxmox.js
+++ packages/trainera.de/pages/training/proxmox.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CourseDD from "@app/ondrejsika-theme/layouts/CourseDD";
-import logo from "@app/data/pictures/courses/white/proxmox.svg";
+import logo from "@app/data/pictures/courses/proxmox-white.svg";
 
 const Page = props => (
   <CourseDD
