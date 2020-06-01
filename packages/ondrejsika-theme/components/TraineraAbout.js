263a96c20b190f1322ee4a9b33e5d70cfd21f857
Zuzana Jeschke <zuz@zuzjes.com>
2020-06-01 15:18:12 +0200

    ondrejsika-theme - add new text to 1 col - TraineraAbout

--- packages/ondrejsika-theme/components/TraineraAbout.js
+++ packages/ondrejsika-theme/components/TraineraAbout.js
@@ -16,7 +16,7 @@ const Page = props => (
         <Text>
           <Translate
             lang={props.lang || "cs"}
-            en="We offer open session or in-house training. "
+            en="Book us for an open session or in-house training and let us boost the productivity of your teams."
             cs="Nabízíme otevřené termíny a in-house školení. Školíme v českém i anglickém jazyce."
           />
         </Text>
@@ -30,25 +30,6 @@ const Page = props => (
           />
         </Text>
       </Col>
-      {/* <TextArea.P>
-        We are training agency focused on DevOps. Our team is made up of
-        experienced DevOps lecturers who work with DeoOps technologies on a
-        daily basis. Therefore they can explain the technology clearly for
-        beginners and answer all practical questions from attendees.
-      </TextArea.P>
-      <TextArea.P>
-        Technology stack, which we train, is a set of depending tools, which
-        boost the effectiveness and productivity of your teams &amp; yours. You
-        can expect faster delivery of new features to your clients as well as
-        cutting your operating budget.
-      </TextArea.P>
-      <TextArea.P>
-        DevOps to the edge. Start with Docker, Kubernetes or CI today!
-      </TextArea.P>
-      <TextArea.P>
-         Due
-        to Covid-19 we are 100% online.
-      </TextArea.P> */}
     </Row>
   </>
 );
