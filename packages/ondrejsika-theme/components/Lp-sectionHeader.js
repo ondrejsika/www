fcb6ea720eeff76caf048fde90808226696b6585
Zuzana Jeschke <zuz@zuzjes.com>
2020-06-01 20:28:48 +0200

    ondrejsika-theme - create Section Header

---
+++ packages/ondrejsika-theme/components/Lp-sectionHeader.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const SectionHeader = styled.h3`
+  font-family: "IBM Plex Mono", monospace;
+  font-size: 2.5em;
+  color: #131480;
+  border-bottom: 5px solid #c8bfb0;
+  box-sizing: border-box;
+  display: inline;
+`;
+
+export default SectionHeader;
