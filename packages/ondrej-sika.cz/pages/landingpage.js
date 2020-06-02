640edd238031e9bc83f5cc0d1a3f82a9de9f2b64
Zuzana Jeschke <zuz@zuzjes.com>
2020-06-02 10:57:12 +0200

    ondrej-sika.cz - edit layout

--- packages/ondrej-sika.cz/pages/landingpage.js
+++ packages/ondrej-sika.cz/pages/landingpage.js
@@ -1,8 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Row, Col, Container } from "react-bootstrap";
-// import ondrejsika_chair from "@app/data/pictures/landing-page/ondrejsika4x_chair.png";
-import ondrej_chair from "@app/data/pictures/landing-page/ondrejsika4x_chair.png";
+import ondrej_chair from "@app/data/pictures/landing-page/ondrej_chair.png";
 
 import Button from "@app/ondrejsika-theme/components/Lp-button";
 import Text from "@app/ondrejsika-theme/components/Lp-text";
@@ -25,55 +24,44 @@ const TextBeige = styled.span`
 const BackgroundBlue = styled.div`
   background-color: #131480;
   padding-bottom: 2em;
+  background-image: url(${ondrej_chair});
+  background-repeat: no-repeat;
+  background-position: right bottom;
+  background-size: 400px;
 `;
-const HeaderBgImg = styled.img`
-  left: 1172px;
-  width: 453px;
-  height: 533px;
-  background: transparent url() 0% 0% no-repeat padding-box;
-  opacity: 1;
-`;
-const MyContainer = styled(Container)`
-  padding-top: 4em;
-  padding-left: 100px;
-  padding-right: 75px;
-  margin: 0;
+
+const HeaderBox = styled.div`
+  width: 1078px;
+  padding: 5em 0 6em 0;
 `;
 
 const Center = styled.div`
   text-align: center;
   margin: 3em 0;
 `;
-const Layout = styled.div`
-  padding-left: 100px;
-  padding-right: 75px;
-  margin-bottom: 16em;
-`;
+
 const Page = () => (
   <>
     <BackgroundBlue>
-      <MyContainer>
-        <Row>
-          <Col md={8}>
-            <MainTitle>
-              Jsem <TextBeige>Ondřej Šika</TextBeige> a pomohu vám s open-source{" "}
-              <TextBeige>DevOps technologiemi</TextBeige>!
-            </MainTitle>
-            <Text white>
-              Snížime spolu náklady na IT, vyřešíme technologický dluh a
-              připravíme Vaši IT architekturu na rapidní škálování.
-            </Text>
-            <Button inverse href="#">
-              Zjistit vice
-            </Button>
-          </Col>
-          <Col md={4}>
-            <HeaderBgImg src={ondrej_chair} />
-          </Col>
-        </Row>
-      </MyContainer>
+      <Container>
+        <HeaderBox>
+          <MainTitle>
+            Jsem <TextBeige>Ondřej Šika</TextBeige>
+            <br /> a pomohu vám s open-source
+            <br />
+            <TextBeige>DevOps technologiemi</TextBeige>!
+          </MainTitle>
+          <Text white>
+            Snížime spolu náklady na IT, vyřešíme technologický dluh a
+            připravíme Vaši IT architekturu na rapidní škálování.
+          </Text>
+          <Button inverse href="#">
+            Zjistit vice
+          </Button>
+        </HeaderBox>
+      </Container>
     </BackgroundBlue>
-    <Layout>
+    <Container>
       <Center>
         <SectionHeader center>S ČÍM VÁM POMOHU</SectionHeader>
       </Center>
@@ -92,7 +80,7 @@ const Page = () => (
           <Box text="Vyškolím Váš team tak, aby mohl sám a efektivně vytvořit DevOps pipeline a pracovat s ním. Vše si vyzkoušíme na hands-on workshopech po jejichž absolvování budete schopni bezpečně spravovat a rychle rozvíjet Vase DevOps prostředí." />
         </Col>
       </Row>
-    </Layout>
+    </Container>
   </>
 );
 
