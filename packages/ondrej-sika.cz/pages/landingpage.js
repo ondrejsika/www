813b2da534c6d9ca86819dab5023ec5977609fd0
Zuzana Jeschke <zuz@zuzjes.com>
2020-06-02 12:39:06 +0200

    ondrej-sika.cz - use new comp in LandingPage

--- packages/ondrej-sika.cz/pages/landingpage.js
+++ packages/ondrej-sika.cz/pages/landingpage.js
@@ -2,11 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import { Row, Col, Container } from "react-bootstrap";
 import ondrej_chair from "@app/data/pictures/landing-page/ondrej_chair.png";
+import ondrej_training from "@app/data/pictures/landing-page/ondrej_training.png";
 
 import Button from "@app/ondrejsika-theme/components/Lp-button";
 import Text from "@app/ondrejsika-theme/components/Lp-text";
 import Box from "@app/ondrejsika-theme/components/Lp-Box";
 import SectionHeader from "@app/ondrejsika-theme/components/Lp-sectionHeader";
+import Companies from "@app/ondrejsika-theme/components/Lp-companies";
 
 const MainTitle = styled.h1`
   font-family: "IBM Plex Mono", monospace;
@@ -40,6 +42,14 @@ const Center = styled.div`
   margin: 3em 0;
 `;
 
+const Img = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
+
+const MarginBottom = styled.div`
+  margin-bottom: 6em;
+`;
 const Page = () => (
   <>
     <BackgroundBlue>
@@ -65,19 +75,34 @@ const Page = () => (
       <Center>
         <SectionHeader center>S ČÍM VÁM POMOHU</SectionHeader>
       </Center>
-      <Row>
-        <Col md={4}>
-          <Box
-            text="Zavedu open-source DevOps do Vaší firmy. Nastavím nástroje a procesy
+
+      <MarginBottom>
+        <Row>
+          <Col md={4}>
+            <Box
+              text="Zavedu open-source DevOps do Vaší firmy. Nastavím nástroje a procesy
           tak, abyste automaticky, spolehlivě a rychle doručovali novou
           funkcionalitu Vašim zákazníkům."
-          />
+            />
+          </Col>
+          <Col md={4}>
+            <Box text="Vyřeším Váš konkrétní problém formou konzultací, čímž se vyvarujete zbytečných a drahých chyb. Já a můj team Vám budeme oporou při provozu Vaší DevOps infrastruktury formou jednoho z našich support plánů." />
+          </Col>
+          <Col md={4}>
+            <Box text="Vyškolím Váš team tak, aby mohl sám a efektivně vytvořit DevOps pipeline a pracovat s ním. Vše si vyzkoušíme na hands-on workshopech po jejichž absolvování budete schopni bezpečně spravovat a rychle rozvíjet Vase DevOps prostředí." />
+          </Col>
+        </Row>
+      </MarginBottom>
+
+      <Row>
+        <Col md={6}>
+          <Img src={ondrej_training} />
         </Col>
-        <Col md={4}>
-          <Box text="Vyřeším Váš konkrétní problém formou konzultací, čímž se vyvarujete zbytečných a drahých chyb. Já a můj team Vám budeme oporou při provozu Vaší DevOps infrastruktury formou jednoho z našich support plánů." />
-        </Col>
-        <Col md={4}>
-          <Box text="Vyškolím Váš team tak, aby mohl sám a efektivně vytvořit DevOps pipeline a pracovat s ním. Vše si vyzkoušíme na hands-on workshopech po jejichž absolvování budete schopni bezpečně spravovat a rychle rozvíjet Vase DevOps prostředí." />
+        <Col md={6}>
+          <Center>
+            <SectionHeader center>moji klienti</SectionHeader>
+          </Center>
+          <Companies />
         </Col>
       </Row>
     </Container>
