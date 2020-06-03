4e91daa681596b3ee3cf3077e5e4a1e4b3e4b609
Zuzana Jeschke <zuz@zuzjes.com>
2020-06-03 13:49:07 +0200

    ondresika-theme/LP - add space around Btn, create huge buton

--- packages/ondrejsika-theme/components/Lp-button.js
+++ packages/ondrejsika-theme/components/Lp-button.js
@@ -2,12 +2,17 @@ import React from "react";
 import styled, { css } from "styled-components";
 import Link from "next/link";
 
+const Margin = styled.div`
+  padding: 2em 0;
+`;
+
 const Button = props => {
   const WrapperButton = styled.a`
     text-align: center;
     vertical-align: middle;
     cursor: pointer;
     padding: 0.475rem 2.25rem;
+    margin: 2em 0;
     font-weight: bold;
 
     background: #131480;
@@ -23,19 +28,26 @@ const Button = props => {
         border-radius: 54px;
         color: #131480;
       `}
+    ${props =>
+      props.huge &&
+      css`
+        padding: 0.675rem 3rem;
+      `}
   `;
+
   return (
-    <>
+    <Margin>
       <Link href={props.href}>
         <WrapperButton
           // site={props.site}
           href={props.href}
           inverse={props.inverse}
+          huge={props.huge}
         >
           {props.children}
         </WrapperButton>
       </Link>
-    </>
+    </Margin>
   );
 };
 
