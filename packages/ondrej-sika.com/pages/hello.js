6271e1d54af2538984dd9932c03dab631cef1b39
Zuzana Jeschke <zuz@zuzjes.com>
2020-06-08 11:29:02 +0200

    ondrej-sika.com - create hello page

---
+++ packages/ondrej-sika.com/pages/hello.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LandingPage from "@app/ondrejsika-theme/layouts/LandingPage";
+
+const Page = props => <LandingPage site={props.site} />;
+Page.getInitialProps = () => {
+  return {
+    meta: {
+      noNavbar: true,
+      noFooter: true
+    }
+  };
+};
+
+export default Page;
