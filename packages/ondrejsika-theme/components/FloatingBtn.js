7f93e5f4ace522dfdf1beb98d25a22813d957305
Zuzana Jeschke <zuz@zuzjes.com>
2020-06-11 15:00:23 +0200

    ondrejsika-theme - create floating email buttom

---
+++ packages/ondrejsika-theme/components/FloatingBtn.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import Link from "next/link";
+
+const FloatingBtn = props => {
+  const WrapperButton = styled.a`
+    position: fixed;
+    width: 60px;
+    height: 60px;
+    bottom: 40px;
+    right: 40px;
+    background-color: #131480;
+    color: #fff;
+    border-radius: 50px;
+    text-align: center;
+    box-shadow: 2px 2px 3px #999;
+    z-index: 5;
+
+    &:hover {
+      background: #707070;
+      color: #131480;
+      text-decoration: none;
+    }
+  `;
+
+  return (
+    <>
+      <Link href={props.href}>
+        <WrapperButton href={props.href}>{props.children}</WrapperButton>
+      </Link>
+    </>
+  );
+};
+
+export default FloatingBtn;
