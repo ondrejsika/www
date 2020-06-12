009e2f0f34fb958cc7981c588431ab652ba5af11
Zuzana Jeschke <zuz@zuzjes.com>
2020-06-12 10:34:14 +0200

    ondrej-sika.com - add more text to index page

--- packages/ondrej-sika.com/pages/index.js
+++ packages/ondrej-sika.com/pages/index.js
@@ -28,16 +28,29 @@ const Index = props => (
     <div className="container">
       <CourseBar lang={props.site.lang} LectureImgHeader="My Training" />
       <TextWithImg TextWithImgHeader="About me">
-        My name is Ondrej, I am a software engineer and DevOps consultant,
-        architect &amp; lecturer. I lead mostly Docker courses and CI; they are
-        both closely interrelated. I have worked for small companies, startups
-        to big corporations. I have four years of experience with training.
+        My name is Ondrej Sika and I have been involved in software development
+        and DevOps since 2009, teaching my practical experiences since 2013.
+        <br />
+        <br />
+        I worked for small companies, startups and large corporations in order
+        to streamline the process of their software development, testing and
+        deployment. Today, I focus primarily on DevOps: designing the software
+        stack, development and operating environment in companies.
+        <br />
+        <br />
+        Working with tools like Docker, Kubernetes and Ansible, I automate as
+        much as possible so that people don&apos;t have to do routines that
+        machines can easily do.
         <br />
         <br />
         <h4>Training</h4>
-        All courses are perform as workshops (hands-on). They are build on my
-        long-term knowledge and experience in developing, testing and running
+        All my courses are hands-on workshops. When creating them, I utilized
+        many years of my experience in developing, testing and operating
         software projects.
+        <br />
+        <br />I work with tools like Git, Docker, Kubernetes, Ansible or CI on a
+        daily basis. Everything I teach I have already tested on my own projects
+        and I have personal experience with these technologies.
       </TextWithImg>
       <CompaniesBar lang={props.site.lang} />
     </div>
