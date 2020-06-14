1ecff7d062a202e0e8267edf742391e43b143946
Ondrej Sika <ondrej@ondrejsika.com>
2020-06-14 12:33:37 +0200

    ondrej-sika.cz/blog - add post: Podman: "Docker" pod non-root uzivatem

--- packages/ondrej-sika.cz/pages/docker/index.js
+++ packages/ondrej-sika.cz/pages/docker/index.js
@@ -1,11 +1,15 @@
+import React from "react";
 import Article from "@app/ondrejsika-theme/layouts/Article";
 
-export default () => (
+const Page = () => (
   <Article
     title="Docker"
     markdown={`
 - [Instalace](/docker/instalace)
 - [Docker s TLS](/docker/docker-s-tls)
+- [Podman: "Docker" pod non-root uzivatelem](/blog/podman-docker-pod-non-root-uzivatelem) (blog post)
 `}
   />
 );
+
+export default Page;
