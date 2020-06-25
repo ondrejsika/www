bb3496fdd2527f267a325fc51705a2c265423e49
Martin Jurco <martin@jurco.io>
2020-06-25 21:39:12 +0200

    fix diacritics in Bitcoin article

--- packages/ondrej-sika.cz/pages/bitcoin.js
+++ packages/ondrej-sika.cz/pages/bitcoin.js
@@ -4,67 +4,67 @@ import Article from "@app/ondrejsika-theme/layouts/Article";
 const Page = () => (
   <Article
     title="Bitcoin"
-    subheader="Vsechno co potrebujete vedet o Bitcoinu. Prehledne! Ne Blockchain, ne crypto."
+    subheader="Všechno co potřebujete vědět o Bitcoinu. Přehledně! Ne Blockchain, ne crypto."
     hideNewsletter={true}
     markdown={`
 ## Investice do Bitcoinu
 
-Nejsem investicni poradce ani obchodnik. Tyto pravidla mi rekl pritel Daniel Steigerwald a me prisly velmi rozumne.
+Nejsem investiční poradce ani obchodník. Tyto pravidla mi řekl přítel Daniel Steigerwald a mě přišly velmi rozumné.
 
 1. Kupovat pouze Bitcoin
-2. Prvni investice by mela byt maximalne jeden mesicni plat
-2. Neprodavat pri prvnim poklesu ceny
+2. První investice by měla být maximálně jeden měsíční plat
+2. Neprodávat při prvním poklesu ceny
 
-Souhlasim s temito pravidly, ale spis doporucuji jak to delam ja. Pravidelne si davam do Bitcoinu penize, kazdy mesic stejnou castku, at je cena jakakoliv. Ano, nakupoval jsem i za cenu 18 000 USD.
+Souhlasím s těmito pravidly, ale spíš doporučuji postup jak to dělám já. Pravidelně si dávám do Bitcoinu peníze, každý měsíc stejnou částku, ať je cena jakákoliv. Ano, nakupoval jsem i za cenu 18 000 USD/BTC.
 
-Bitcoin ma smysl pouze koupit. Tezeni nema smyl, pro jednotlivce, kteri nechteji investovat miliony. Ruzne online & cloud nesmysly (cloud mining, klikacky) jsou scam.
+Bitcoin má smysl pouze koupit. Těžení nemá smysl pro jednotlivce, kteří nechtějí investovat milióny. Různé online & cloud nesmysly (cloud mining, klikačky) jsou scam.
 
-## Penezenky
+## Peněženky
 
-- Hardware - Pro velke castky, Bitcoinova banka ve vlastnich rukou
-- Aplikace - Pro male castky, vzdy po ruce, jednoduse pouzitelne
+- Hardware - Pro velké částky, Bitcoinová banka ve vlastních rukou
+- Aplikace - Pro malé částky, vždy po ruce, jednoduše použitelné
 
 #### Trezor
 
-Trezor je prvni hardware penezenka a zaroven je od ceskych tvurcu. Trezor je idealni pro uchovani vetsich castek. Jako hardware penezenka je to bezpecne misto kde mit ulozene Bitcoiny. Cena je 59 EUR (cca 1600 CZK).
+Trezor je první hardware peněženka a zároveň je od českých tvůrců. Trezor je ideální pro uchování větších částek. Jako hardware peněženka je to bezpečné místo, kde mít uložené Bitcoiny. Cena je 59 EUR (cca 1600 CZK).
 
-[Koupit na shop.trezor.io](https://shop.trezor.io/product/trezor-one-white)
+[Koupíte na shop.trezor.io](https://shop.trezor.io/product/trezor-one-white)
 
 #### Blockstream Green
 
-Blockstream Green je mobilni a desktopova penezenka. Je idealni na male mnozstvi penez, neni tak bezpecna jako Trezor, ale na penize na kafe staci.
+Blockstream Green je mobilní a desktopová peněženka. Je ideální na malé množství peněz, není tak bezpečná jako Trezor, ale na peníze na kafe stačí.
 
 [Blockstream Green website](https://blockstream.com/green/) (odkazy na verzi pro iPhone, Mac, Android, Linux a Windows)
 
-## Nakup Bitcoinu
+## Nákup Bitcoinu
 
-Bitcoin ma smysl pouze koupit. Moznosti nakupu je nekolik, zde jsou ty spolehlive a doporucuji pouzivat tyto. Pokud budete chtit koupit Bitcoiny i jinde, dejte mi vedet.
+Bitcoin má smysl pouze koupit. Možnosti nákupu je několik, zde jsou ty spolehlivé a doporučují je používat. Pokud budete chtít koupit Bitcoiny i jinde, dejte mi vědět.
 
 #### Bitcoin ATM (Bankomaty)
 
-Nejjednodussi nakup Bitcoinu je z bankomatu. Vlozite hotovost a dostanete Bitcoiny na Vasi penezenku. V bankomatech je fee (rozdil oproti burze) od 4 do 15 procent.
+Nejjednodušší nákup Bitcoinu je z bankomatů. Vložíte hotovost a dostanete Bitcoiny na Váši peněženku. V bankomatech je fee (rozdíl oproti burze) od 4 do 15 procent.
 
-Mapu bankomatu najdete na: [Coin ATM Radar](https://coinatmradar.com/)
+Mapu bankomatů najdete na: [Coin ATM Radar](https://coinatmradar.com/)
 
-#### Nakup kartou v Trezoru
+#### Nákup kartou v Trezoru
 
-Pokud chete koupit Bitcoin kartou primo z Trezoru, jde to velmi jednoduse primo z [wallet.trezor.io](https://wallet.trezor.io). Poplatek je cca 13 procent.
+Pokud chcete koupit Bitcoin kartou přímo z Trezoru, jde to velmi jednoduše přímo z [wallet.trezor.io](https://wallet.trezor.io). Poplatek je cca 13 procent.
 
-#### Smenarny
+#### Směnárny
 
-Bitcoin se da koupit i ve smenarnach, poplatky jsou cca 3 az 5 procent. Jako smenarnu doporucuji [Simple Coin](https://client.simplecoin.eu/cs).
+Bitcoin se dá koupit i ve směnárnách, poplatky jsou cca 3 až 5 procent. Jako směnárnu doporučuju [Simple Coin](https://client.simplecoin.eu/cs).
 
 #### Local Bitcoins
 
-[Local Bitcoins](https://localbitcoins.com/) je sluzba, ktera propojuje lidi, kteri chteji koupit a prodat Bitcoiny. Platit se da ruzne: bankovni prevod, revolut, osobne. Bezpecnost transakce je zarucena portalem. Fee jsou tam nizsi, cca 2 az 10 procent, podle prodejce.
+[Local Bitcoins](https://localbitcoins.com/) je služba, která propojuje lidi, kteří chtějí koupit a prodat Bitcoiny. Platit se dá různě: bankovní převod, revolut, osobně. Bezpečnost transakce je zaručena portálem. Fee jsou tam nižší, cca 2 až 10 procent, podle prodejce.
 
 #### Burza
 
-Nejlevnejsi je nakup primo na burze. Je tam vsak slozitejsi registrace a deposit typicky v eurech. Pro vetsi objemy je to nejlepsi cesta. Z burz doporucuji [Coinmate](https://coinmate.io/) (stoji za ni cesi) a [Bitstamp](https://bitstamp.net/).
+Nejlevnější je nákup přímo na burze. Je tam však složitější registrace a deposit typicky v eurech. Pro větší objemy je to nejlepší cesta. Z burz doporučuju [Coinmate](https://coinmate.io/) (stojí za ní češi) a [Bitstamp](https://bitstamp.net/).
 
-## Muj Bitcoin Newsletter
+## Můj Bitcoin Newsletter
 
-Nepravidelny newsletter s dulezitymi informacemi ohledne Bitcoinu. [Chci byt v obraze](https://sika.link/bitcoin-newsletter)
+Nepravidelný newsletter s důležitými informacemi ohledně Bitcoinu. [Chci být v obraze](https://sika.link/bitcoin-newsletter)
 
 `}
   />
