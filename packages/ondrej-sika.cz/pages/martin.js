9f39b580e1fd34a8772018f8bb521ff8a07f026a
Ondrej Sika <ondrej@ondrejsika.com>
2020-07-02 07:09:27 +0200

    ondrej-sika.cz/martin - add photo & contacts

--- packages/ondrej-sika.cz/pages/martin.js
+++ packages/ondrej-sika.cz/pages/martin.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Article from "@app/ondrejsika-theme/layouts/Article";
+import martin from "@app/data/pictures/martinjurco_portrait.jpg";
 
 export default function InvitePage() {
   return (
@@ -7,11 +8,28 @@ export default function InvitePage() {
       title="Martin Jurco"
       subheader="#operations #scaling #marketing #sales"
       hideNewsletter={true}
-      markdown={`
-Martin je moje prava ruka co se tyka operativy, skalovani, marketingu a obchodu.
-
-Potkali jsme se spolu na Slush Poolu a diky nemu nemusim resit agendu a muzu delat to v cem jsem dobry - DevOps.
-      `}
-    />
+    >
+      <div className="row">
+        <div className="col-8">
+          <p>
+            Martin je moje prava ruka co se tyka operativy, skalovani,
+            marketingu a obchodu.
+          </p>
+          <p>
+            Potkali jsme se spolu na Slush Poolu a diky nemu nemusim resit
+            agendu a muzu delat to v cem jsem dobry - DevOps.
+          </p>
+          <p>
+            Kontakt na Martina je{" "}
+            <a href="mailto:martin@jurco.io">martin@jurco.io</a>, pro pripadnou
+            spolupraci se mnou (Ondrejem Sikou), piste na{" "}
+            <a href="mailto:spoluprace@sika.io">spoluprace@sika.io</a>
+          </p>
+        </div>
+        <div className="col-4">
+          <img src={martin} className="img-fluid" />
+        </div>
+      </div>
+    </Article>
   );
 }
