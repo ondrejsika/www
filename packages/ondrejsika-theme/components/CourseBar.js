dce0ab6cc6cf80bcbfe3ea7121bb8ac0218d32ff
Zuzana Jeschke <zuz@zuzjes.com>
2020-08-19 23:14:46 +0200

    ondrejsika-theme - use trainera course bar, delete ondrej-sika course bar

--- packages/ondrejsika-theme/components/CourseBar.js
+++ packages/ondrejsika-theme/components/CourseBar.js
@@ -1,5 +1,7 @@
 import React from "react";
-import styled from "styled-components";
+import Link from "next/link";
+import styled, { css } from "styled-components";
+
 import docker from "@app/data/pictures/courses/white/docker.svg";
 import git from "@app/data/pictures/courses/white/git.svg";
 import gitlabci from "@app/data/pictures/courses/white/gitlab_ci.svg";
@@ -8,113 +10,212 @@ import react from "@app/data/pictures/courses/white/react.svg";
 import terraform from "@app/data/pictures/courses/white/terraform.svg";
 import ansible from "@app/data/pictures/courses/white/ansible.svg";
 import rancher from "@app/data/pictures/courses/white/rancher.svg";
-import prometheus from "@app/data/pictures/courses/prometheus-white.png";
+import prometheus from "@app/data/pictures/courses/white/prometheus.svg";
 import proxmox from "@app/data/pictures/courses/white/proxmox.svg";
+import elk from "@app/data/pictures/courses/white/elk.svg";
 
-import Link from "next/link";
+import default_colors from "@app/ondrejsika-theme/config/colors";
+import { Row, Col, Container } from "react-bootstrap";
+
+const H4 = styled.h4`
+  color: white;
+  text-align: center;
+  font-size: 0.8em;
+  padding-top: 0.4em;
+`;
+
+const Trainera = styled.div`
+  background-color: ${props =>
+    (props.site && props.site.colors && props.site.colors.PRIMARY) ||
+    default_colors.BLUE};
+`;
+const Header = styled.h3`
+  font-size: 2.5em;
+  font-weight: 700;
+  padding: 0.5em 0;
+  text-align: center;
+  color: white;
+`;
 
 const Box = styled.div`
+  width: 100%;
+  max-width: 221px;
+  text-align: center;
+  border: 2px solid transparent;
+  border-radius: 2px;
+  margin-bottom: 5px;
+  background-color: ${props =>
+    (props.site && props.site.colors && props.site.colors.PRIMARY) ||
+    default_colors.BLUE};
   &:hover {
-    background-color: #c7bfb0;
+    border: 2px solid
+      ${props =>
+        (props.site && props.site.colors && props.site.colors.SECONDARY) ||
+        default_colors.WHITE};
   }
+`;
+
+const Img = styled.img`
+  max-width: 90%;
+  height: auto;
+  padding: 0 1em 1em 1em;
+`;
+
+const MyCol = styled(Col)`
+  padding: 0px;
+`;
+
+const TriangleBottomRight = styled.div`
+  width: 100%;
+  height: 80px;
+  /* background: #088958; */
+  background-color: ${props =>
+    (props.site && props.site.colors && props.site.colors.PRIMARY) ||
+    default_colors.BLUE};
+  clip-path: polygon(-1px 100%, 100% -1px, 100% 100%);
+  margin-bottom: -1px;
+  ${props =>
+    props.hiddenTop &&
+    css`
+      display: none;
+    `}
+`;
+
+const TriangleTopLeft = styled.div`
+  width: 100%;
+  height: 80px;
+  /* background: #088958; */
+  background-color: ${props =>
+    (props.site && props.site.colors && props.site.colors.PRIMARY) ||
+    default_colors.BLUE};
+  clip-path: polygon(-1px 100%, 100% -1px, -1px -1px);
+  ${props =>
+    props.hiddenBottom &&
+    css`
+      display: none;
+    `}
 `;
 
 const Card = props => {
   let training = { en: "training", cs: "skoleni" }[props.lang || "cs"];
+
   return (
-    <Box className="box" id={props.courseId}>
-      <Link href={`/${training}/${props.courseId}`}>
-        <a className="course-header">
-          <h4 className="text-center text-white course-header pt-3">
-            <span className="course-header">{props.courseName}</span>
-          </h4>
-          <img src={props.courseImg} className="img img-fluid p-3" />
-        </a>
-      </Link>
-    </Box>
+    <MyCol md={2} sm={4} xs={6}>
+      <Box site={props.site}>
+        <Link href={`/${training}/${props.courseId}`}>
+          <a className="course-header">
+            <H4>
+              <span>{props.courseName}</span>
+            </H4>
+            <Img src={props.courseImg} />
+          </a>
+        </Link>
+      </Box>
+    </MyCol>
   );
 };
 
-const CourseBar = props => {
+const TraineraCourseBar = props => {
   return (
-    <div className="">
-      <h2 className="pt-4 pb-2" id="my-training">
-        {props.LectureImgHeader}
-      </h2>
-      <div className="container-flex banner-courses">
-        <div className="d-flex flex-wrap flex-sm-wrap flex-lg-nowrap d-lg-flex flex-row">
-          <Card
-            lang={props.lang}
-            courseName="Docker"
-            courseId="docker"
-            courseImg={docker}
-          />
-          <Card
-            lang={props.lang}
-            courseName="Kubernetes"
-            courseId="kubernetes"
-            courseImg={k8s}
-          />
-          <Card
-            lang={props.lang}
-            courseName="Git"
-            courseId="git"
-            courseImg={git}
-          />
-          <Card
-            lang={props.lang}
-            courseName="Gitlab CI"
-            courseId="gitlab-ci"
-            courseImg={gitlabci}
-          />
-          <Card
-            lang={props.lang}
-            courseName="Ansible"
-            courseId="ansible"
-            courseImg={ansible}
-          />
-          <Card
-            lang={props.lang}
-            courseName="Prometheus"
-            courseId="prometheus"
-            courseImg={prometheus}
-          />
-          {(() => {
-            if (!props.lang || props.lang == "cs") {
-              return (
-                <>
-                  <Card
-                    lang={props.lang}
-                    courseName="Terraform"
-                    courseId="terraform"
-                    courseImg={terraform}
-                  />
-                  <Card
-                    lang={props.lang}
-                    courseName="Rancher"
-                    courseId="rancher"
-                    courseImg={rancher}
-                  />
-                  <Card
-                    lang={props.lang}
-                    courseName="React"
-                    courseId="react"
-                    courseImg={react}
-                  />
-                  <Card
-                    lang={props.lang}
-                    courseName="Proxmox"
-                    courseId="proxmox"
-                    courseImg={proxmox}
-                  />
-                </>
-              );
-            }
-          })()}
-        </div>
-      </div>
-    </div>
+    <>
+      <TriangleBottomRight hiddenTop={props.hiddenTop} site={props.site} />
+      <Trainera site={props.site}>
+        <Container>
+          <Header>{props.LectureImgHeader}</Header>
+          <div className="container-flex banner-courses pb-3">
+            <Row>
+              <Card
+                site={props.site}
+                lang={props.lang}
+                courseName="Docker"
+                courseId="docker"
+                courseImg={docker}
+              />
+              <Card
+                site={props.site}
+                lang={props.lang}
+                courseName="Kubernetes"
+                courseId="kubernetes"
+                courseImg={k8s}
+              />
+              <Card
+                site={props.site}
+                lang={props.lang}
+                courseName="Git"
+                courseId="git"
+                courseImg={git}
+              />
+              <Card
+                site={props.site}
+                lang={props.lang}
+                courseName="Gitlab CI"
+                courseId="gitlab-ci"
+                courseImg={gitlabci}
+              />
+              <Card
+                site={props.site}
+                lang={props.lang}
+                courseName="Prometheus"
+                courseId="prometheus"
+                courseImg={prometheus}
+              />
+              <Card
+                site={props.site}
+                lang={props.lang}
+                courseName="Terraform"
+                courseId="terraform"
+                courseImg={terraform}
+              />
+              <Card
+                site={props.site}
+                lang={props.lang}
+                courseName="Ansible"
+                courseId="ansible"
+                courseImg={ansible}
+              />
+              <Card
+                site={props.site}
+                lang={props.lang}
+                courseName="Rancher"
+                courseId="rancher"
+                courseImg={rancher}
+              />
+              {(() => {
+                if (!props.lang || props.lang == "cs") {
+                  return (
+                    <>
+                      <Card
+                        site={props.site}
+                        lang={props.lang}
+                        courseName="React"
+                        courseId="react"
+                        courseImg={react}
+                      />
+                      <Card
+                        site={props.site}
+                        lang={props.lang}
+                        courseName="Elk"
+                        courseId="elk"
+                        courseImg={elk}
+                      />
+                      <Card
+                        site={props.site}
+                        lang={props.lang}
+                        courseName="Proxmox"
+                        courseId="proxmox"
+                        courseImg={proxmox}
+                      />
+                    </>
+                  );
+                }
+              })()}
+            </Row>
+          </div>
+        </Container>
+      </Trainera>
+      <TriangleTopLeft site={props.site} hiddenBottom={props.hiddenBottom} />
+    </>
   );
 };
 
-export default CourseBar;
+export default TraineraCourseBar;
