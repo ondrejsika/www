30bd49c472569d01ad07d3dac45c09392d206152
Ondrej Sika <ondrej@ondrejsika.com>
2020-08-19 18:57:31 +0200

    ccc.oxs.cz/components/Links - fix js errors

--- packages/ccc.oxs.cz/components/Links.js
+++ packages/ccc.oxs.cz/components/Links.js
@@ -1,12 +1,15 @@
+import React from "react";
+
 export default props => {
-  return props.links.map(link => {
+  return props.links.map((link, i) => {
     let name = link[0];
     let url = link[1];
     return (
-      <p>
+      <p key={i}>
         <a
           className="btn btn-outline-primary btn-block text-left"
           target="_blank"
+          rel="noopener noreferrer"
           href={url}
         >
           {name}
