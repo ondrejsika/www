6f80f7e64963d78e1bb4268d72658b6cc2308da9
Ondrej Sika <ondrej@ondrejsika.com>
2020-08-24 23:00:49 +0200

    ondrej-sika.cz/certifikaty - Add: Kubernetes Certified Application Developer (CKAD) with Tests (on Udemy)

--- packages/ondrej-sika.cz/pages/certifikaty.js
+++ packages/ondrej-sika.cz/pages/certifikaty.js
@@ -8,7 +8,8 @@ const Page = () => (
     markdown={`
 ## Udemy
 
-- https://www.udemy.com/certificate/UC-db716601-7905-4d98-9f5b-e9cf8a8a9d32/
+- [Kubernetes Certified Application Developer (CKAD) with Tests](https://www.udemy.com/certificate/UC-cf27dfc2-9943-4759-94e9-a2ff9cd22bf3/)
+- [Fundamentals of Kubernetes](https://www.udemy.com/certificate/UC-db716601-7905-4d98-9f5b-e9cf8a8a9d32/)
 `}
   />
 );
