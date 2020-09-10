43f0390ec38dad156269deeee027009500dfe15e
Ondrej Sika <ondrej@ondrejsika.com>
2020-09-10 15:20:03 +0200

    ondrejsika-theme/layouts/Article - upgrade footer (to BlogPostFooter)

--- packages/ondrejsika-theme/layouts/Article.js
+++ packages/ondrejsika-theme/layouts/Article.js
@@ -1,12 +1,27 @@
 import React from "react";
 import MainBar from "@app/ondrejsika-theme/components/MainBar";
 import Markdown from "@app/common/components/Markdown";
-import AdBar from "@app/ondrejsika-theme/components/AdBar";
-import NewsletterBar from "@app/ondrejsika-theme/components/NewsletterBar";
+import BlogPostFooter from "@app/ondrejsika-theme/components/BlogPostFooter";
 
 import Head from "next/head";
 
 const ArticleLayout = props => {
+  let trainingButtonLink = null;
+  let trainingButtonText = null;
+
+  if (props.ad) {
+    trainingButtonLink = "/skoleni/" + props.ad;
+    trainingButtonText = {
+      git: "Školení Gitu",
+      docker: "Školení Dockeru",
+      kubernetes: "Školení Kubernetes",
+      terraform: "Školení Terraformu",
+      react: "Školení Reactu",
+      "gitlab-ci": "Školení Gitlab CI",
+      rancher: "Školení Rancheru"
+    }[props.ad];
+  }
+
   return (
     <div>
       <Head>
@@ -21,26 +36,20 @@ const ArticleLayout = props => {
       />
 
       <div className="container pt-4 pb-2 article-body">
-        {props.children}
-        {(() => {
-          if (props.markdown) return <Markdown source={props.markdown} />;
-        })()}
-        {(() => {
-          if (props.after) return props.after;
-        })()}
-
-        {!props.ad && !props.hideNewsletter && (
-          <NewsletterBar site={props.site} />
-        )}
-        {props.ad && (
-          <div className="row">
-            <div className="col-6">
-              <AdBar ad={props.ad} />
-            </div>
-            <div className="col-6">
-              <NewsletterBar site={props.site} />
-            </div>
-          </div>
+        <div className="article">
+          {props.children}
+          {(() => {
+            if (props.markdown) return <Markdown source={props.markdown} />;
+          })()}
+          {(() => {
+            if (props.after) return props.after;
+          })()}
+        </div>
+        {!props.hideFooter && (
+          <BlogPostFooter
+            trainingButtonText={trainingButtonText}
+            trainingButtonLink={trainingButtonLink}
+          />
         )}
       </div>
     </div>
