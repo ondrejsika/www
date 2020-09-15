06ae02b36ac12d3c4e4c36d3f0593a6262bbfd32
Ondrej Sika <ondrej@ondrejsika.com>
2020-09-15 17:49:13 +0200

    Add follow up: Gordic Git

---
+++ packages/ondrej-sika.cz/pages/follow-up/2020-09-14-gordic-git.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Article from "@app/ondrejsika-theme/layouts/Article";
+
+const Page = () => (
+  <Article
+    title="Follow Up: Git, Gordic, 14. 9. 2020"
+    header="Follow Up: Git"
+    subheader="Gordic, 14. 9. 2020"
+    markdown={`
+## Materiály
+
+### Repozitář s kurzem
+
+https://github.com/ondrejsika/git-training/
+
+## Repozirář co jsme vytvořili
+
+https://github.com/sika-training-examples/git--gordic--2020-09-14--example-project
+
+### Články na mém webu ohledně Gitu
+
+https://ondrej-sika.cz/git
+
+Videa budou brzy, dám vědět a přidám je sem.
+
+## Další Školení
+
+Pokud byste měli zájem o nějaké další školení, můžete si vybrat zde:
+
+- Všechny kurzy - https://ondrej-sika.cz/seznam-skoleni
+- Veřejné termíny - https://ondrej-sika.cz/verejne-terminy/
+
+### Ostatní zajímavé repozitáře
+
+- https://ondrej-sika.cz/repozitare/ - Seznam zajímavých repozitářů
+- https://github.com/ondrejsika - Všechny mé repozitáře na Githubu
+
+Pokud se Vám bude něco líbit, budu rád za hvězdičky. Díky.
+
+## DevOps Live
+
+Začal jsem streamovat live coding na témata z DevOps, pokud by Vás to zajímalo více, koukněte na stránku [DevOps Live](/devopslive).
+
+Zde jsou zaznamy a blog posty z DevOps Live:
+
+- https://ondrej-sika.cz/blog/tag/devopslive
+
+## Další kroky
+
+- Budu rád, když mi napíšete doporučení na LinkedIn a na Twitter
+  - Linkedin: přidejte si mne a já vám pošlu žádost - https://www.linkedin.com/in/ondrejsika/
+  - Twitter: Tweetnete něco s \`@ondrejsika\`.
+- Přidejte se do komunit účastníků mých školení, které buduji na Slacku, Facebooku a Linkedinu - https://join.sika.io
+- Pokud jste ve zpětné vazbě nezaškrtly, že chcete newsletter, můžete se přihlásit zde - https://sika.link/newsletter/
+
+Ať se Vám daří ve všem, nejen s Gitem! Pivo si muzem doluvit na https://calendly.com/ondrejsika/beer.
+
+Hezky den,
+O.
+`}
+  />
+);
+
+export default Page;
--- packages/ondrej-sika.cz/pages/private/follow-up.js
+++ packages/ondrej-sika.cz/pages/private/follow-up.js
@@ -6,6 +6,7 @@ const Page = () => (
     title="Follow Up Index"
     hideNewsletter={true}
     markdown={`
+- [Git, Gordic, 14. 9. 2020](/follow-up/2020-09-14-gordic-git)
 - [Git, ZCU, 9. 9. 2020](/follow-up/2020-09-09-zcu-git)
 - [Docker & Kubernetes, Adbros, 5. 8. 2020](/follow-up/2020-08-05-adbros-docker-kubernetes)
 - [Gitlab CI, SNK, 31. 7. 2020](/follow-up/2020-07-31-snk-gitlab-ci)
