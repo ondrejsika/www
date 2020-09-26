ac080d22ebfdfdaa44310511f5410e2a1378f0c7
Ondrej Sika <ondrej@ondrejsika.com>
2020-09-26 09:07:20 +0200

    ondrej-sika.cz follow up - add Terraform 2020-09-25

--- packages/ondrej-sika.cz/pages/private/follow-up.js
+++ packages/ondrej-sika.cz/pages/private/follow-up.js
@@ -6,6 +6,7 @@ const Page = () => (
     title="Follow Up Index"
     hideNewsletter={true}
     markdown={`
+- [Terraform, 25. 9. 2020](/follow-up/2020-09-25-terraform)
 - [Git, Gordic, 14. 9. 2020](/follow-up/2020-09-14-gordic-git)
 - [Git, ZCU, 9. 9. 2020](/follow-up/2020-09-09-zcu-git)
 - [Docker & Kubernetes, Adbros, 5. 8. 2020](/follow-up/2020-08-05-adbros-docker-kubernetes)
