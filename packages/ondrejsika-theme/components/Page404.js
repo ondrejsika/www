f733758740b8873c98b90904f0dafd4483944ae1
Zuzana Jeschke <zuz@zuzjes.com>
2020-09-29 22:35:14 +0200

    ondrejsika-theme - fix layout of 404 page

--- packages/ondrejsika-theme/components/Page404.js
+++ packages/ondrejsika-theme/components/Page404.js
@@ -1,8 +1,10 @@
 import React from "react";
 import CourseBar from "@app/ondrejsika-theme/components/CourseBar";
-import Article from "@app/ondrejsika-theme/layouts/Article";
 import styled from "styled-components";
 import Translate from "@app/common/components/Translate";
+import MainBar from "@app/ondrejsika-theme/components/MainBar";
+import { Container } from "react-bootstrap";
+import Head from "next/head";
 
 const FontSize = styled.div`
   p,
@@ -10,71 +12,66 @@ const FontSize = styled.div`
     font-size: 24px;
   }
 `;
+const Page404 = props => (
+  <>
+    <Head>
+      <title>
+        404 Not found - {(props.site && props.site.name) || "Ondřej Šika"}
+      </title>
+    </Head>
+    <MainBar site={props.site} header="404 Not found" text={props.subheader} />
 
-export const Page404 = props => {
-  return (
-    <>
-      <Article
-        site={props.site}
-        title="404 Not found"
-        header={
-          <Translate
-            lang={props.site.lang}
-            default="en"
-            en="404 Don't found you are looking for ..."
-            cs="404 Nenašli jste co hledáte ..."
-          />
-        }
-        hideNewsletter={props.site.lang != "cs"}
-      >
-        <FontSize>
-          <Translate
-            lang={props.site.lang}
-            default="en"
-            en={
-              <p>
-                Send me and email{" "}
-                <a href="mailto:ondrej@sika.io">ondrej@sika.io</a> and let me
-                know what you need. I&apos;d like to help you.
-              </p>
-            }
-            cs={
-              <p>
-                Napište mi email na{" "}
-                <a href="mailto:ondrej@sika.io">ondrej@sika.io</a> a dejte mi
-                vědět co potřebujete. Rád Vám pomohu. Nebo se podivejte na kurzy
-                co školím:
-              </p>
-            }
-          />
-        </FontSize>
-        <CourseBar
-          site={props.site}
-          hidden={true}
-          LectureImgHeader="Vyberte si školení"
+    <FontSize>
+      <Container className="mt-5 mb-5">
+        <Translate
+          lang={props.site.lang}
+          default="en"
+          en={
+            <p>
+              Send me and email{" "}
+              <a href="mailto:ondrej@sika.io">ondrej@sika.io</a> and let me know
+              what you need. I&apos;d like to help you.
+            </p>
+          }
+          cs={
+            <p>
+              Napište mi email na{" "}
+              <a href="mailto:ondrej@sika.io">ondrej@sika.io</a> a dejte mi
+              vědět co potřebujete. Rád Vám pomohu. Nebo se podivejte na kurzy
+              co školím:
+            </p>
+          }
         />
-        <div className="mt-5" />
-        <FontSize>
-          <Translate
-            lang={props.site.lang}
-            default="en"
-            en={
-              <p>
-                If something doesnt work, please report to me on{" "}
-                <a href="mailto:ondrej@sika.io">ondrej@sika.io</a>
-              </p>
-            }
-            cs={
-              <p>
-                Pokud něco nefunguje, dejte mi vědět na{" "}
-                <a href="mailto:ondrej@sika.io">ondrej@sika.io</a>
-              </p>
-            }
-          />
-        </FontSize>
-      </Article>
-    </>
-  );
-};
+      </Container>
+    </FontSize>
+    <CourseBar
+      hiddenTop
+      hiddenBottom
+      site={props.site}
+      hidden={true}
+      LectureImgHeader="Vyberte si školení"
+    />
+    <Container className="mt-5 mb-5">
+      <FontSize>
+        <Translate
+          lang={props.site.lang}
+          default="en"
+          en={
+            <p>
+              If something doesnt work, please report to me on{" "}
+              <a href="mailto:ondrej@sika.io">ondrej@sika.io</a>
+            </p>
+          }
+          cs={
+            <p>
+              Pokud něco nefunguje, dejte mi vědět na{" "}
+              <a href="mailto:ondrej@sika.io">ondrej@sika.io</a>
+            </p>
+          }
+        />
+      </FontSize>
+    </Container>
+  </>
+);
 
 export default Page404;
