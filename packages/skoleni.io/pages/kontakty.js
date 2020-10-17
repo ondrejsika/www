030ff1bed71f84c7c2c8c3baaeaa866dbd72e2f3
Vojtech Mares <iam@vojtechmares.com>
2020-10-17 17:58:51 +0200

    skoleni.io - removed obsolete pricing in contacts

--- packages/skoleni.io/pages/kontakty.js
+++ packages/skoleni.io/pages/kontakty.js
@@ -16,16 +16,6 @@ const Kontakty = () => (
     <Container>
       <H1>Kontakty</H1>
       <P>
-        Chcete inzerovat sva skoleni na nasem webu? Napiste email a urcite se
-        domluvime. Cena za inzerci je 20% z ceny kurzu za kazdeho kdo se na kurz
-        registruje prez tento web. Pokud k vam klient, ktereho jste ziskali prez
-        skoleni.io, prijde i na dalsi skoleni, budu rad kdyz zaplatite 5% z ceny
-        kurzu. Verim ve svobodu a ferovost, chci at si kazdy sve kurzy promuje
-        jak uzna za vhodne, skoleni.io nemusi byt jedine misto kde bude nabizet
-        a prodavat kurzy. Je to ale dobre misto kde kurzy agregovat a nechat
-        firmy prochazet nezavysle profesionaly.
-      </P>
-      <P>
         Mate zajem o kurz? Napiste nam email na:
         <br />
         <a href="mailto:skoleni@skoleni.io">skoleni@skoleni.io</a>
@@ -34,12 +24,6 @@ const Kontakty = () => (
         Chcete s nami spolupracovat? Urcite se domluvime. Dejte vedet na:
         <br />
         <a href="mailto:spoluprace@skoleni.io">spoluprace@skoleni.io</a>
-        <br />
-        <br />
-        Mate nejakou otazku? Nebojte se zeptat na:
-        <br />
-        <a href="mailto:dotazy@skoleni.io">dotazy@skoleni.io</a>
-        <br />
       </P>
       <Margin />
     </Container>
