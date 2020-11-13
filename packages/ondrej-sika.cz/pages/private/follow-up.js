eb9706a18f1d1f217b34d35e1e71aceefe5c7a7a
Ondrej Sika <ondrej@ondrejsika.com>
2020-11-13 13:28:43 +0100

    ondrej-sika.cz - Add follow up: Kubernetes, Partners Group, 28. 10. 2020

--- packages/ondrej-sika.cz/pages/private/follow-up.js
+++ packages/ondrej-sika.cz/pages/private/follow-up.js
@@ -8,6 +8,7 @@ const Page = () => (
     markdown={`
 - [Git, O2, 11. 11. 2020](/follow-up/2020-11-11-o2-git)
 - [Gitlab CI, Mall, 9. 11. 2020](/follow-up/2020-11-09-mall-gitlab-ci)
+- [Kubernetes, Partners Group, 28. 10. 2020](/follow-up/2020-10-28-partners-group-kubernetes)
 - [Gitlab CI & Docker, Neeco, 22. 10. 2020](/follow-up/2020-10-22-neeco-gitlab-ci-docker)
 - [Kubernetes, Mavenir, 6. 10. 2020](/follow-up/2020-10-06-mavenir-kubernetes)
 - [Docker & Kubernetes, Eurosoftware, 29. 9. 2020](/follow-up/2020-09-29-eurosoftware-docker-kubernetes)
