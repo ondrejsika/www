245db1b258bead6d79b8fe20e4905f1013df6874
Ondrej Sika <ondrej@ondrejsika.com>
2020-11-26 22:30:01 +0100

    CI statica - add example of test deployment

--- generate-gitlab-ci-statica.py
+++ generate-gitlab-ci-statica.py
@@ -13,6 +13,11 @@ SITES = [
     {"name": "skoleni-rancher.cz"},
 ]
 
+SITES_DEV = [
+    {"name": "skoleni.io"},
+    {"name": "ondrej-sika.cz"},
+]
+
 _COURSE_LANDING_DEPENDENCIES = [
     "packages/data/training/sessions.yml",
     "packages/data/training/recommendations/**/*",
@@ -33,6 +38,7 @@ out = {
     "stages": [
         "start",
         "deploy",
+        "deploy test",
     ],
     "image": "ondrejsika/ci-node-statica",
     "variables": {
@@ -55,6 +61,29 @@ for site in SITES:
                     "rm -rf ./packages/%s/out" % name,
                     "yarn",
                     "yarn run deploy-%s" % name,
+                ],
+                "only": {
+                    "changes": gen_deps(_COURSE_LANDING_DEPENDENCIES, name),
+                },
+            }
+        }
+    )
+
+for site in SITES_DEV:
+    name = site["name"]
+    statica_domain = (
+        "test-" + name.replace(".", "-") + "-$CI_COMMIT_REF_SLUG.$STATICA_BASE_DOMAIN"
+    )
+    out.update(
+        {
+            "deploy test %s"
+            % name: {
+                "stage": "deploy test",
+                "script": [
+                    "rm -rf ./packages/%s/out" % name,
+                    "yarn",
+                    "yarn run static-%s" % name,
+                    "statica %s ./packages/%s/out" % (statica_domain, name),
                 ],
                 "only": {
                     "changes": gen_deps(_COURSE_LANDING_DEPENDENCIES, name),
