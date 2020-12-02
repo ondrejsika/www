5fa2ea11389167e7b8df160ab3e90276222cf965
Martin Jurco <martin@jurco.io>
2020-12-02 10:27:24 +0100

    fix typos in new instruction page Priprava Digital Ocean

--- packages/ondrej-sika.cz/pages/skoleni/digitalocean/priprava-na-skoleni.js
+++ packages/ondrej-sika.cz/pages/skoleni/digitalocean/priprava-na-skoleni.js
@@ -3,12 +3,12 @@ import Article from "@app/ondrejsika-theme/layouts/Article";
 
 const Page = () => (
   <Article
-    title="Příprava na Školenír Digital Oceanu"
+    title="Příprava na Školení Digital Oceanu"
     hideNewsletter={true}
     markdown={`
-Na školení Digigtal Oceanu je potřeba:
+Na školení Digital Oceanu je potřeba:
 
-- Účet na DigitalOcean (Pokud nemáte můžete použít můj referral: [Registrace + 50 USD kredit](https://sika.link/do-ref))
+- Účet na Digital Ocean (Pokud nemáte můžete použít můj referral: [Registrace + 50 USD kredit](https://sika.link/do-ref))
 - Digitlal Ocean CLI (\`doctl\`) - [návod na instalaci](https://www.digitalocean.com/docs/apis-clis/doctl/how-to/install/)
 - Nainstalovaný Terraform - [návod na instalaci](/terraform/instalace)
 
