10cbcc2c221e86325998b2fcefc69465cc47c5f2
Ondrej Sika <ondrej@ondrejsika.com>
2020-12-22 16:49:51 +0100

    Add docker-build-push.sh script (build docker image of site)

---
+++ docker-build-push.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+SITE=$1
+IMAGE=$2
+
+rm -rf packages/$SITE/out packages/$SITE/.next
+yarn static-$SITE
+rm -rf ci/docker/out
+cp -r packages/$SITE/out ci/docker/out
+docker build -t $IMAGE ci/docker --pull
+rm -rf ci/docker/out
+docker push $IMAGE
