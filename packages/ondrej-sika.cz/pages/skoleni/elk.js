4419f109faa6d3559e4de521d40668acbebba96f
Ondrej Sika <ondrej@ondrejsika.com>
2021-01-10 10:12:44 +0100

    courses.yml,ondrej-sika.cz - Add Czech description for ELK (Elastic Stack) course

--- packages/ondrej-sika.cz/pages/skoleni/elk.js
+++ packages/ondrej-sika.cz/pages/skoleni/elk.js
@@ -1,26 +1,16 @@
 import React from "react";
-import Course from "@app/ondrejsika-theme/layouts/Course";
+import CourseDD from "@app/ondrejsika-theme/layouts/CourseDD";
 import logo from "@app/data/pictures/courses/elk-color.png";
 
 const Page = props => (
-  <Course
+  <CourseDD
     site={props.site}
-    course_id="react"
-    show_sessions={true}
+    course_id="elk"
     lang={props.site.lang}
-    course_title="Školení ELK / EFK"
     logo={logo}
-    price_self_funded="11 999 CZK"
-    price_company_funded="14 999 CZK"
-    price_in_house="50 000 CZK"
     location="cz"
-  >
-    <div className="alert alert-danger mt-5" role="alert">
-      <h2>Připravuji ...</h2>
-      Na kurzu momentálně pracuji, pokud máte zájem, napište mi email anebo
-      vyplňte poptávku. Ozvu se Vám.
-    </div>
-  </Course>
+    show_sessions={true}
+  />
 );
 
 export default Page;
