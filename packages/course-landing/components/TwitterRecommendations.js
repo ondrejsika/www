8b87691237fa1f22d44cdbc680b658c1601a8f87
Ondrej Sika <ondrej@ondrejsika.com>
2021-04-19 07:17:32 +0200

    feat(courses-landing/TwitterRecommendations): Add support for 4 recommendations

--- packages/course-landing/components/TwitterRecommendations.js
+++ packages/course-landing/components/TwitterRecommendations.js
@@ -15,6 +15,40 @@ export default props => {
   let rec1 = props.ids[0];
   let rec2 = props.ids[1];
   let rec3 = props.ids[2];
+  let rec4 = props.ids[3];
+  if (rec4)
+    return (
+      <>
+      <div className="container-flex mb-4 row">
+        <div className="col-lg-6 col-md-6 col-sm-12">
+          <TwitterTweetEmbed
+            tweetId={get_by_id(rec1).tweet_id}
+            options={{ conversation: "none" }}
+          />
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-12">
+          <TwitterTweetEmbed
+            tweetId={get_by_id(rec2).tweet_id}
+            options={{ conversation: "none" }}
+          />
+        </div>
+      </div>
+      <div className="container-flex mb-4 row">
+        <div className="col-lg-6 col-md-6 col-sm-12">
+          <TwitterTweetEmbed
+            tweetId={get_by_id(rec3).tweet_id}
+            options={{ conversation: "none" }}
+          />
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-12">
+          <TwitterTweetEmbed
+            tweetId={get_by_id(rec4).tweet_id}
+            options={{ conversation: "none" }}
+          />
+        </div>
+      </div>
+      </>
+    );
   if (rec3)
     return (
       <div className="container-flex mb-4 row">
