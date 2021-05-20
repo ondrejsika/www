122c6e63ee4950ae822484ca6531dd48cfd13604
Ondrej Sika <ondrej@ondrejsika.com>
2021-05-20 19:37:47 +0200

    feat(ondrej-sika.cz): Add follow up for: TietoEVRY: Kubernetes 15. 04. 2021

---
+++ packages/ondrej-sika.cz/pages/follow-up/2021-04-19-tietoevry-kubernetes.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Article from "@app/ondrejsika-theme/layouts/Article";
+
+const Page = () => (
+  <Article
+    title="Follow Up: TietoEVRY: Kubernetes, 15. 04. 2021"
+    header="Follow Up: TietoEVRY: Kubernetes"
+    subheader="15. 04. 2021"
+    hideNewsletter={true}
+    markdown={`
+## Materiály
+
+### Repozitáře, které jsme použili
+
+- Kubernetes - https://github.com/ondrejsika/kubernetes-training/
+- Terraform - https://github.com/ondrejsika/terraform-training/
+- ELK - https://github.com/ondrejsika/elk-training
+- Prometheus - https://github.com/ondrejsika/prometheus-training
+
+### Záznamy
+
+- 1st day - https://www.youtube.com/watch?v=QZO3JKfrwyQ
+- 2nd day - https://www.youtube.com/watch?v=i_crE-7bQM0
+- 3rd day - https://www.youtube.com/watch?v=3_8LbVIwu9o
+
+### Další zajímavé odkazy a repozitáře
+
+- [DevOps Live](/devopslive) - Live streamy na témata z DevOps
+- [Kubernetes na mém blogu](/blog/tag/kubernetes/)
+- [https://ondrej-sika.cz/repozitare/](/repozitare) - Seznam zajímavých repozitářů
+- https://github.com/ondrejsika - Všechny mé repozitáře na Githubu
+
+### Repozitáře, které jsme při školení vytvořili
+
+- https://github.com/sika-training-examples
+
+### Další použité repozitáře
+
+- Terraform Infrastruktura Demo Gitlabu - https://github.com/ondrejsika/terraform-demo-gitlab
+- Traefik Ingress for Kubernets - https://github.com/ondrejsika/kubernetes-ingress-traefik
+- Demo Kubernetes Cluster on DigitalOcean - https://github.com/ondrejsika/terraform-do-kubernetes-example
+- Prometheus on Kubernetes - https://github.com/ondrejsika/ondrejsika-k8s-prom
+
+### Témata, které jsme nakousli
+
+- Terraform - Infrastructure as a code, [Školení Terraformu](/skoleni/terraform)
+- RKE & Rancher - On premise Kubernetes [Školení Rancheru](/skoleni/rancher)
+- DigitalOcean - Cloudova Platforma, [Školení DigitalOcean](/skoleni/digitalocean)
+
+Pokud byste měli zájem o nějaké další školení, můžete si vybrat zde:
+
+- Všechny kurzy - [https://ondrej-sika.cz/seznam-skoleni/](/seznam-skoleni/)
+- Veřejné termíny - [https://ondrej-sika.cz/verejne-terminy/](/verejne-terminy/)
+
+Pokud se Vám bude něco líbit, budu rád za hvězdičky. Díky.
+
+## DevOps Live
+
+Začal jsem streamovat live coding na témata z DevOps, pokud by Vás to zajímalo více, koukněte na stránku [DevOps Live](/devopslive).
+
+## Další kroky
+
+- Budu rád, když mi napíšete doporučení na LinkedIn a na Twitter
+  - Linkedin: přidejte si mne a já vám pošlu žádost - https://www.linkedin.com/in/ondrejsika/
+  - Twitter: Tweetnete něco s \`@ondrejsika\`.
+- Přidejte se do komunit účastníků mých školení, které buduji na Slacku, Facebooku a Linkedinu - https://join.sika.io
+- Pokud jste ve zpětné vazbě nezaškrtly, že chcete newsletter, můžete se přihlásit zde - https://sika.link/newsletter/
+
+Ať se Vám daří ve všem, nejen s Kubernetes!
+<br>O.
+`}
+  />
+);
+
+export default Page;
