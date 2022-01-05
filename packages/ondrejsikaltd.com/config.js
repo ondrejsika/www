38d29d3c17c3b729a3464d1daf0f8a322a710820
Ondrej Sika <ondrej@ondrejsika.com>
2022-01-05 09:20:22 +0100

    cleanup: Remove ondrejsikaltd.com

--- packages/ondrejsikaltd.com/config.js
+++
@@ -1,8 +0,0 @@
-const site = {
-  name: "sikahq.com",
-  lang: "en",
-  gauges_site_id: "5adb359de2283457d3a10718",
-  google_analytics_site_id: "XXX"
-};
-
-export default site;
--- packages/ondrejsikaltd.com/next.config.js
+++
@@ -1,2 +0,0 @@
-// Next.js config is same for all course landing pages, so we can import it ;)
-module.exports = require("../bootstrap-theme/next-config/next.config.js");
