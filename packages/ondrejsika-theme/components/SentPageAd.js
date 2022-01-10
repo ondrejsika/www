3baf356df2bdfd91121b6aab8e8bd0d1cbd15deb
Ondrej Sika <ondrej@ondrejsika.com>
2022-01-10 22:29:46 +0100

    feat(ondrejsika-theme/SentPageAd): Create Ad component for send confirmation page

---
+++ packages/ondrejsika-theme/components/SentPageAd.js
@@ -0,0 +1,97 @@
+import React from "react";
+
+const SentPageAd = () => {
+  return (
+    <div className="row">
+      <div className="col-lg-4">
+        <h3>Follow me on</h3>
+        <ul>
+          <li>
+            <a
+              href="https://twitter.com/ondrejsika"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://linkedin.com/in/ondrejsika"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/ondrejsikadevops/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Facebook
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-lg-4">
+        <h3>Join my</h3>
+        <ul>
+          <li>
+            <a
+              href="https://sika.link/slack-sikapublic"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Slack
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/groups/8902190/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn Group
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/groups/3484478441626937/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Facebook Group
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-lg-4">
+        <h3>Subscribe</h3>
+        <ul>
+          <li>
+            <a
+              href="https://sika.link/newsletter"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Newsletter
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://eepurl.com/dwRHHP"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              DevOps Live Newsletter
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default SentPageAd;
