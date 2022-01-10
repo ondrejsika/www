26b60a78810b4a7311dda4913dca2e797f0e388c
Ondrej Sika <ondrej@ondrejsika.com>
2022-01-10 22:09:33 +0100

    feat(ondrej-sika.cz/kniha): Add form (backed by TrainingCRM WaitingListAPI)

--- packages/ondrej-sika.cz/pages/kniha.js
+++ packages/ondrej-sika.cz/pages/kniha.js
@@ -1,10 +1,11 @@
 import React from "react";
 import MainBar from "@app/ondrejsika-theme/components/MainBar";
 import img from "@app/data/pictures/kniha/kniha_v1.jpg";
+import ChciKnihuForm from "@app/ondrejsika-theme/components/ChciKnihuForm";
 
 import Head from "next/head";
 
-const Install = () => (
+const Install = props => (
   <div>
     <Head>
       <title>DevOps Kniha - Ondrej Sika</title>
@@ -14,15 +15,27 @@ const Install = () => (
       <div className="row">
         <div className="col-lg-8 mb-5">
           <p className="h2">
-            Píši knihu o DevOps, brzy bude více informací. Pokud chcete dostávat
+            Píši knihu o DevOps,
+            <br />
+            brzy bude více informací.
+            {/* Pokud chcete dostávat
             novinky nejen novinky o knize, přihlaste se do mého{" "}
             <a href="https://sika.link/newsletter" className="h2">
               newsletteru
             </a>
-            .
+            . */}
           </p>
-          <p className="h2 mt-5">
-            Pokud máte o knihu zájem přijímam předobjednávky emailem na{" "}
+          <div className="text-center alert-success p-4 mt-4 mb-4 rounded">
+            <p>Zanechte mi email a já vám budu posílat update ke knize</p>
+            <div className="row justify-content-center align-items-center">
+              <ChciKnihuForm site={props.site} />
+            </div>
+          </div>
+
+          <p className="h2 mt-4">
+            Pokud máte o knihu zájem,
+            <br />
+            přijímam předobjednávky emailem na{" "}
             <a href="mailto:ondrej@sika.io?subject=Kniha" className="h2">
               ondrej@sika.io
             </a>
