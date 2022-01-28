168c3682064506208bf439d45625f13a3a32d1f8
Ondrej Sika <ondrej@ondrejsika.com>
2022-01-28 08:15:24 +0100

    feat(ondrej-sika.cz/skoleni): List all courses (basic list)

--- packages/ondrej-sika.cz/pages/skoleni/index.js
+++ packages/ondrej-sika.cz/pages/skoleni/index.js
@@ -1,8 +1,33 @@
 import React from "react";
-import CourseBar from "@app/ondrejsika-theme/components/CourseBar";
 import MainBar from "@app/ondrejsika-theme/components/MainBar";
 
 import Head from "next/head";
+import Link from "next/link";
+
+let links = [
+  ["GIT", "/skoleni/git"],
+  ["GITLAB CI", "/skoleni/gitlab-ci"],
+  ["DOCKER", "/skoleni/docker"],
+  ["KUBERNETES", "/skoleni/kubernetes"],
+  ["OPENSHIFT", "/skoleni/openshift", "new"],
+  ["ARGOCD", "/skoleni/argocd", "new"],
+  ["RANCHER", "/skoleni/rancher"],
+  ["ANSIBLE", "/skoleni/ansible"],
+  ["TERRAFORM", "/skoleni/terraform"],
+  ["CLOUDFLARE", "/skoleni/cloudflare", "new"],
+  ["PROMETHEUS", "/skoleni/prometheus"],
+  ["PROXMOX", "/skoleni/proxmox"],
+  ["ELK / EFK", "/skoleni/elk"],
+  ["DIGITAL OCEAN", "/skoleni/digitalocean"],
+  ["PACKER", "/skoleni/packer", "new"],
+  ["VAULT", "/skoleni/vault", "new"],
+  // ["AWS", "/skoleni/aws"],
+  // ["OPEN STACK", "/skoleni/openstack"],
+  // ["GO (GOLANG)", "/skoleni/go"],
+  ["DJANGO", "/skoleni/django"],
+  // ["REACT & NEXT.JS", "/skoleni/react"],
+  ["VIDEOKURZY", "/videokurzy"]
+];
 
 const Index = () => (
   <div>
@@ -10,8 +35,18 @@ const Index = () => (
       <title>Školení - Ondřej Šika</title>
     </Head>
     <MainBar header="Nabídka Školení" />
-    <div className="container pb-4">
-      <CourseBar LectureImgHeader="Kurzy co školím" />
+    <div className="container pb-4 mt-5">
+      {links.map((link, i) => {
+        return (
+          <div key={i}>
+            <Link href={link[1]}>
+              <a>
+                <h1>{link[0]}</h1>
+              </a>
+            </Link>
+          </div>
+        );
+      })}
     </div>
   </div>
 );
