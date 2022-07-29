8847f132c03ab860bc3b9b506f335f1d62457388
Ondrej Sika <ondrej@ondrejsika.com>
2022-07-29 06:22:28 +0200

    ci: Add --platform linux/amd64 to docker build

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -7,6 +7,6 @@ rm -rf packages/$SITE/out packages/$SITE/.next
 yarn static-$SITE
 rm -rf ci/docker/out
 cp -r packages/$SITE/out ci/docker/out
-docker build -t $IMAGE ci/docker --pull
+docker build --platform linux/amd64 -t $IMAGE ci/docker --pull
 rm -rf ci/docker/out
 docker push $IMAGE
--- generate-gitlab-ci.py
+++ generate-gitlab-ci.py
@@ -162,7 +162,7 @@ for site in SITES:
     - yarn run static-%(site)s
     - docker login $CI_REGISTRY -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD
     - cp ci/docker/* packages/%(site)s/
-    - docker build -t $CI_REGISTRY_IMAGE/%(site)s:$CI_COMMIT_SHORT_SHA packages/%(site)s
+    - docker build --platform linux/amd64 -t $CI_REGISTRY_IMAGE/%(site)s:$CI_COMMIT_SHORT_SHA packages/%(site)s
     - rm packages/%(site)s/Dockerfile
     - rm packages/%(site)s/nginx-site.conf
     - docker push $CI_REGISTRY_IMAGE/%(site)s:$CI_COMMIT_SHORT_SHA
