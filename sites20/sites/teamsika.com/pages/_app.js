dd1c493d48fff8178d50454030027377327ae6ea
Ondrej Sika <ondrej@ondrejsika.com>
2023-01-08 10:33:10 +0100

    feat(teamsika.com): Create teamsika.com site

---
+++ sites20/sites/teamsika.com/pages/_app.js
@@ -0,0 +1,5 @@
+import App from "next/app";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default App;
---
+++ sites20/sites/teamsika.com/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+
+const Index = () => (
+  <div>
+    <Head>
+      <title>teamsika.com</title>
+    </Head>
+    <div className="container-fluid mt-2">
+      <h1>
+        Team behind <a href="https://sika.io">Ondrej Sika</a> &amp;{" "}
+        <a href="https://sikalabs.com">Sika Labs</a>
+      </h1>
+    </div>
+  </div>
+);
+
+export default Index;
