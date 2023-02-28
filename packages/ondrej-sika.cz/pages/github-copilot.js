331d06f1e7eb9ea74c1c916ac96b6f83e3fd8516
Ondrej Sika <ondrej@ondrejsika.com>
2023-02-28 21:50:00 +0100

    content(ondrej-sika.cz/github-copilot): Add "Hello Welt"

--- packages/ondrej-sika.cz/pages/github-copilot.js
+++ packages/ondrej-sika.cz/pages/github-copilot.js
@@ -3,10 +3,11 @@ import Article from "@app/ondrejsika-theme/layouts/Article";
 import img1 from "@app/ondrej-sika.cz/public/content/github_copilot/2022-10-04_1.png";
 import img2 from "@app/ondrej-sika.cz/public/content/github_copilot/2022-10-04_2.png";
 import img3 from "@app/ondrej-sika.cz/public/content/github_copilot/2022-10-04_3.png";
+import img4 from "@app/ondrej-sika.cz/public/content/github_copilot/2023-02-26_german.png";
 
 const Page = () => (
   <Article title="Github Copilot Ukazky">
-    {[img1, img2, img3].map((img, i) => (
+    {[img4, img1, img2, img3].map((img, i) => (
       <div key={i}>
         <img
           src={img.src}
