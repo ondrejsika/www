20d8c39d3203727e92b90158e1b6adfb7553761a
Ondrej Sika <ondrej@ondrejsika.com>
2023-06-05 12:45:20 +0200

    feat(ondrej-sika.cz/follow-up): Add follow up for: go 2023-05-29

--- packages/ondrej-sika.cz/pages/private/follow-up.js
+++ packages/ondrej-sika.cz/pages/private/follow-up.js
@@ -7,6 +7,7 @@ const Page = () => (
     hideNewsletter={true}
     markdown={`
 <!--- PLACEHOLDER FOLLOW_UP_LINK (for create-follow-up.py script) --->
+- [go, 2023-05-29](/follow-up/2023-05-29-go)
 - [kubernetes, 2023-02-19](/follow-up/2023-02-19-kubernetes)
 - [gitlab-ci, 2022-10-03](/follow-up/2022-10-03-gitlab-ci)
 - [Heidelberg Cement, 2022-04-19](/follow-up/2022-04-19-heidelberg-cement)
