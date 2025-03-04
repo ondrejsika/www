f3661685a67a45b05c219dc6068c520d12ad8c61
Ondrej Sika <ondrej@ondrejsika.com>
2025-03-04 09:10:58 +0100

    feat(ondrej-sika.cz/jsemvkurzu): Prepare empty page for Jsem v Kurzu courses, committed in the sky on the way from Oslo to Svalbard

---
+++ packages/ondrej-sika.cz/pages/jsemvkurzu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Article from "@app/ondrejsika-theme/layouts/Article";
+
+export const ObchodniSpoluprace = () => (
+  <Article
+    title="JSEM V KURZU"
+    hideNewsletter={false}
+    markdown={`
+TODO
+`}
+  />
+);
+
+export default ObchodniSpoluprace;
