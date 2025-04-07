4e2a7c0a2293807001a08472903907de1b792890
Ondrej Sika <ondrej@ondrejsika.com>
2025-04-07 20:03:34 +0200

    feat(ondrej-sika.cz/clanky/zajimave-osobni-stranky): Add links to Github sources for jma.dev

--- packages/ondrej-sika.cz/pages/clanky/zajimave-osobni-stranky.js
+++ packages/ondrej-sika.cz/pages/clanky/zajimave-osobni-stranky.js
@@ -44,7 +44,7 @@ Můj seznam zajímavých osobních webů obsahem i formou. Průběžne aktualizu
 - <https://danfarrelly.com>
 - <https://yurimilner.com/>
 - <https://arne.me/> ([source](https://github.com/bahlo/arne.me), [fork](https://github.com/ondrejsika-inspiration/arne.me))
-- <https://jma.dev/>
+- <https://jma.dev/> ([source](https://github.com/imjma/jma.dev), [fork](https://github.com/ondrejsika-inspiration/jma.dev))
     `}
     />
   );
