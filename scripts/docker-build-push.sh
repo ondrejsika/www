9acce171a993b0dd903245c6ec0b31af0369332f
Ondrej Sika <ondrej@ondrejsika.com>
2026-07-10 23:09:02 +0200

    feat(scripts/docker-build-push.sh): Add .../api/version.json to final image

--- scripts/docker-build-push.sh
+++ scripts/docker-build-push.sh
@@ -4,7 +4,11 @@ SITE=$1
 IMAGE=$2
 
 rm -rf packages/$SITE/out packages/$SITE/.next
+mkdir -p packages/$SITE/public/api
+CI_COMMIT_TITLE=$(git show -s --format=%s)
+slu static-api version -e "CI_COMMIT_TITLE=$CI_COMMIT_TITLE" -e "HOSTNAME=$(hostname)" > packages/$SITE/public/api/version.json
 yarn static-$SITE
+rm -rf packages/$SITE/public/api/version.json
 rm -rf ci/docker/out
 cp -r packages/$SITE/out ci/docker/out
 docker build --platform linux/amd64 -t $IMAGE ci/docker --pull
